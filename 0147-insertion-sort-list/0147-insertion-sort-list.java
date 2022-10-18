ea725d03c087f1c8a32801ddbdd101226554f371
Jihoo Park <67787453+wlgn8648@users.noreply.github.com>
2022-10-18 22:30:48 +0900

    Time: 65 ms (6.67%), Space: 45 MB (28.18%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.java
+++ 0147-insertion-sort-list/0147-insertion-sort-list.java
@@ -9,28 +9,25 @@
  * }
  */
 class Solution {
-    public ListNode insertionSortList(ListNode head) {
-        ListNode sorted = new ListNode(head.val);
+    public ListNode insertionSortList(ListNode head) {ListNode sorted = new ListNode(head.val);
         ListNode now = head.next;
-        if(now == null)
+        if (now == null)
             return sorted;
 
         ListNode answer = sorted;
-        while(now != null){
+        while (now != null) {
             ListNode point = answer;
             boolean flag = false;
-            while(point != null){
-                if(now.val >= point.val && (point.next==null || point.next != null && now.val < point.next.val)){
-                    ListNode insert = new ListNode(now.val, point.next);
-                    point.next = insert;
+            while (point != null) {
+                if (now.val >= point.val && (point.next == null || now.val < point.next.val)) {
+                    point.next = new ListNode(now.val, point.next);
                     flag = true;
                     break;
-                }else
+                } else
                     point = point.next;
             }
-            if(!flag){
-                ListNode insert = new ListNode(now.val, answer);
-                answer = insert;
+            if (!flag) {
+                answer = new ListNode(now.val, answer);
             }
             now = now.next;
         }
