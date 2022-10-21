dfda7d65d9e70a1f9e86ef5e01b393114ccb84e4
Jihoo Park <67787453+wlgn8648@users.noreply.github.com>
2022-10-22 00:56:09 +0900

    Time: 664 ms (58.90%), Space: 16.2 MB (14.47%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,5 +1,3 @@
-from collections import OrderedDict
-
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
         dictArr = {i:abs(x - arr[i]) for i in range(len(arr))}
