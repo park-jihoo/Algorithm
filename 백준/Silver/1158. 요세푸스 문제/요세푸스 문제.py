b5621eec868585bbbb1a7e31bcea5d3e614831c4
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2022-12-06 14:14:00 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 500 ms, Memory: 231260 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+n, k = map(int, input().split())
+
+q = deque(list(range(1, n + 1)))
+a = []
+
+for i in range(n):
+    for j in range(k):
+        temp = q.popleft()
+        if j == k-1:
+            a.append(str(temp))
+        else:
+            q.append(temp)
+
+print("<"+", ".join(a)+">")
