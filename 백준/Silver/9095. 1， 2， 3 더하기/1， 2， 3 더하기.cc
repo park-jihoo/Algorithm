bd7e8a3f2ad8135676ca2f4000d566d1e4753508
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2022-12-06 14:15:03 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(void){
+    int T, cnt;
+	cin>>T;
+	vector<int>ans;
+	int ways[11];
+	for(int i=0;i<T;i++){
+		int num;
+		cin>>num;
+		ans.push_back(num);
+	}
+	ways[0]=1;
+	ways[1]=2;
+	ways[2]=4;
+	for(int i=3;i<11;i++){
+		ways[i]=ways[i-1]+ways[i-2]+ways[i-3];
+	}
+	for(int i=0;i<T;i++){
+		cout<<ways[ans[i]-1]<<endl;
+	}
+}
