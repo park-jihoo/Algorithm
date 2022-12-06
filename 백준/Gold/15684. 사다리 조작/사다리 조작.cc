859e9b2a00e1c6940da605f7d17efe18ecdfb435
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2022-12-06 18:33:27 +0900

    [Gold III] Title: 사다리 조작, Time: 576 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.cc
@@ -0,0 +1,43 @@
+#include<cstdio>
+int N, M, H, mincnt=999999;
+int D[31][11];
+bool isladder(){
+    for(int i=1, pos; i<=N;i++){
+        pos=i;
+        for(int j=1;j<=H;j++){
+            if(D[j][pos]==1)pos++;
+            else if(D[j][pos-1]==1)pos--;
+        }
+        if(i!=pos) return false;
+    }
+    return true;
+}
+void func(int cnt, int x, int y){
+    if(cnt>=mincnt)return;
+    if(isladder()){
+        mincnt=cnt;
+        return;
+    }
+    if(cnt==3)return;
+    for(int i=y;i<=H;i++, x=1)
+        for(int j=x;j<N;j++){
+            if(D[i][j]){
+                j++;
+                continue;
+            }
+            D[i][j]=1;
+            func(cnt+1, j+2, i);
+            D[i][j]=0;
+        }
+}
+int main(){
+    scanf("%d %d %d",&N, &M, &H);
+    for(int i=0, a, b;i<M;i++){
+        scanf("%d %d", &a, &b);
+        D[a][b] =1;
+    }
+    func(0, 1, 1);
+    if(mincnt>3){printf("-1");}
+    else{printf("%d", mincnt);}
+    return 0;
+}
