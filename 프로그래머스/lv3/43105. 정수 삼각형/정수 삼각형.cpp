ddc49771722a804d28d57462737dbc14b33d7d47
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2022-12-06 18:31:19 +0900

    [level 3] Title: 정수 삼각형, Time: 0.80 ms, Memory: 8.85 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<vector<int>> triangle) {
+    int mytri[500][500];
+    int answer=0;
+    mytri[0][0]=triangle[0][0];
+    for(int i=1;i<triangle.size();i++){
+        mytri[i][0]=triangle[i][0]+mytri[i-1][0];
+        for(int j=1;j<i;j++){
+            mytri[i][j]=max(mytri[i-1][j], mytri[i-1][j-1])+triangle[i][j];
+        }
+        mytri[i][i]=triangle[i][i]+mytri[i-1][i-1];
+    }
+    for(int i=0;i<triangle.size();i++){
+        if(mytri[triangle.size()-1][i]>answer)answer=mytri[triangle.size()-1][i];
+    }
+    return answer;
+}
