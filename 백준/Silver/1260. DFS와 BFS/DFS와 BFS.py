82198f4547304c77d8522bed12cb5664c00f3462
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2022-12-06 14:12:10 +0900

    [Silver II] Title: DFS와 BFS, Time: 720 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -0,0 +1,34 @@
+n, m, v = map(int, input().split())
+
+board = {x: [] for x in range(1, n + 1)}
+
+for i in range(m):
+    x, y = map(int, input().split())
+    board[x].append(y)
+    board[y].append(x)
+
+
+def dfs(board, v):
+    need_visited, visited = list(), list()
+    need_visited.append(v)
+    while need_visited:
+        node = need_visited.pop()
+        if node not in visited:
+            visited.append(node)
+            need_visited.extend(sorted(board[node], reverse=True))
+    return visited
+
+
+def bfs(board, v):
+    need_visited, visited = list(), list()
+    need_visited.append(v)
+    while need_visited:
+        node = need_visited[0]
+        del need_visited[0]
+        if node not in visited:
+            visited.append(node)
+            need_visited.extend(sorted(board[node]))
+    return visited
+
+print(" ".join([str(x) for x in dfs(board, v)]))
+print(" ".join([str(x) for x in bfs(board, v)]))
