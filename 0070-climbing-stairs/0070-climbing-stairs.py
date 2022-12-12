e3cd42dc6a9d84237d6f2a45677e6ac73fec58a4
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2022-12-12 15:43:15 +0900

    Time: 50 ms (62.81%), Space: 13.8 MB (58.08%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def climbStairs(self, n):
+        arr = [0, 1, 2]
+        for i in range (n+1):
+            if i>2:
+                arr.append(arr[i-1]+arr[i-2])
+        return arr[n]
