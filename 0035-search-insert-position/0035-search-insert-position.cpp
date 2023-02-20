0a8351d3528feb19634533516622947248f04538
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-02-20 15:34:09 +0900

    Time: 3 ms (90.12%), Space: 9.8 MB (31.02%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int start = 0;
+        int end = nums.size() - 1;
+        while(start <= end){            
+            int center = (start + end) / 2;
+            if(nums[center] > target)
+                end = center - 1;
+            else if(nums[center] < target)
+                start = center + 1;
+            else
+                return center;
+        }
+        return start;
+    }
+};
