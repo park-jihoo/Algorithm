79d5332d714883878ab72e3d9add8325feacff24
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-02-22 23:11:04 +0900

    Time: 1056 ms (10.94%), Space: 17 MB (78.06%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,23 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        def possible(capacity):
+            ships = []
+            for c in weights:
+                if c > capacity:
+                    return False
+                elif len(ships)==0 or ships[-1] + c > capacity:
+                    ships.append(c)
+                else:
+                    ships[-1]+=c
+                if len(ships) > days:
+                    return False
+            return True
+        left = 0
+        right = sum(weights)
+        while left <= right:
+            center = (left + right) // 2
+            if possible(center):
+                right = center - 1
+            else:
+                left = center + 1
+        return left
