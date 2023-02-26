361dc83a16e894a56f0505eb5129a7177ba056d4
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-02-26 14:35:06 +0900

    Time: 2137 ms (12.91%), Space: 15.4 MB (31.68%) - LeetHub

---
+++ 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def distinctPrimeFactors(self, nums: List[int]) -> int:
+        def getFactor(n):
+            n_sqrt = int(n ** 1/2) + 1
+            factorset = set()
+            for i in range(2, n_sqrt):
+                while n % i == 0:
+                    factorset.add(i)
+                    n /= i
+            if len(factorset) == 0:
+                factorset.add(n)
+            return factorset
+        answerset = set()
+        for i in nums:
+            answerset.update(getFactor(i))
+        return len(answerset)
