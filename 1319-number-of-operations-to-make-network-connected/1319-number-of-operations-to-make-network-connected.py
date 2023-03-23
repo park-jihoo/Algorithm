08e81c4f8cd0611d04f31ead87d2aa977976cde3
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-03-23 22:48:34 +0900

    Time: 519 ms (55.92%), Space: 42.5 MB (34.59%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+class Solution:
+    def dfs(self, x, graph, visited):
+        visited[x] = True
+        for i in graph[x]:
+            if not visited[i]:
+                self.dfs(i, graph, visited)
+
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        if len(connections) < n-1:
+            return -1
+        graph = {x: set() for x in range(n)}
+        for s, e in connections:
+            graph[s].add(e)
+            graph[e].add(s)
+        visited = [False]*n
+        answer = -1
+        for computer in range(n):
+            if not visited[computer]:
+                self.dfs(computer, graph, visited)
+                answer+=1
+        return answer
