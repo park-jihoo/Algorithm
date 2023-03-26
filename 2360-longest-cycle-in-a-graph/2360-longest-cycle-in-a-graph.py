2e7748799f08979f8253f332f534e7fb6e7c8def
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-03-26 16:15:19 +0900

    Time: 1376 ms (60.44%), Space: 123.1 MB (57.94%) - LeetHub

---
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
@@ -0,0 +1,26 @@
+class Solution:
+    def dfs(self, i, edges):
+        global visited, stack, start, answer, dfsvisited
+        visited[i] = True
+        if edges[i] == -1:
+            dfsvisited = []
+        elif not visited[edges[i]]:
+            dfsvisited.append(edges[i])
+            self.dfs(edges[i], edges)
+        elif edges[i] in dfsvisited:
+            answer = max(answer, len(dfsvisited) - dfsvisited.index(edges[i]))
+        stack.append(i)
+        pass
+
+    def longestCycle(self, edges: List[int]) -> int:
+        global visited, stack, start, answer, dfsvisited
+        visited = [False for i in range(len(edges))]
+        stack = []
+        answer = -1
+        for i in range(len(edges)):
+            if visited[i] == False:
+                start = i
+                dfsvisited = [i]
+                self.dfs(i, edges)
+        
+        return answer
