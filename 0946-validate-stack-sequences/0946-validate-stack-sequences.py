b9303817e2a33d6dd4e4f67e12e6396d886f93a4
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-04-13 10:59:09 +0900

    Time: 73 ms (61.68%), Space: 14.1 MB (34.41%) - LeetHub

---
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        push_idx, pop_idx, n = 0, 0, len(pushed)
+        stack = deque([])
+        stack.append(pushed[push_idx])
+        push_idx+=1
+
+        while stack:
+            while stack[-1] != popped[pop_idx]:
+                if push_idx == n:
+                    return False
+                stack.append(pushed[push_idx])
+                push_idx +=1
+            stack.pop()
+            pop_idx+=1
+            if(len(stack)==0 and push_idx < n):
+                stack.append(pushed[push_idx])
+                push_idx +=1
+
+        return True
