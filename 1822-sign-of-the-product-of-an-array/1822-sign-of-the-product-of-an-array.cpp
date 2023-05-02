af79d0c8a81b48e924a4a25b77ba33d5e1792743
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-05-02 15:04:00 +0900

    Time: 11ms Memory 10.2MB

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        bool flag = true;
+        for(int num:nums){
+            if(num == 0)
+                return 0;
+            else if(num < 0)
+                flag = !(flag);
+        }
+        if(flag)
+            return 1;
+        else
+            return -1;
+    }
+};
