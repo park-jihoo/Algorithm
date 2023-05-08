b28a9da7f61af5f815a0f5167871226303dcdcff
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-05-08 16:11:19 +0900

    Time: 65 ms (36.28%) | Memory: 44 MB (40.47%) - LeetSync

---
+++ matrix-diagonal-sum/matrix-diagonal-sum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[][]} mat
+ * @return {number}
+ */
+var diagonalSum = function(mat) {
+    const n = mat.length;
+    let answer = 0;
+    for(var i=0;i<n;i++){
+        answer += mat[i][i];
+        if(i != n - i - 1){
+            answer += mat[i][n-i-1];
+        }
+    }
+    return answer;
+};
