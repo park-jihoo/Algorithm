64cc9068eb8e84adb005d0deb41bc8898206b13e
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-05-09 11:26:28 +0900

    Time: 40 ms (20.88%) | Memory: 16.3 MB (6.05%) - LeetSync

---
+++ spiral-matrix/spiral-matrix.py
@@ -0,0 +1,17 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        m, n = len(matrix), len(matrix[0])
+        answer = []
+        now = (0, 0)
+        nowd = 0
+        d = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        visited = [[False]*n for _ in range(len(matrix))]
+        for i in range(m*n):
+            answer.append(matrix[now[0]][now[1]])
+            visited[now[0]][now[1]] = True
+            if not 0 <= now[0]+ d[nowd][0] < m or not 0 <= now[1]+d[nowd][1] < n:
+                nowd = (nowd+1)%4
+            elif visited[now[0]+ d[nowd][0]][now[1]+d[nowd][1]]:
+                nowd = (nowd+1)%4
+            now = (now[0]+d[nowd][0], now[1]+d[nowd][1])
+        return answer
