76762d06d2a84f770a25bb0e8251d6b8f31b7037
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-05-12 16:22:19 +0900

    Time: 1665 ms (60.08%) | Memory: 62.8 MB (51.85%) - LeetSync

---
+++ solving-questions-with-brainpower/solving-questions-with-brainpower.py
@@ -0,0 +1,10 @@
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        dp = [0]*(len(questions) + 1)
+        for i in reversed(range(len(questions))):
+            point, jump = questions[i]
+            idx = min(i + jump + 1, len(questions))
+            cur = point + dp[idx]
+            dp[i] = max(cur, dp[i+1])
+
+        return dp[0]
