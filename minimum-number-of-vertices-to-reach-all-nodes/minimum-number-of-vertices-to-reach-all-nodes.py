f042c2b391c0f7b435702e14099757901b7e78a7
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-05-18 10:28:30 +0900

    Time: 1188 ms (48.46%) | Memory: 54.9 MB (20.27%) - LeetSync

--- minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.py
+++ minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.py
@@ -1,5 +1,10 @@
 class Solution:
     def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
+        reachable = set([x[1] for x in edges])
+        return list(set(range(n)) - reachable)
+
+class Solution2:
+    def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
         reach = {x : [] for x in range(n)}
         for edge in edges:
             reach[edge[1]].append(edge[0])
