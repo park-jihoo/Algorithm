e9613bafc636b2e43b75ae20c28eb294d7d15685
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-05-18 10:26:21 +0900

    Time: 1276 ms (14.38%) | Memory: 55 MB (20.27%) - LeetSync

--- minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.py
+++ minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.py
@@ -1,9 +1,7 @@
 class Solution:
     def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
-        graph = {x : [] for x in range(n)}
         reach = {x : [] for x in range(n)}
         for edge in edges:
-            graph[edge[0]].append(edge[1])
             reach[edge[1]].append(edge[0])
         #unreachable nodes
         unreachable = [x for x in range(n) if len(reach[x]) == 0]
