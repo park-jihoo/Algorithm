6973095183194ef024b4dc87c7bb1d6fe33bdbc2
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-06-03 10:02:50 +0900

    Time: 1323 ms (60.67%) | Memory: 48.3 MB (47.89%) - LeetSync

---
+++ time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.py
@@ -0,0 +1,22 @@
+from collections import defaultdict, deque
+
+class Solution:
+    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+        answer = 0
+        if n==1:
+            return 0
+        graph = defaultdict(list)
+        for idx, val in enumerate(manager):
+            if val != -1:
+                graph[val].append(idx)
+        q = deque([headID])
+        informed = [False]*n
+        informed[headID] = True
+        while q:
+            node = q.popleft()
+            for child in graph[node]:
+                if not informed[child]:
+                    informed[child] = True
+                    informTime[child] += informTime[node]
+                    q.append(child)
+        return max(informTime)
