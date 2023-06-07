d6814a254f7edc90e20ca4247d85f8eb25f1f153
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-06-07 12:19:23 +0900

    Time: 39 ms (81.54%) | Memory: 16.3 MB (68.57%) - LeetSync

---
+++ minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minFlips(self, a: int, b: int, c: int) -> int:
+        c = bin(c)[2:]
+        a = bin(a)[2:]
+        b = bin(b)[2:]
+        answer = 0
+        maxlen = max(len(a), len(b), len(c))
+        if len(a) < maxlen:
+            a = '0'*(maxlen-len(a)) + a
+        if len(b) < maxlen:
+            b = '0'*(maxlen-len(b)) + b
+        if len(c) < maxlen:
+            c = '0'*(maxlen-len(c)) + c
+        for i, j, k in zip(a, b, c):
+            print(i, j, k)
+            if i == '0' and j == '0' and k == '1':
+                answer +=1
+            elif k == '0' and int(i)+int(j)>0:
+                answer += int(i)+int(j)
+        return answer
