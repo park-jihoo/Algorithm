41a4c5fd276878ebe9e2d1731be93292856c924a
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-06-19 12:45:45 +0900

    Time: 49 ms (62.90%) | Memory: 16.4 MB (26.06%) - LeetSync

--- find-the-highest-altitude/find-the-highest-altitude.py
+++ find-the-highest-altitude/find-the-highest-altitude.py
@@ -1,5 +1,9 @@
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
+        return max(max([sum(gain[:x]) for x in range(len(gain))]), sum(gain))
+
+class Solution2:
+    def largestAltitude(self, gain: List[int]) -> int:
         answer, now = 0, 0
         for g in gain:
             now += g
