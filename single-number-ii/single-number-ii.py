26267c8b47f5e461b09b239460d77c111edcdc51
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-07-04 09:16:08 +0900

    Time: 78 ms (53.41%) | Memory: 18.2 MB (44.80%) - LeetSync

--- single-number-ii/single-number-ii.py
+++ single-number-ii/single-number-ii.py
@@ -1,4 +1,13 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        ones, twos = 0, 0
+        for num in nums:
+            ones ^= num & ~twos
+            twos ^= num & ~ones
+        return ones
+
+
+class Solution2:
+    def singleNumber(self, nums: List[int]) -> int:
         c = Counter(nums)
         return c.most_common()[::-1][0][0]
