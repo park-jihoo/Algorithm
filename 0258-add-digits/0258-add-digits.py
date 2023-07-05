401e62791664ec6f0e84f561565b3302c84d30c6
jihoopark <wlgn8648@gmail.com>
2023-07-06 02:35:21 +0900

    rename folder and file

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -1,7 +1,8 @@
 class Solution:
     def addDigits(self, num: int) -> int:
-        if num < 10:
-            return num
-        strnum = str(num)
-        numlist = [int(x) for x in list(strnum)]
-        return self.addDigits(sum(numlist))
+        # 38 = 3*10 + 8
+        # 38 - 3 - 8 = 3*9
+        # abc = a*100 + b * 10 + c
+        # abc - a - b - c = a*99 + b*9
+        # 결국 9의 배수만큼 빠져나감
+        return 0 if num == 0 else (num-1) % 9 + 1
