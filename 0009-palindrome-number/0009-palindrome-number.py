d556b2b2dfaa9dd2bdb2fb82d377b7053e5dec6f
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-07-06 17:22:43 +0900

    Time: 68 ms (83.95%), Space: 16.4 MB (36.8%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,5 +1,9 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        return str(x)[::-1] == str(x)
+
+class Solution2:
+    def isPalindrome(self, x: int) -> bool:
         if x<0:
             return False
         k = int(str(x)[::-1])
