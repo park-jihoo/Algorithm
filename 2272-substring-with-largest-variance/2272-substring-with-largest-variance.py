d5e300ad7d6ec8241f2eb09129ad3a88e8bb1939
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-07-09 22:03:04 +0900

    Time: 3558 ms (64.96%), Space: 16.5 MB (25.64%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.py
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.py
@@ -1,31 +1,30 @@
 class Solution:
+    def kadane(self, a: str, b: str, s: str) -> int:
+        ans = 0
+        countA = 0
+        countB = 0
+        canExtendPrevB = False
+
+        for c in s:
+            if c != a and c != b:
+                continue
+            if c == a:
+                countA += 1
+            else:
+                countB += 1
+            if countB > 0:
+                ans = max(ans, countA - countB)
+            elif countB == 0 and canExtendPrevB:
+                ans = max(ans, countA - 1)
+
+            if countB > countA:
+                countA = 0
+                countB = 0
+                canExtendPrevB = True
+        return ans
+
     def largestVariance(self, s: str) -> int:
-        #array, dynamic programming (kadane algorithm)
-        def kadane(a: str, b: str) -> int:
-            ans = 0
-            countA = 0
-            countB = 0
-            canExtendPrevB = False
-
-            for c in s:
-                if c != a and c != b:
-                    continue
-                if c == a:
-                    countA += 1
-                else:
-                    countB += 1
-                if countB > 0:
-                    ans = max(ans, countA - countB)
-                elif countB == 0 and canExtendPrevB:
-                    ans = max(ans, countA - 1)
-
-                if countB > countA:
-                    countA = 0
-                    countB = 0
-                    canExtendPrevB = True
-            return ans
-
-        return max(kadane(c1, c2)
+        return max(self.kadane(c1, c2, s)
             for c1 in string.ascii_lowercase
             for c2 in string.ascii_lowercase
             if c1 != c2)
