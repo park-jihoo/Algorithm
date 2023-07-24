ac6e3eaeda36111b7390c9d0f074f6cfd3ef376d
park-jihoo <park-jihoo@users.noreply.github.com>
2023-07-24 00:06:33 +0000

    Code formatting

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,7 +1,7 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if n==0:
+        if n == 0:
             return 1
-        elif n<0:
-            return pow(1/x, -n)
-        return pow(x,n)
+        elif n < 0:
+            return pow(1 / x, -n)
+        return pow(x, n)
