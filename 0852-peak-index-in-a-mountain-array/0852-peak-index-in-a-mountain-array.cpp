2cc041a5f2693ef986eb086f5981440569ba0614
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-07-25 11:26:37 +0900

    Time: 136 ms (21.45%), Space: 59.5 MB (88.25%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        // binary search
+        int start = 0;
+        int end = arr.size()-1;
+        int mid = (start + end) / 2;
+
+        while(mid > 0 and mid < arr.size() - 1){
+            if(arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1])
+                return mid;
+            else if(arr[mid] < arr[mid - 1])
+                end = mid;
+            else
+                start= mid;
+            mid = (start + end) / 2;
+        }
+        return 0;
+    }
+};
