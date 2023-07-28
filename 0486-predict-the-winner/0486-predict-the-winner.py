c9c91b51999afaf3ec98c6b5c5e4c718b0a2d4e0
park-jihoo <park-jihoo@users.noreply.github.com>
2023-07-28 00:47:27 +0000

    Code formatting

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,11 +1,13 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
         n = len(nums)
-        dp = nums[:] # difference between first and second player
-        
+        dp = nums[:]  # difference between first and second player
+
         for x in range(1, n):
             for l in range(n - x):
-                r = l + x 
-                dp[l] = max(nums[l] - dp[l + 1], nums[r] - dp[l])  # 현재 플레이어가 얻을 수 있는 최대 점수
+                r = l + x
+                dp[l] = max(
+                    nums[l] - dp[l + 1], nums[r] - dp[l]
+                )  # 현재 플레이어가 얻을 수 있는 최대 점수
 
-        return dp[0] >= 0 
+        return dp[0] >= 0
