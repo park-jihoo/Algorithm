10966dcc880a31b352c2a19e90d8b00e814ac9a0
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-08-01 10:01:13 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,12 +1,14 @@
 class Solution:
-    def combine(self, n: int, k: int) -> List[List[int]]:
-        def dfs(idx, lst):
-            if len(lst) == k:
-                answer.append(lst[:])
-                return
-            for i in range(idx, n):
-                dfs(i + 1, lst + [i + 1])
+    def dfs(self, idx, lst):
+        if len(lst) == self.k:
+            self.answer.append(lst[:])
+            return
+        for i in range(idx, self.n):
+            self.dfs(i + 1, lst + [i + 1])
 
-        answer = []
-        dfs(0, [])
-        return answer
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        self.answer = []
+        self.n = n
+        self.k = k
+        self.dfs(0, [])
+        return self.answer
