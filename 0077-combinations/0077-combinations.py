eae1f91935217dd02aa6d0bb30983672d925c026
park-jihoo <park-jihoo@users.noreply.github.com>
2023-08-01 01:00:13 +0000

    Code formatting

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,12 +1,14 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         answer = []
-        l = list(range(1,n+1))
+        l = list(range(1, n + 1))
+
         def dfs(idx, lst):
             if len(lst) == k:
                 answer.append(lst[:])
                 return
             for i in range(idx, n):
-                dfs(i+1, lst+[l[i]])
+                dfs(i + 1, lst + [l[i]])
+
         dfs(0, [])
         return answer
