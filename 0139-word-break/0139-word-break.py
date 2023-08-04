382e6356bb788d70d1d0126575c0384a8066ec8b
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-08-04 10:10:43 +0900

    Time: 45 ms (88.3%), Space: 16.3 MB (97.29%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,15 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        q = deque([0])
+        visited = set()
+        while q:
+            start = q.popleft()
+            if start == len(s):
+                return True
+            for end in range(start+1, len(s)+1):
+                if end in visited:
+                    continue
+                if s[start:end] in wordDict:
+                    q.append(end)
+                    visited.add(end)
+        return False
