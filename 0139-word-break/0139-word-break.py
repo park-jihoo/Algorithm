ce439c60eec0451cc1f23e778e593a0609d9f35f
park-jihoo <park-jihoo@users.noreply.github.com>
2023-08-04 01:11:32 +0000

    Code formatting

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -6,7 +6,7 @@ class Solution:
             start = q.popleft()
             if start == len(s):
                 return True
-            for end in range(start+1, len(s)+1):
+            for end in range(start + 1, len(s) + 1):
                 if end in visited:
                     continue
                 if s[start:end] in wordDict:
