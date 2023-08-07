e0e344900fe1c99f9ff93ed905cc72c84c293851
park-jihoo <park-jihoo@users.noreply.github.com>
2023-08-07 03:38:41 +0000

    Code formatting

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int searchInsert(vector<vector<int>>& matrix, int target) {
+  int searchInsert(vector<vector<int>> &matrix, int target) {
     int start = 0;
     int end = matrix.size() - 1;
     while (start <= end) {
@@ -14,21 +14,21 @@ public:
     }
     return start;
   }
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        int row = searchInsert(matrix, target);
-        int left = 0;
-        int right = n;
-        while (left < right){
-            int mid = (left + right) / 2;
-            if(matrix[row][mid] == target)
-                return true;
-            else if(matrix[row][mid] > target)
-                right = mid;
-            else
-                left = mid+1;
-        }
-        return false;
+  bool searchMatrix(vector<vector<int>> &matrix, int target) {
+    int m = matrix.size();
+    int n = matrix[0].size();
+    int row = searchInsert(matrix, target);
+    int left = 0;
+    int right = n;
+    while (left < right) {
+      int mid = (left + right) / 2;
+      if (matrix[row][mid] == target)
+        return true;
+      else if (matrix[row][mid] > target)
+        right = mid;
+      else
+        left = mid + 1;
     }
+    return false;
+  }
 };
