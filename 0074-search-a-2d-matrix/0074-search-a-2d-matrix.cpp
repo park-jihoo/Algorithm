8a00a9d980539b4f8139f18a0d49d44402ded58b
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-08-07 12:38:07 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int searchInsert(vector<vector<int>>& matrix, int target) {
+    int start = 0;
+    int end = matrix.size() - 1;
+    while (start <= end) {
+      int center = (start + end) / 2;
+      if (matrix[center][0] > target)
+        end = center - 1;
+      else if (matrix[center][0] < target)
+        start = center + 1;
+      else
+        return center;
+    }
+    return start;
+  }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int row = searchInsert(matrix, target);
+        int left = 0;
+        int right = n;
+        while (left < right){
+            int mid = (left + right) / 2;
+            if(matrix[row][mid] == target)
+                return true;
+            else if(matrix[row][mid] > target)
+                right = mid;
+            else
+                left = mid+1;
+        }
+        return false;
+    }
+};
