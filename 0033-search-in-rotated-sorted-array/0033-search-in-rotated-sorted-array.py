7fac819082ab9ac795641b0174b1e830c38ed610
park-jihoo <park-jihoo@users.noreply.github.com>
2023-08-08 05:27:31 +0000

    Code formatting

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,12 +1,12 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         start, end = 0, len(nums) - 1
-        while start<=end:
+        while start <= end:
             mid = (start + end) // 2
-            if(nums[mid] == target):
+            if nums[mid] == target:
                 return mid
             if nums[start] <= nums[mid]:
-                if target >=nums[start] and target < nums[mid]:
+                if target >= nums[start] and target < nums[mid]:
                     end = mid - 1
                 else:
                     start = mid + 1
