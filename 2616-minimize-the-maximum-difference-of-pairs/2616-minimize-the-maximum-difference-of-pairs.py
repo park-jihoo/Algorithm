24c7020dc35f2f3faa4eded002f364c9c21f1dba
park-jihoo <park-jihoo@users.noreply.github.com>
2023-08-09 06:20:11 +0000

    Code formatting

--- 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.py
+++ 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.py
@@ -8,11 +8,11 @@ class Solution:
                 idx += 1
             idx += 1
         return cnt
-        
+
     def minimizeMax(self, nums: List[int], p: int) -> int:
         n = len(nums)
         nums.sort()
-        
+
         left, right = 0, nums[-1] - nums[0]
         while left < right:
             mid = (left + right) // 2
@@ -21,4 +21,3 @@ class Solution:
             else:
                 left = mid + 1
         return left
-    
