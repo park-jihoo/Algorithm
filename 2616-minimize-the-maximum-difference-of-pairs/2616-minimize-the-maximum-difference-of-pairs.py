76909b8d67adaa965428b62e77ea24732487ea61
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-08-09 15:19:22 +0900

    Time: 939 ms (90.97%), Space: 30.9 MB (96.13%) - LeetHub

---
+++ 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.py
@@ -0,0 +1,24 @@
+class Solution:
+    def countPairs(self, nums, difference):
+        n = len(nums)
+        idx, cnt = 0, 0
+        while idx < n - 1:
+            if nums[idx + 1] - nums[idx] <= difference:
+                cnt += 1
+                idx += 1
+            idx += 1
+        return cnt
+        
+    def minimizeMax(self, nums: List[int], p: int) -> int:
+        n = len(nums)
+        nums.sort()
+        
+        left, right = 0, nums[-1] - nums[0]
+        while left < right:
+            mid = (left + right) // 2
+            if self.countPairs(nums, mid) >= p:
+                right = mid
+            else:
+                left = mid + 1
+        return left
+    
