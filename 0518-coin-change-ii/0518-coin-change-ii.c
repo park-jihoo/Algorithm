d34765c887d394aafd3357a5417ba2fe60c49ba3
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-08-11 09:20:47 +0900

    Time: 2 ms (95.83%), Space: 5.8 MB (52.08%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.c
@@ -0,0 +1,13 @@
+int change(int amount, int* coins, int coinsSize){
+    int dp[amount + 1];
+    memset(dp, 0, sizeof(dp));
+    dp[0] = 1;
+    
+    for (int i = 0; i < coinsSize; i++) {
+        for (int j = coins[i]; j <= amount; j++) {
+            dp[j] += dp[j - coins[i]];
+        }
+    }
+    
+    return dp[amount];
+}
