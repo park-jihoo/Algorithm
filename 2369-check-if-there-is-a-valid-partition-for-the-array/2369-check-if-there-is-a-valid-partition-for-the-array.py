0050c5619930370c3ed61cf74429ba53930e4c2d
park-jihoo <park-jihoo@users.noreply.github.com>
2023-08-13 03:29:27 +0000

    Code formatting

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
@@ -5,14 +5,14 @@ class Solution:
         elif len(nums) == 3:
             if nums[0] == nums[1] and nums[1] == nums[2]:
                 return True
-            elif nums[0]+1 == nums[1] and nums[1]+1 == nums[2]:
+            elif nums[0] + 1 == nums[1] and nums[1] + 1 == nums[2]:
                 return True
         return False
-            
+
     def validPartition(self, nums: List[int]) -> bool:
         n = len(nums)
         dp = [False] * (n + 1)
-        dp[0] = True  
+        dp[0] = True
         for end in range(1, n + 1):
             for start in range(end - 3, end):
                 if start < 0:
