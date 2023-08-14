776698ed9b294122538bbafb53e30b8736f93557
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-08-15 01:24:42 +0900

    [level 3] Title: 순위, Time: 40.63 ms, Memory: 11.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/49191. 순위/순위.py
@@ -0,0 +1,21 @@
+def solution(n, results):
+    answer = 0
+    graph1 = {}
+    graph2 = {}
+    for i in range (n):
+        graph1[i+1]=set()
+        graph2[i+1]=set()
+    for i in range(1, n+1):
+        for result in results:
+            if result[0]==i:
+                graph1[i].add(result[1])
+            if result[1]==i:
+                graph2[i].add(result[0])
+        for winner in graph2[i]:
+            graph1[winner].update(graph1[i])
+        for loser in graph1[i]:
+            graph2[loser].update(graph2[i])
+    for i in range(1, n+1):
+        if len(graph1[i])+len(graph2[i]) == n-1:
+            answer+=1
+    return answer
