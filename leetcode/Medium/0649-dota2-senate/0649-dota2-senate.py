cf00bb04e7563172e242b75110e323a31364a6ca
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-08-14 18:23:58 +0900

    Time: 49 ms (96.19%), Space: 16.8 MB (36.48%) - LeetHub

---
+++ leetcode/Medium/0649-dota2-senate/0649-dota2-senate.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        n = len(senate)
+        dq, rq = deque([]), deque([])
+        for idx, s in enumerate(list(senate)):
+            if s == "R":
+                rq.append(idx)
+            else:
+                dq.append(idx)
+        while rq and dq:
+            ri, di = rq.popleft(), dq.popleft()
+            if ri < di:
+                rq.append(ri+n)
+            else:
+                dq.append(di+n)
+        return "Radiant" if len(rq) > len(dq) else "Dire"
