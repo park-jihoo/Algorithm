3470c8e4e034004967e4f9b9a750a0f48945f4b3
park-jihoo <park-jihoo@users.noreply.github.com>
2023-08-14 05:14:14 +0000

    Code formatting

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,72 +1,74 @@
+#include <algorithm>
 #include <iostream>
-#include <vector>
 #include <map>
 #include <queue>
-#include <algorithm>
+#include <vector>
 
 using namespace std;
 
-vector<int> dfs(const map<int, vector<int>>& board, int v) {
-    vector<int> visited, need_visited;
-    need_visited.push_back(v);
-    while (!need_visited.empty()) {
-        int node = need_visited.back();
-        need_visited.pop_back();
-        if (find(visited.begin(), visited.end(), node) == visited.end()) {
-            visited.push_back(node);
-            if (board.count(node)) {
-                vector<int> neighbors = board.at(node);
-                sort(neighbors.rbegin(), neighbors.rend());
-                need_visited.insert(need_visited.end(), neighbors.begin(), neighbors.end());
-            }
-        }
+vector<int> dfs(const map<int, vector<int>> &board, int v) {
+  vector<int> visited, need_visited;
+  need_visited.push_back(v);
+  while (!need_visited.empty()) {
+    int node = need_visited.back();
+    need_visited.pop_back();
+    if (find(visited.begin(), visited.end(), node) == visited.end()) {
+      visited.push_back(node);
+      if (board.count(node)) {
+        vector<int> neighbors = board.at(node);
+        sort(neighbors.rbegin(), neighbors.rend());
+        need_visited.insert(need_visited.end(), neighbors.begin(),
+                            neighbors.end());
+      }
     }
-    return visited;
+  }
+  return visited;
 }
 
-vector<int> bfs(const map<int, vector<int>>& board, int v) {
-    vector<int> visited, need_visited;
-    need_visited.push_back(v);
-    while (!need_visited.empty()) {
-        int node = need_visited.front();
-        need_visited.erase(need_visited.begin());
-        if (find(visited.begin(), visited.end(), node) == visited.end()) {
-            visited.push_back(node);
-            if (board.count(node)) {
-                vector<int> neighbors = board.at(node);
-                sort(neighbors.begin(), neighbors.end());
-                need_visited.insert(need_visited.end(), neighbors.begin(), neighbors.end());
-            }
-        }
+vector<int> bfs(const map<int, vector<int>> &board, int v) {
+  vector<int> visited, need_visited;
+  need_visited.push_back(v);
+  while (!need_visited.empty()) {
+    int node = need_visited.front();
+    need_visited.erase(need_visited.begin());
+    if (find(visited.begin(), visited.end(), node) == visited.end()) {
+      visited.push_back(node);
+      if (board.count(node)) {
+        vector<int> neighbors = board.at(node);
+        sort(neighbors.begin(), neighbors.end());
+        need_visited.insert(need_visited.end(), neighbors.begin(),
+                            neighbors.end());
+      }
     }
-    return visited;
+  }
+  return visited;
 }
 
 int main() {
-    int n, m, v;
-    cin >> n >> m >> v;
+  int n, m, v;
+  cin >> n >> m >> v;
 
-    map<int, vector<int>> board;
+  map<int, vector<int>> board;
 
-    for (int i = 0; i < m; ++i) {
-        int x, y;
-        cin >> x >> y;
-        board[x].push_back(y);
-        board[y].push_back(x);
-    }
+  for (int i = 0; i < m; ++i) {
+    int x, y;
+    cin >> x >> y;
+    board[x].push_back(y);
+    board[y].push_back(x);
+  }
 
-    vector<int> dfsResult = dfs(board, v);
-    vector<int> bfsResult = bfs(board, v);
+  vector<int> dfsResult = dfs(board, v);
+  vector<int> bfsResult = bfs(board, v);
 
-    for (int i : dfsResult) {
-        cout << i << " ";
-    }
-    cout << endl;
+  for (int i : dfsResult) {
+    cout << i << " ";
+  }
+  cout << endl;
 
-    for (int i : bfsResult) {
-        cout << i << " ";
-    }
-    cout << endl;
+  for (int i : bfsResult) {
+    cout << i << " ";
+  }
+  cout << endl;
 
-    return 0;
+  return 0;
 }
