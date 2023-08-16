9ee176922e89d20562a4b5fbd7e97a4df7e9ec04
park-jihoo <park-jihoo@users.noreply.github.com>
2023-08-16 00:36:57 +0000

    Code formatting

--- leetcode/Hard/0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ leetcode/Hard/0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> q;  
-        vector<int> ans;  
+  vector<int> maxSlidingWindow(vector<int> &nums, int k) {
+    deque<int> q;
+    vector<int> ans;
 
-        for (int i = 0; i < nums.size(); ++i) {
-            cleanDeque(q, nums, i, k);
-            q.push_back(i);
-            if (i >= k - 1) {
-                ans.push_back(nums[q.front()]);
-            }
-        }
-
-        return ans;
+    for (int i = 0; i < nums.size(); ++i) {
+      cleanDeque(q, nums, i, k);
+      q.push_back(i);
+      if (i >= k - 1) {
+        ans.push_back(nums[q.front()]);
+      }
     }
 
-    void cleanDeque(deque<int>& q, vector<int>& nums, int i, int k) {
-        if (!q.empty() && q.front() == i - k) {
-            q.pop_front();
-        }
-        while (!q.empty() && nums[i] >= nums[q.back()]) {
-            q.pop_back();
-        }
+    return ans;
+  }
+
+  void cleanDeque(deque<int> &q, vector<int> &nums, int i, int k) {
+    if (!q.empty() && q.front() == i - k) {
+      q.pop_front();
     }
+    while (!q.empty() && nums[i] >= nums[q.back()]) {
+      q.pop_back();
+    }
+  }
 };
