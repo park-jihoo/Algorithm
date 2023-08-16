6ed8d6bf3f0c81f546b408e3dd93235c8aa29429
park-jihoo <park-jihoo@users.noreply.github.com>
2023-08-16 00:35:20 +0000

    Code formatting

--- leetcode/Hard/0239-sliding-window-maximum/0239-sliding-window-maximum.c
+++ leetcode/Hard/0239-sliding-window-maximum/0239-sliding-window-maximum.c
@@ -1,35 +1,35 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* maxSlidingWindow(int* nums, int numsSize, int k, int* returnSize) {
-    int* res = (int*)malloc((numsSize - k + 1) * sizeof(int));
-    int resIndex = 0;
-    int* window = (int*)malloc(k * sizeof(int));
-    int windowIndex = 0;
+int *maxSlidingWindow(int *nums, int numsSize, int k, int *returnSize) {
+  int *res = (int *)malloc((numsSize - k + 1) * sizeof(int));
+  int resIndex = 0;
+  int *window = (int *)malloc(k * sizeof(int));
+  int windowIndex = 0;
 
-    for (int i = 0; i < k; i++) {
-        while (windowIndex > 0 && nums[i] >= nums[window[windowIndex - 1]]) {
-            windowIndex--;
-        }
-        window[windowIndex++] = i;
+  for (int i = 0; i < k; i++) {
+    while (windowIndex > 0 && nums[i] >= nums[window[windowIndex - 1]]) {
+      windowIndex--;
     }
+    window[windowIndex++] = i;
+  }
+  res[resIndex++] = nums[window[0]];
+
+  for (int i = k; i < numsSize; i++) {
+    if (window[0] == i - k) {
+      for (int j = 1; j < windowIndex; j++) {
+        window[j - 1] = window[j];
+      }
+      windowIndex--;
+    }
+    while (windowIndex > 0 && nums[i] >= nums[window[windowIndex - 1]]) {
+      windowIndex--;
+    }
+    window[windowIndex++] = i;
     res[resIndex++] = nums[window[0]];
+  }
 
-    for (int i = k; i < numsSize; i++) {
-        if (window[0] == i - k) {
-            for (int j = 1; j < windowIndex; j++) {
-                window[j - 1] = window[j];
-            }
-            windowIndex--;
-        }
-        while (windowIndex > 0 && nums[i] >= nums[window[windowIndex - 1]]) {
-            windowIndex--;
-        }
-        window[windowIndex++] = i;
-        res[resIndex++] = nums[window[0]];
-    }
-
-    free(window);
-    *returnSize = numsSize - k + 1;
-    return res;
+  free(window);
+  *returnSize = numsSize - k + 1;
+  return res;
 }
