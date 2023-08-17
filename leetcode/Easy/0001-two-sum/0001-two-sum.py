a3e96f12fc4c4480dad62959a4446018cf83cb82
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-08-17 23:15:18 +0900

    Time: 62 ms (90.76%), Space: 17.9 MB (5.72%) - LeetHub

---
+++ leetcode/Easy/0001-two-sum/0001-two-sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        vals = {}
+        for i in range(len(nums)):
+            if nums[i] in vals:
+                return [vals[nums[i]], i]
+            else:
+                vals[target-nums[i]]=i
