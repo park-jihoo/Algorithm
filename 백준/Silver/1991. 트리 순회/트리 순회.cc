8e9818d72f38075b98f6e8cba2ff2f1ef05ff1a2
park-jihoo <park-jihoo@users.noreply.github.com>
2023-08-17 15:55:43 +0000

    Code formatting

--- 백준/Silver/1991. 트리 순회/트리 순회.cc
+++ 백준/Silver/1991. 트리 순회/트리 순회.cc
@@ -1,46 +1,49 @@
 #include <iostream>
-#include <vector>
 #include <map>
+#include <vector>
 
 using namespace std;
 
-map<char, pair<char, char> > adj;
+map<char, pair<char, char>> adj;
 
-void preorder(char root){
-    if(root == '.') return;
-    cout << root;
-    preorder(adj[root].first);
-    preorder(adj[root].second);
+void preorder(char root) {
+  if (root == '.')
+    return;
+  cout << root;
+  preorder(adj[root].first);
+  preorder(adj[root].second);
 }
 
-void inorder(char root){
-    if(root=='.') return;
-    inorder(adj[root].first);
-    cout << root;
-    inorder(adj[root].second);
+void inorder(char root) {
+  if (root == '.')
+    return;
+  inorder(adj[root].first);
+  cout << root;
+  inorder(adj[root].second);
 }
 
-void postorder(char root){
-    if(root=='.') return;
-    postorder(adj[root].first);
-    postorder(adj[root].second);
-    cout << root;
+void postorder(char root) {
+  if (root == '.')
+    return;
+  postorder(adj[root].first);
+  postorder(adj[root].second);
+  cout << root;
 }
 
-int main(){
-    int n;
-    cin >> n;
-    for(int i=0;i<n;i++){
-        char node, left, right;
-        cin>>node>>left>>right;
-        adj[node] = make_pair(left, right);
-    }
+int main() {
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    char node, left, right;
+    cin >> node >> left >> right;
+    adj[node] = make_pair(left, right);
+  }
 
-    preorder('A');
-    cout << endl;
-    inorder('A');
-    cout << endl;
-    postorder('A');
-    cout << endl;
-    return 0;
+  preorder('A');
+  cout << endl;
+  inorder('A');
+  cout << endl;
+  postorder('A');
+  cout << endl;
+  return 0;
 }
