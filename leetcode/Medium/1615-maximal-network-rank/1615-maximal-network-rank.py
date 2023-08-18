96a0947234ddb22f958821430dd7f5ce376867f9
park-jihoo <park-jihoo@users.noreply.github.com>
2023-08-18 04:43:05 +0000

    Code formatting

--- leetcode/Medium/1615-maximal-network-rank/1615-maximal-network-rank.py
+++ leetcode/Medium/1615-maximal-network-rank/1615-maximal-network-rank.py
@@ -6,7 +6,7 @@ class Solution:
             graph[end].append(start)
         answer = 0
         for i in range(n):
-            for j in range(i+1, n):
+            for j in range(i + 1, n):
                 iset = set([(i, x) for x in graph[i]])
                 jset = set([(j, x) for x in graph[j] if x != i])
                 answer = max(len(iset) + len(jset), answer)
