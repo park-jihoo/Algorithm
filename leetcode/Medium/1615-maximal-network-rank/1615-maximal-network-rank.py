f47b32c93d68c0f8ff96dbf33272b4c0d2bdc700
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-08-18 13:42:25 +0900

    Time: 1059 ms (5.07%), Space: 17.9 MB (83.19%) - LeetHub

---
+++ leetcode/Medium/1615-maximal-network-rank/1615-maximal-network-rank.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maximalNetworkRank(self, n: int, roads: List[List[int]]) -> int:
+        graph = defaultdict(list)
+        for start, end in roads:
+            graph[start].append(end)
+            graph[end].append(start)
+        answer = 0
+        for i in range(n):
+            for j in range(i+1, n):
+                iset = set([(i, x) for x in graph[i]])
+                jset = set([(j, x) for x in graph[j] if x != i])
+                answer = max(len(iset) + len(jset), answer)
+        return answer
