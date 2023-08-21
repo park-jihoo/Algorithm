c3496c6dd939626d93f8d35f8dc69b615d14b1bd
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-08-21 15:38:24 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0;
+    sort(citations.begin(), citations.end());
+    for(answer=0;answer<citations.size();answer++){
+        if(citations[citations.size()-answer]<answer)
+                return answer-1;
+    }
+}
