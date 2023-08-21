4ca33aed2e32610e7fd492698b949cbc5db03e05
park-jihoo <park-jihoo@users.noreply.github.com>
2023-08-21 10:49:04 +0000

    Code formatting

--- leetcode/Medium/0049-group-anagrams/0049-group-anagrams.cpp
+++ leetcode/Medium/0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> anagramGroups;
-        
-        for (const auto& s : strs) {
-            vector<int> charCount(26, 0);  
-            for (auto c : s) {
-                charCount[c - 'a']++;
-            }
-            
-            string sRepresentation;
-            for (auto count : charCount) {
-                sRepresentation += to_string(count) + '#';
-            }
-            
-            if (anagramGroups.find(sRepresentation) != anagramGroups.end()) {
-                anagramGroups[sRepresentation].push_back(s);
-            } else {
-                anagramGroups[sRepresentation] = {s};
-            }
-        }
-        
-        vector<vector<string>> result;
-        for (const auto& kv : anagramGroups) {
-            result.push_back(kv.second);
-        }
-        
-        return result;
+  vector<vector<string>> groupAnagrams(vector<string> &strs) {
+    unordered_map<string, vector<string>> anagramGroups;
+
+    for (const auto &s : strs) {
+      vector<int> charCount(26, 0);
+      for (auto c : s) {
+        charCount[c - 'a']++;
+      }
+
+      string sRepresentation;
+      for (auto count : charCount) {
+        sRepresentation += to_string(count) + '#';
+      }
+
+      if (anagramGroups.find(sRepresentation) != anagramGroups.end()) {
+        anagramGroups[sRepresentation].push_back(s);
+      } else {
+        anagramGroups[sRepresentation] = {s};
+      }
     }
+
+    vector<vector<string>> result;
+    for (const auto &kv : anagramGroups) {
+      result.push_back(kv.second);
+    }
+
+    return result;
+  }
 };
