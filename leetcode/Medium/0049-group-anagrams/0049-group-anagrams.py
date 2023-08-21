b245a8f09a6d8bf77a05dba4582e3d2f11a4091f
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-08-21 19:41:18 +0900

    Time: 88 ms (97.63%), Space: 19.6 MB (84.89%) - LeetHub

---
+++ leetcode/Medium/0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_groups = {}
+        for s in strs:
+            s_sorted = ''.join(sorted(s)) 
+            if s_sorted in anagram_groups:
+                anagram_groups[s_sorted].append(s)
+            else:
+                anagram_groups[s_sorted] = [s]
+        return list(anagram_groups.values())
