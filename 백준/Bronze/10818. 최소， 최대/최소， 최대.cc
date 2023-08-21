ae656dca71d424617113c4d13f839e4ac87fd74d
park-jihoo <park-jihoo@users.noreply.github.com>
2023-08-21 16:08:32 +0000

    Code formatting

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -2,21 +2,21 @@
 
 using namespace std;
 
-int main(){
-    int n;
-    cin >> n;
-    int min = 1000001;
-    int max = -10000001;
-    for(int i=0;i<n;i++){
-        int x;
-        cin >> x;
-        if(x>max){
-            max = x;
-        }
-        if(x<min){
-            min = x;
-        }
+int main() {
+  int n;
+  cin >> n;
+  int min = 1000001;
+  int max = -10000001;
+  for (int i = 0; i < n; i++) {
+    int x;
+    cin >> x;
+    if (x > max) {
+      max = x;
     }
+    if (x < min) {
+      min = x;
+    }
+  }
 
-    cout << min << " " << max << endl;
+  cout << min << " " << max << endl;
 }
