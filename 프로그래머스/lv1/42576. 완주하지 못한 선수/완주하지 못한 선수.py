d9b853834c9139bf849a6baa4dc3299ea83d9fb5
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-08-21 15:31:28 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 68.50 ms, Memory: 26.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,8 @@
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    for part, comp in zip(participant, completion):
+        if(part!=comp):
+            return part
+        
+    return participant[-1]
