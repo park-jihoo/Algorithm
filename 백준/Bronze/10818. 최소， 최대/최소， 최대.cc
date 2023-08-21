1455ae822599357eb542cce851c9c5eddd3ab0ba
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-08-22 01:08:01 +0900

    [Bronze III] Title: 최소, 최대, Time: 452 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int min = 1000001;
+    int max = -10000001;
+    for(int i=0;i<n;i++){
+        int x;
+        cin >> x;
+        if(x>max){
+            max = x;
+        }
+        if(x<min){
+            min = x;
+        }
+    }
+
+    cout << min << " " << max << endl;
+}
