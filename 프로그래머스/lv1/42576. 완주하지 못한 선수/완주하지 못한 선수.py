93e0383b4d36e993daf62bea0eabeb7541aa5569
park-jihoo <park-jihoo@users.noreply.github.com>
2023-08-21 06:32:09 +0000

    Code formatting

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -2,7 +2,7 @@ def solution(participant, completion):
     participant.sort()
     completion.sort()
     for part, comp in zip(participant, completion):
-        if(part!=comp):
+        if part != comp:
             return part
-        
+
     return participant[-1]
