e3638aaea1fa14a431323e20c5453f72de3375a3
park-jihoo <park-jihoo@users.noreply.github.com>
2023-08-21 06:37:25 +0000

    Code formatting

--- 프로그래머스/lv3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.cpp
@@ -1,23 +1,25 @@
 #include <string>
 #include <vector>
 using namespace std;
-bool visited[200]={0, };
-void dfs(int x, vector<vector<int>>computers){
-    for(int i=0;i<computers.size();i++){
-        if(computers[i][x] && !visited[i]){
-            visited[i]=true;
-            dfs(i, computers);
-        }
+bool visited[200] = {
+    0,
+};
+void dfs(int x, vector<vector<int>> computers) {
+  for (int i = 0; i < computers.size(); i++) {
+    if (computers[i][x] && !visited[i]) {
+      visited[i] = true;
+      dfs(i, computers);
     }
+  }
 }
 int solution(int n, vector<vector<int>> computers) {
-    int answer = 0;
-    for(int i=0;i<n;i++){
-        if(!visited[i]){
-            visited[i]=true;
-            dfs(i, computers);
-            answer++;
-        }
+  int answer = 0;
+  for (int i = 0; i < n; i++) {
+    if (!visited[i]) {
+      visited[i] = true;
+      dfs(i, computers);
+      answer++;
     }
-    return answer;
+  }
+  return answer;
 }
--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.cpp
@@ -1,43 +1,48 @@
+#include <algorithm>
+#include <queue>
 #include <string>
 #include <vector>
-#include <queue>
-#include <algorithm>
 
 using namespace std;
 bool visited[50];
 string mybeg, mytar;
-int answer=100;
-bool com(string a, string b){
-    int cnt=0;
-    for(int i=0;i<a.size();i++){
-        if(a[i]!=b[i])cnt++;
-        if(cnt==2)return false;
-    }
-    if(cnt==1)return true;
-    return false;
+int answer = 100;
+bool com(string a, string b) {
+  int cnt = 0;
+  for (int i = 0; i < a.size(); i++) {
+    if (a[i] != b[i])
+      cnt++;
+    if (cnt == 2)
+      return false;
+  }
+  if (cnt == 1)
+    return true;
+  return false;
 }
-void dfs(string begin, string target, vector<string>words, int cnt){
-    if(begin==target){
-        if(answer>cnt)
-            answer=cnt;
-        return;
+void dfs(string begin, string target, vector<string> words, int cnt) {
+  if (begin == target) {
+    if (answer > cnt)
+      answer = cnt;
+    return;
+  }
+  for (int i = 0; i < words.size(); i++) {
+    if (!visited[i] && com(begin, words[i])) {
+      visited[i] = true;
+      dfs(words[i], target, words, cnt + 1);
+      visited[i] = false;
     }
-    for(int i=0;i<words.size();i++){
-        if(!visited[i] && com(begin, words[i])){
-            visited[i]=true;
-            dfs(words[i],target, words, cnt+1);
-            visited[i]=false;
-        }
-    }
+  }
 }
 int solution(string begin, string target, vector<string> words) {
-    mybeg=begin;
-    mytar=target;
-    bool existence=false;
-    for(int i=0;i<words.size();i++){
-        if(target==words[i]) existence=true;
-    }
-    if(!existence) return 0;
-    dfs(begin, target, words, 0);
-    return answer;
+  mybeg = begin;
+  mytar = target;
+  bool existence = false;
+  for (int i = 0; i < words.size(); i++) {
+    if (target == words[i])
+      existence = true;
+  }
+  if (!existence)
+    return 0;
+  dfs(begin, target, words, 0);
+  return answer;
 }
