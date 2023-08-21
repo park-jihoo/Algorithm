ce92a193bc828ee707a274b8bb7086671d887d1f
park-jihoo <park-jihoo@users.noreply.github.com>
2023-08-21 06:39:09 +0000

    Code formatting

--- 프로그래머스/lv2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/lv2/42747. H－Index/H－Index.cpp
@@ -1,14 +1,14 @@
+#include <algorithm>
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int solution(vector<int> citations) {
-    int answer = 0;
-    sort(citations.begin(), citations.end());
-    for(answer=0;answer<citations.size();answer++){
-        if(citations[citations.size()-answer]<answer)
-                return answer-1;
-    }
+  int answer = 0;
+  sort(citations.begin(), citations.end());
+  for (answer = 0; answer < citations.size(); answer++) {
+    if (citations[citations.size() - answer] < answer)
+      return answer - 1;
+  }
 }
