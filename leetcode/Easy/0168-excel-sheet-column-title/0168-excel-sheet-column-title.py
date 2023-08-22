e919f83d1a03110c40e6ae67ab36338ee34810d5
park-jihoo <park-jihoo@users.noreply.github.com>
2023-08-22 04:18:40 +0000

    Code formatting

--- leetcode/Easy/0168-excel-sheet-column-title/0168-excel-sheet-column-title.py
+++ leetcode/Easy/0168-excel-sheet-column-title/0168-excel-sheet-column-title.py
@@ -2,7 +2,7 @@ class Solution:
     def convertToTitle(self, columnNumber: int) -> str:
         answer = ""
         while columnNumber > 0:
-            columnNumber -=1
-            answer = answer + chr(ord('A')+columnNumber % 26)
+            columnNumber -= 1
+            answer = answer + chr(ord("A") + columnNumber % 26)
             columnNumber //= 26
-        return ''.join(reversed(list(answer)))
+        return "".join(reversed(list(answer)))
