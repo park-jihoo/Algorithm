5bde2cece153300d7883476338be08b37f637c8a
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-08-24 20:14:38 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5984 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string a, b;
+    cin>>a;
+    cin>>b;
+    int m = a.size();
+    int n = b.size();
+    vector<vector<int> >dp(m+1, vector<int>(n+1, 0));
+    for(int i=1;i<m+1;i++){
+        for(int j=1;j<n+1;j++){
+            if(a[i-1] == b[j-1])
+                dp[i][j] = dp[i-1][j-1]+1;
+            else if(dp[i][j-1] > dp[i-1][j])
+                dp[i][j] = dp[i][j-1];
+            else
+                dp[i][j] = dp[i-1][j];
+        }
+    }
+
+    cout << dp[m][n] << endl;
+
+}
