3b6578c484e154deb4614de91673fc6ce5d20018
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-08-24 20:21:13 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.86 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
@@ -0,0 +1,17 @@
+def calc(priority, n, expression):
+    if n==2:
+        return str(eval(expression))
+    if priority[n]=='*':
+        return str(eval('*'.join([calc(priority, n+1, e) for e in expression.split('*')])))
+    if priority[n]=='+':
+        return str(eval('+'.join([calc(priority, n+1, e) for e in expression.split('+')])))
+    if priority[n]=='-':
+        return str(eval('-'.join([calc(priority, n+1, e) for e in expression.split('-')])))      
+
+def solution(expression):
+    answer = 0
+    priority = [('*', '-', '+'), ('*', '+', '-'),('+','-','*'),('+','*','-'),('-','+','*'),('-','*','+')]
+    for prior in priority:
+        res = int(calc(prior, 0 ,expression))
+        answer = max(answer, abs(res))
+    return answer
