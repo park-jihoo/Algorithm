6e2ad84ceb35bf87f6ae5ff3ec04be4daa5829c2
park-jihoo <park-jihoo@users.noreply.github.com>
2023-08-27 12:29:28 +0000

    Code formatting

--- leetcode/Easy/0035-search-insert-position/0035-search-insert-position.c
+++ leetcode/Easy/0035-search-insert-position/0035-search-insert-position.c
@@ -1,14 +1,14 @@
-int searchInsert(int* nums, int numsSize, int target){
-    int start = 0;
-    int end = numsSize - 1;
-    while (start <= end) {
-      int center = (start + end) / 2;
-      if (nums[center] > target)
-        end = center - 1;
-      else if (nums[center] < target)
-        start = center + 1;
-      else
-        return center;
-    }
-    return start;
+int searchInsert(int *nums, int numsSize, int target) {
+  int start = 0;
+  int end = numsSize - 1;
+  while (start <= end) {
+    int center = (start + end) / 2;
+    if (nums[center] > target)
+      end = center - 1;
+    else if (nums[center] < target)
+      start = center + 1;
+    else
+      return center;
+  }
+  return start;
 }
