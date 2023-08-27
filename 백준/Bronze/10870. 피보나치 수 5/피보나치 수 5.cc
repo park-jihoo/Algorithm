1d48b20f364b2a6f0716494353a7137814c29e9d
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-08-27 17:26:47 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int n;
+int f(int n) {
+	if (n == 1)
+		return 1;
+	if (n == 0)
+		return 0;
+	else
+		return f(n - 1) + f(n - 2);
+}
+int main() {
+	cin >> n;
+	cout << f(n);
+	return 0;
+}
