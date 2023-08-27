ae21ba014bf05691958d264a03b0bba99beb4da8
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-08-27 17:15:38 +0900

    [level 2] Title: 구명보트, Time: 12.68 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -0,0 +1,14 @@
+def solution(people, limit):
+    people.sort()
+    length=len(people)
+    light=0
+    heavy=length-1
+    cnt=0
+    while(light<heavy):
+        if people[light]+people[heavy]<=limit:
+            cnt+=1
+            light+=1
+            heavy-=1
+        else:
+            heavy-=1
+    return length-cnt
