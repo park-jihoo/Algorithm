654039950e18dde86a86988f02bdf5ac8ee4ef51
park-jihoo <park-jihoo@users.noreply.github.com>
2023-08-27 08:22:52 +0000

    Code formatting

--- 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.py
+++ 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.py
@@ -1,8 +1,8 @@
 def solution(n, s):
-    if n>s:
+    if n > s:
         return [-1]
-    [portion, remain]=divmod(s,n)
-    answer=[portion]*n
+    [portion, remain] = divmod(s, n)
+    answer = [portion] * n
     for i in range(remain):
-        answer[i]+=1
+        answer[i] += 1
     return sorted(answer)
--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.cpp
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.cpp
@@ -1,29 +1,33 @@
-// 문제가 개편되었습니다. 이로 인해 함수 구성이나 테스트케이스가 변경되어, 과거의 코드는 동작하지 않을 수 있습니다.
-// 새로운 함수 구성을 적용하려면 [코드 초기화] 버튼을 누르세요. 단, [코드 초기화] 버튼을 누르면 작성 중인 코드는 사라집니다.
+// 문제가 개편되었습니다. 이로 인해 함수 구성이나 테스트케이스가 변경되어,
+// 과거의 코드는 동작하지 않을 수 있습니다. 새로운 함수 구성을 적용하려면 [코드
+// 초기화] 버튼을 누르세요. 단, [코드 초기화] 버튼을 누르면 작성 중인 코드는
+// 사라집니다.
 #include <string>
 #include <vector>
 
 using namespace std;
 int answer = 9;
-void dfs(int N, int number, int count, int currentnumber){
-    if(count>=9){
-        return;
-    }
-    if(currentnumber==number){
-        if(answer>count) answer=count;
-        return;
-    }
-    int tempnumber=0;
-    for(int i=0;i+count<9;i++){
-        tempnumber=tempnumber*10+N;
-        dfs(N, number, count+1+i, currentnumber+tempnumber);
-        dfs(N, number, count+1+i, currentnumber-tempnumber);
-        dfs(N, number, count+1+i, currentnumber*tempnumber);
-        dfs(N, number, count+1+i, currentnumber/tempnumber);
-    }
+void dfs(int N, int number, int count, int currentnumber) {
+  if (count >= 9) {
+    return;
+  }
+  if (currentnumber == number) {
+    if (answer > count)
+      answer = count;
+    return;
+  }
+  int tempnumber = 0;
+  for (int i = 0; i + count < 9; i++) {
+    tempnumber = tempnumber * 10 + N;
+    dfs(N, number, count + 1 + i, currentnumber + tempnumber);
+    dfs(N, number, count + 1 + i, currentnumber - tempnumber);
+    dfs(N, number, count + 1 + i, currentnumber * tempnumber);
+    dfs(N, number, count + 1 + i, currentnumber / tempnumber);
+  }
 }
 int solution(int N, int number) {
-    dfs(N, number, 0, 0);
-    if(answer>=9)answer=-1;
-    return answer;
+  dfs(N, number, 0, 0);
+  if (answer >= 9)
+    answer = -1;
+  return answer;
 }
