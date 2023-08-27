2860935f41ef9b89b53dd4a69d772436e7aa724e
park-jihoo <park-jihoo@users.noreply.github.com>
2023-08-27 08:21:03 +0000

    Code formatting

--- 프로그래머스/lv1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/lv1/42748. K번째수/K번째수.cpp
@@ -1,18 +1,18 @@
+#include <algorithm>
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
-    vector<int> answer;
-    for(int i=0;i<commands.size();i++){
-        vector<int>newarr;
-        for(int j=commands[i][0];j<=commands[i][1];j++){
-            newarr.push_back(array[j-1]);
-        }
-        sort(newarr.begin(), newarr.end());
-        answer.push_back(newarr[commands[i][2]-1]);
+  vector<int> answer;
+  for (int i = 0; i < commands.size(); i++) {
+    vector<int> newarr;
+    for (int j = commands[i][0]; j <= commands[i][1]; j++) {
+      newarr.push_back(array[j - 1]);
     }
-    return answer;
+    sort(newarr.begin(), newarr.end());
+    answer.push_back(newarr[commands[i][2] - 1]);
+  }
+  return answer;
 }
--- 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -1,13 +1,14 @@
 from itertools import combinations
 from collections import Counter
 
+
 def solution(orders, course):
     answer = []
     for k in course:
         c = []
         for menu in orders:
             for i in combinations(menu, k):
-                c.append(''.join(sorted(i)))
+                c.append("".join(sorted(i)))
         sc = Counter(c).most_common()
         answer += [x for x, cnt in sc if cnt > 1 and cnt == sc[0][1]]
     return sorted(answer)
