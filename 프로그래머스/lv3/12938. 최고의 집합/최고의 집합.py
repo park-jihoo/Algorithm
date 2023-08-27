d7b79a5bc2a79c521509e68efbe4ab72e5f528ec
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-08-27 17:21:15 +0900

    [level 3] Title: 최고의 집합, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.py
@@ -0,0 +1,8 @@
+def solution(n, s):
+    if n>s:
+        return [-1]
+    [portion, remain]=divmod(s,n)
+    answer=[portion]*n
+    for i in range(remain):
+        answer[i]+=1
+    return sorted(answer)
