3e1a606e55d64243cb57e757af50ebceec47af9a
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-08-27 17:26:39 +0900

    [Silver IV] Title: 큐, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,28 @@
+import sys
+
+N = int(sys.stdin.readline())
+q = []
+
+for i in range(N):
+    x = sys.stdin.readline().split()
+    if len(x) == 2:
+        q.append(int(x[1]))
+    elif x[0] == "pop":
+        if q:
+            print(q.pop(0))
+        else:
+            print("-1")
+    elif x[0] == "size":
+        print(len(q))
+    elif x[0] == "empty":
+        print(int(len(q) == 0))
+    elif x[0] == 'front':
+        if q:
+            print(q[0])
+        else:
+            print("-1")
+    elif x[0] == 'back':
+        if q:
+            print(q[len(q) - 1])
+        else:
+            print("-1")
