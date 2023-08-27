726825e825a9f3fd27f423675971ffe69c057e0c
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-08-27 17:17:16 +0900

    [level 2] Title: 더 맵게, Time: 207.46 ms, Memory: 41.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+    priority_queue<int, vector<int>, greater<int>> scovilleq;
+    for(int i=0;i<scoville.size();i++)
+        scovilleq.push(scoville[i]);
+    while(scovilleq.top()<K){
+        int a, b;
+        if(scovilleq.size()==1)
+            return -1;
+        a=scovilleq.top();
+        scovilleq.pop();
+        b=scovilleq.top();
+        scovilleq.pop();
+        scovilleq.push(a+b*2);
+        answer++;
+    }
+    return answer;
+}
