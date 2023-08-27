8474a8e51c22572ddf9c262d22b943e318f858a3
park-jihoo <park-jihoo@users.noreply.github.com>
2023-08-27 08:18:44 +0000

    Code formatting

--- 프로그래머스/lv3/42884. 단속카메라/단속카메라.cpp
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.cpp
@@ -1,20 +1,20 @@
+#include <algorithm>
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int solution(vector<vector<int>> routes) {
-    int answer = 1;
-    sort(routes.begin(), routes.end());
-    int cPosition=routes[0][1];
-    for(int i=1;i<routes.size();i++){
-     if(routes[i][1]<cPosition)
-         cPosition=routes[i][1];
-        if(routes[i][0]>cPosition){
-            answer++;
-            cPosition=routes[i][1];
-        }
+  int answer = 1;
+  sort(routes.begin(), routes.end());
+  int cPosition = routes[0][1];
+  for (int i = 1; i < routes.size(); i++) {
+    if (routes[i][1] < cPosition)
+      cPosition = routes[i][1];
+    if (routes[i][0] > cPosition) {
+      answer++;
+      cPosition = routes[i][1];
     }
-    return answer;
+  }
+  return answer;
 }
--- 프로그래머스/lv3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.py
@@ -1,11 +1,11 @@
 def solution(routes):
     answer = 1
     routes = sorted(routes)
-    cPosition=routes[0][1]
+    cPosition = routes[0][1]
     for i in range(1, len(routes)):
-        if routes[i][1]<cPosition:
-            cPosition=routes[i][1]
-        if routes[i][0]>cPosition:
-            answer+=1
-            cPosition=routes[i][1]
+        if routes[i][1] < cPosition:
+            cPosition = routes[i][1]
+        if routes[i][0] > cPosition:
+            answer += 1
+            cPosition = routes[i][1]
     return answer
