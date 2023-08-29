1b8be67493410603f4fc000f10615c378ceb5782
park-jihoo <park-jihoo@users.noreply.github.com>
2023-08-29 05:06:21 +0000

    Code formatting

--- 백준/Gold/16947. 서울 지하철 2호선/서울 지하철 2호선.cc
+++ 백준/Gold/16947. 서울 지하철 2호선/서울 지하철 2호선.cc
@@ -1,7 +1,7 @@
-#include <iostream>
-#include <vector>
-#include <queue>
 #include <cstring>
+#include <iostream>
+#include <queue>
+#include <vector>
 #define MAX 3001
 
 using namespace std;
@@ -14,62 +14,63 @@ int pre[MAX];
 bool hasCycle;
 int dist[MAX];
 
-void dfs(int start){
-    visited[start] = true;
-    for(int next : graph[start]){
-        if(hasCycle) return;
-        if(visited[next]){
-            if(pre[start] != next){
-                hasCycle = true;
-                cycle[start] = true;
-                while(start != next){
-                    cycle[pre[start]] = true;
-                    start = pre[start];
-                }
-                return;
-            }
-        }else{
-            pre[next] = start;
-            dfs(next);
+void dfs(int start) {
+  visited[start] = true;
+  for (int next : graph[start]) {
+    if (hasCycle)
+      return;
+    if (visited[next]) {
+      if (pre[start] != next) {
+        hasCycle = true;
+        cycle[start] = true;
+        while (start != next) {
+          cycle[pre[start]] = true;
+          start = pre[start];
         }
+        return;
+      }
+    } else {
+      pre[next] = start;
+      dfs(next);
     }
+  }
 }
 
-int main(){
-    cin >> n;
-    for(int i=0;i<n;i++){
-        int start, end;
-        cin >> start >> end;
-        graph[start].push_back(end);
-        graph[end].push_back(start);
-    }
+int main() {
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    int start, end;
+    cin >> start >> end;
+    graph[start].push_back(end);
+    graph[end].push_back(start);
+  }
 
-    // find cycle with dfs
-    dfs(1);
+  // find cycle with dfs
+  dfs(1);
 
-    // find distance with bfs
-    memset(visited, false, sizeof(visited));
-    queue<pair<int, int> > q;
-    for(int i=1;i<=n;i++){
-        if(cycle[i]){
-            q.push(make_pair(i, 0));
-            visited[i] = true;
-        }
+  // find distance with bfs
+  memset(visited, false, sizeof(visited));
+  queue<pair<int, int>> q;
+  for (int i = 1; i <= n; i++) {
+    if (cycle[i]) {
+      q.push(make_pair(i, 0));
+      visited[i] = true;
     }
-    while(!q.empty()){
-        int cur = q.front().first;
-        int curDist = q.front().second;
-        q.pop();
-        dist[cur] = curDist;
-        for(int next : graph[cur]){
-            if(!visited[next]){
-                visited[next] = true;
-                q.push(make_pair(next, curDist+1));
-            }
-        }
+  }
+  while (!q.empty()) {
+    int cur = q.front().first;
+    int curDist = q.front().second;
+    q.pop();
+    dist[cur] = curDist;
+    for (int next : graph[cur]) {
+      if (!visited[next]) {
+        visited[next] = true;
+        q.push(make_pair(next, curDist + 1));
+      }
     }
+  }
 
-    for(int i=1;i<=n;i++){
-        cout << dist[i] << " ";
-    }
+  for (int i = 1; i <= n; i++) {
+    cout << dist[i] << " ";
+  }
 }
