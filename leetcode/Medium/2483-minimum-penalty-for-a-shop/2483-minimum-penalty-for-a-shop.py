42ee5ff948fc5841920d230de8777da6c12e5459
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-08-29 10:20:57 +0900

    Time: 118 ms (81.02%), Space: 17.4 MB (73.49%) - LeetHub

---
+++ leetcode/Medium/2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
@@ -0,0 +1,14 @@
+class Solution:
+    def bestClosingTime(self, customers: str) -> int:
+        cur = customers.count('Y')
+        opt = cur
+        ans = 0
+        for idx, status in enumerate(customers):
+            if status == 'Y':
+                cur -=1
+            else:
+                cur +=1
+            if cur < opt:
+                ans = idx + 1
+                opt = cur
+        return ans
