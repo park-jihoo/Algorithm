01a15a435a820ae5f438eeb150323b6d3751ab77
park-jihoo <park-jihoo@users.noreply.github.com>
2023-09-01 00:02:59 +0000

    Code formatting

--- leetcode/Easy/0338-counting-bits/0338-counting-bits.py
+++ leetcode/Easy/0338-counting-bits/0338-counting-bits.py
@@ -1,6 +1,6 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
         answer = []
-        for i in range(n+1):
-            answer.append(bin(i).count('1'))
+        for i in range(n + 1):
+            answer.append(bin(i).count("1"))
         return answer
