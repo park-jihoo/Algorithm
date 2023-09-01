3990a213fb397fd63a6036709a05412fc7e6966c
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-09-01 11:23:59 +0900

    [Gold IV] Title: N-Queen, Time: 4120 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int ans;
+int board[15];
+
+bool check(int cdx){
+    for(int i=0;i<cdx;i++){
+        if(board[i]==board[cdx] || abs(board[i]-board[cdx])==cdx-i)
+            return false;
+    }
+    return true;
+}
+
+void nQueen(int cdx){
+    if (cdx == n){
+        ans++;
+        return;
+    }
+    for(int i=0;i<n;i++){
+        board[cdx]=i;
+        if(check(cdx))
+            nQueen(cdx+1);
+    }
+}
+
+int main(){
+    cin >> n;
+    nQueen(0);
+    cout << ans << endl;
+}
