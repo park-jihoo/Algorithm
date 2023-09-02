faf31af52dafd61fba58e5665481ab0af6eedd5d
park-jihoo <park-jihoo@users.noreply.github.com>
2023-09-02 06:15:23 +0000

    Code formatting

--- leetcode/Medium/2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
+++ leetcode/Medium/2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
@@ -2,14 +2,14 @@ class Solution:
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
         max_val = len(s) + 1
         dp = [max_val] * (len(s) + 1)
-        dp[0] = 0 
+        dp[0] = 0
         dict_set = set(dictionary)
 
         for i in range(1, len(s) + 1):
             dp[i] = dp[i - 1] + 1
 
-            for l in range(1, i + 1): 
-                if s[i-l:i] in dict_set:
-                    dp[i] = min(dp[i], dp[i-l])
-                    
+            for l in range(1, i + 1):
+                if s[i - l : i] in dict_set:
+                    dp[i] = min(dp[i], dp[i - l])
+
         return dp[-1]
