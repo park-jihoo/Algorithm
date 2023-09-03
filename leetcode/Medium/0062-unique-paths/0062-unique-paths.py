944707febdb514e81a60aab24e54d9bc50217d51
park-jihoo <park-jihoo@users.noreply.github.com>
2023-09-03 00:04:34 +0000

    Code formatting

--- leetcode/Medium/0062-unique-paths/0062-unique-paths.py
+++ leetcode/Medium/0062-unique-paths/0062-unique-paths.py
@@ -1,5 +1,8 @@
 import math
 
+
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        return (factorial(m+n-2) // (factorial(m-1)*factorial(n-1)))%(2*10**9)
+        return (factorial(m + n - 2) // (factorial(m - 1) * factorial(n - 1))) % (
+            2 * 10**9
+        )
