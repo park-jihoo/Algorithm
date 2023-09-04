fab67d875104986f0addb00e100e52558d71907d
park-jihoo <park-jihoo@users.noreply.github.com>
2023-09-04 01:20:37 +0000

    Code formatting

--- leetcode/Easy/0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ leetcode/Easy/0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -8,17 +8,17 @@
  */
 class Solution {
 public:
-    bool hasCycle(ListNode *head) {
-        if(head == NULL)
-            return false;
-        ListNode* one = head;
-        ListNode* two = head->next;
-        while (one != two){
-            if (two == NULL || two->next == NULL)
-                return false;
-            one = one->next;
-            two = two->next->next;
-        }
-        return true;
+  bool hasCycle(ListNode *head) {
+    if (head == NULL)
+      return false;
+    ListNode *one = head;
+    ListNode *two = head->next;
+    while (one != two) {
+      if (two == NULL || two->next == NULL)
+        return false;
+      one = one->next;
+      two = two->next->next;
     }
+    return true;
+  }
 };
