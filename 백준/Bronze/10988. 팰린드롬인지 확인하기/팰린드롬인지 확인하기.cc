a72110c81976b06e96741fd92b48256da3f263a8
park-jihoo <park-jihoo@users.noreply.github.com>
2023-09-04 01:49:19 +0000

    Code formatting

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -3,16 +3,17 @@
 
 using namespace std;
 
-int main(){
-    string s;
-    cin >> s;
+int main() {
+  string s;
+  cin >> s;
 
-    for(int i=0; i<s.length()/2; i++){
-        if (s[i] == s[s.length()-1-i]) continue;
-        else{
-            cout << 0;
-            return 0;
-        }
+  for (int i = 0; i < s.length() / 2; i++) {
+    if (s[i] == s[s.length() - 1 - i])
+      continue;
+    else {
+      cout << 0;
+      return 0;
     }
-    cout << 1;
+  }
+  cout << 1;
 }
