a06dbd4707f7688cd52f5bde58b8e9907d22e70c
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-09-05 21:17:51 +0900

    [level 2] Title: 배달, Time: 0.83 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12978. 배달/배달.py
@@ -0,0 +1,27 @@
+import heapq
+
+def dijkstra(graph, start):
+    distances = {node: float('inf') for node in graph}
+    distances[start] = 0
+    queue = []
+    heapq.heappush(queue, [distances[start], start])
+    while queue:
+        cdist, cdest = heapq.heappop(queue)
+        if distances[cdest] < cdist:
+            continue
+        for newdes, newdis in graph[cdest].items():
+            distance = cdist + newdis
+            if distance < distances[newdes]:
+                distances[newdes] = distance
+                heapq.heappush(queue, [distance, newdes])
+    return distances
+
+            
+def solution(N, road, K):
+    answer = 0
+    graph = {x+1: {y+1 : float('inf') for y in range(N)} for x in range(N)}
+    for r in road:
+        graph[r[0]][r[1]] = min(r[2], graph[r[0]][r[1]])
+        graph[r[1]][r[0]] = min(r[2], graph[r[1]][r[0]])
+    result = dijkstra(graph, 1)
+    return len([x for x, y in result.items() if y <= K])
