af181b4c2617a6c02d2caafbc6ebc4cb93264252
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-09-05 21:20:48 +0900

    [level 2] Title: 소수 찾기, Time: 5.57 ms, Memory: 13.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
@@ -0,0 +1,10 @@
+from itertools import permutations
+
+def solution(numbers):
+    a=set()
+    for i in range(len(numbers)):
+        a|=set(map(int, map(''.join, permutations(list(numbers), i+1))))
+    a-=set(range(0, 2))
+    for i in range(2, int(max(a)**0.5+1)):
+        a-=set(range(i*2, max(a)+1, i))
+    return len(a)
