0d90d593a992bae621c1bbd52d01b2cbdd8e2828
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-09-06 09:18:15 +0900

    Time: 44 ms (64.64%), Space: 16.8 MB (57.88%) - LeetHub

---
+++ leetcode/Medium/0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def splitListToParts(self, head: Optional[ListNode], k: int) -> List[Optional[ListNode]]:
+        length = 0
+        curr = head
+        while curr:
+            curr = curr.next
+            length +=1
+        wid, rem = divmod(length, k)
+        answer = []
+        curr = head
+        for i in range(k):
+            head = write = ListNode(None)
+            for j in range(wid + (i < rem)):
+                write.next = write = ListNode(curr.val)
+                if curr:
+                    curr = curr.next
+            answer.append(head.next)
+        return answer
