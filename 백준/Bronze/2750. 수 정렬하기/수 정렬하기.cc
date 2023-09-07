893714c46ca736f2f9d682865c27277757fea0e1
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-09-07 15:13:34 +0900

    [Bronze II] Title: 수 정렬하기, Time: 60 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> v;
+    for(int i=0;i<n;i++){
+        int val;
+        cin >> val;
+        v.push_back(val);
+    }
+    
+    // bubble sort
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(v[j] > v[j+1]){
+                int tmp = v[j];
+                v[j] = v[j+1];
+                v[j+1] = tmp;
+            }
+        }
+    }
+    
+    for(int i=0;i<n;i++){
+        cout << v[i] << endl;
+    }
+}
