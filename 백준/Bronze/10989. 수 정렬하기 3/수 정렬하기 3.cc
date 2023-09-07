8d665c018033ef3e496cc86898557daad66c311f
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-09-07 15:21:12 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1980 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int T;
+    cin >> T;
+
+    int arr[10001] = {0};
+
+    for(int i = 0 ; i < T; i++){
+        int a;
+        cin >> a;
+        arr[a] += 1;
+    }
+
+    for(int i = 1 ; i <= 10000; i++)
+        for (int j = 0; j < arr[i]; j++)
+            cout << i << "\n";
+}
