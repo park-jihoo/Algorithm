5e230c2cc7cf66c7034cbe25302b18482b483545
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-09-07 15:14:28 +0900

    [Bronze II] Title: 수 정렬하기, Time: 40 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -3,28 +3,39 @@
 
 using namespace std;
 
-int main() {
-  int n;
-  cin >> n;
-  vector<int> v;
-  for (int i = 0; i < n; i++) {
-    int val;
-    cin >> val;
-    v.push_back(val);
-  }
-
-  // bubble sort
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n - i - 1; j++) {
-      if (v[j] > v[j + 1]) {
-        int tmp = v[j];
-        v[j] = v[j + 1];
-        v[j + 1] = tmp;
-      }
+int main(){
+    int n;
+    cin >> n;
+    vector<int> v;
+    for(int i=0;i<n;i++){
+        int val;
+        cin >> val;
+        v.push_back(val);
     }
-  }
-
-  for (int i = 0; i < n; i++) {
-    cout << v[i] << endl;
-  }
+    
+    // bubble sort
+    /*for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(v[j] > v[j+1]){
+                int tmp = v[j];
+                v[j] = v[j+1];
+                v[j+1] = tmp;
+            }
+        }
+    }*/
+    
+    // insertion sort
+    for(int i=1;i<n;i++){
+        int key = v[i];
+        int j = i-1;
+        while(j>=0 && v[j]>key){
+            v[j+1] = v[j];
+            j--;
+        }
+        v[j+1] = key;
+    }
+    
+    for(int i=0;i<n;i++){
+        cout << v[i] << endl;
+    }
 }
