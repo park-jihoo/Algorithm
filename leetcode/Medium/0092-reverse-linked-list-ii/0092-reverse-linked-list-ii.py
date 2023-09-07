07872907bc323d273fc03f3aedb1f1e2ccf09e68
park-jihoo <park-jihoo@users.noreply.github.com>
2023-09-07 00:57:10 +0000

    Code formatting

--- leetcode/Medium/0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
+++ leetcode/Medium/0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -4,14 +4,16 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        temp = ListNode(-1) # sentinel
+    def reverseBetween(
+        self, head: Optional[ListNode], left: int, right: int
+    ) -> Optional[ListNode]:
+        temp = ListNode(-1)  # sentinel
         prev = temp
         temp.next = head
         for i in range(left - 1):
             prev = prev.next
         cur = prev.next
-        for i in range(right-left):
+        for i in range(right - left):
             ptr = prev.next
             prev.next = cur.next
             cur.next = cur.next.next
