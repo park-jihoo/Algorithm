fdf1d4e8802753279af41d461e8f2537448fdd8e
park-jihoo <park-jihoo@users.noreply.github.com>
2023-09-07 06:20:55 +0000

    Code formatting

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -2,60 +2,61 @@
 
 using namespace std;
 
-void merge(int* arr, int* sortedArr, int begin, int middle, int end) {
-    int sortedInd = begin;
-    int i = begin;
-    int j = middle+1;
+void merge(int *arr, int *sortedArr, int begin, int middle, int end) {
+  int sortedInd = begin;
+  int i = begin;
+  int j = middle + 1;
 
+  while (i <= middle && j <= end) {
+    if (arr[i] < arr[j])
+      sortedArr[sortedInd] = arr[i++];
+    else
+      sortedArr[sortedInd] = arr[j++];
 
-    while (i <= middle && j <= end) {
-        if (arr[i] < arr[j]) sortedArr[sortedInd] = arr[i++];
-        else  sortedArr[sortedInd] = arr[j++];
+    sortedInd++;
+  }
 
-        sortedInd++;
+  if (i > middle) {
+    while (j <= end) {
+      sortedArr[sortedInd++] = arr[j++];
     }
+  }
 
-    if (i > middle) {
-        while (j <= end) {
-            sortedArr[sortedInd++] = arr[j++];
-        }
+  if (j > end) {
+    while (i <= middle) {
+      sortedArr[sortedInd++] = arr[i++];
     }
-    
-    if (j > end) {
-        while (i <= middle) {
-            sortedArr[sortedInd++] = arr[i++];
-        }
-    }
+  }
 
-    for (int k = begin; k <= end; k++) arr[k] = sortedArr[k];
-
+  for (int k = begin; k <= end; k++)
+    arr[k] = sortedArr[k];
 }
 
-void mergeSort(int* arr, int* sortedArr, int begin, int end) {
-    int middle;
-    if (begin<end) {
-        middle = (begin + end) / 2;
-        mergeSort(arr, sortedArr, begin, middle);
-        mergeSort(arr, sortedArr, middle + 1, end);
-        merge(arr, sortedArr, begin, middle, end);
-    }
+void mergeSort(int *arr, int *sortedArr, int begin, int end) {
+  int middle;
+  if (begin < end) {
+    middle = (begin + end) / 2;
+    mergeSort(arr, sortedArr, begin, middle);
+    mergeSort(arr, sortedArr, middle + 1, end);
+    merge(arr, sortedArr, begin, middle, end);
+  }
 }
 
 int main() {
-    cin.tie(NULL);
-    ios::sync_with_stdio(false);
-    int n;
-    cin >> n;
+  cin.tie(NULL);
+  ios::sync_with_stdio(false);
+  int n;
+  cin >> n;
 
-    int* arr = new int[n];
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
+  int *arr = new int[n];
+  for (int i = 0; i < n; i++)
+    cin >> arr[i];
 
-    int* sortedArr = new int[n];
-    mergeSort(arr, sortedArr, 0, n-1);
+  int *sortedArr = new int[n];
+  mergeSort(arr, sortedArr, 0, n - 1);
 
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << '\n';
+  for (int i = 0; i < n; i++)
+    cout << arr[i] << '\n';
 
-    return 0;
+  return 0;
 }
