1d98b72be031d7b35cfca2c565d746857b4c15bc
park-jihoo <park-jihoo@users.noreply.github.com>
2023-09-07 06:14:08 +0000

    Code formatting

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -3,28 +3,28 @@
 
 using namespace std;
 
-int main(){
-    int n;
-    cin >> n;
-    vector<int> v;
-    for(int i=0;i<n;i++){
-        int val;
-        cin >> val;
-        v.push_back(val);
+int main() {
+  int n;
+  cin >> n;
+  vector<int> v;
+  for (int i = 0; i < n; i++) {
+    int val;
+    cin >> val;
+    v.push_back(val);
+  }
+
+  // bubble sort
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n - i - 1; j++) {
+      if (v[j] > v[j + 1]) {
+        int tmp = v[j];
+        v[j] = v[j + 1];
+        v[j + 1] = tmp;
+      }
     }
-    
-    // bubble sort
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n-i-1;j++){
-            if(v[j] > v[j+1]){
-                int tmp = v[j];
-                v[j] = v[j+1];
-                v[j+1] = tmp;
-            }
-        }
-    }
-    
-    for(int i=0;i<n;i++){
-        cout << v[i] << endl;
-    }
+  }
+
+  for (int i = 0; i < n; i++) {
+    cout << v[i] << endl;
+  }
 }
