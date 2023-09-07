b3061a630b2e9bd890cf9613a8eb9c3807a37eae
park-jihoo <park-jihoo@users.noreply.github.com>
2023-09-07 00:58:59 +0000

    Code formatting

--- leetcode/Medium/0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ leetcode/Medium/0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -10,19 +10,19 @@
  */
 class Solution {
 public:
-    ListNode* reverseBetween(ListNode* head, int left, int right) {
-        ListNode* temp = new ListNode(-1);
-        ListNode* prev = temp;
-        temp->next= head;
-        for(int i=0;i<left-1;i++)
-            prev = prev->next;
-        ListNode* cur = prev->next;
-        for(int i=0;i<right-left;i++){
-            ListNode* ptr = prev->next;
-            prev->next=cur->next;
-            cur->next = cur->next->next;
-            prev->next->next=ptr;
-        }
-        return temp->next;
+  ListNode *reverseBetween(ListNode *head, int left, int right) {
+    ListNode *temp = new ListNode(-1);
+    ListNode *prev = temp;
+    temp->next = head;
+    for (int i = 0; i < left - 1; i++)
+      prev = prev->next;
+    ListNode *cur = prev->next;
+    for (int i = 0; i < right - left; i++) {
+      ListNode *ptr = prev->next;
+      prev->next = cur->next;
+      cur->next = cur->next->next;
+      prev->next->next = ptr;
     }
+    return temp->next;
+  }
 };
