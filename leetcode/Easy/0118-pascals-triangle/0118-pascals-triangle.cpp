ca81e6778153df9dc2659b3839d0630a7eed95f8
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-09-08 10:11:12 +0900

    Time: 4 ms (27.26%), Space: 7.9 MB (6.18%) - LeetHub

---
+++ leetcode/Easy/0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> answer;
+        if(numRows == 0)
+            return answer;
+        if(numRows == 1){
+            answer.push_back({1});
+            return answer;
+        }
+        answer = generate(numRows - 1);
+        vector<int> line(1, 1);
+        for(int i=1;i<numRows - 1;i++){
+            line.push_back(answer[numRows-2][i] + answer[numRows-2][i-1]);
+        }
+        line.push_back(1);
+        answer.push_back(line);
+        return answer;
+    }
+};
