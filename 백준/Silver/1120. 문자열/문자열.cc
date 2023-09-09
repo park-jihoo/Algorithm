83c8ad59cd9b5ba2a0857800906a5d91f93a7498
park-jihoo <park-jihoo@users.noreply.github.com>
2023-09-09 03:21:57 +0000

    Code formatting

--- 백준/Silver/1120. 문자열/문자열.cc
+++ 백준/Silver/1120. 문자열/문자열.cc
@@ -3,18 +3,20 @@
 
 using namespace std;
 
-int main(){
-    string a, b;
-    cin >> a >> b;
-    int ans = b.length();
-    for(int i=0; i<=b.length()-a.length(); i++){
-        int cnt = 0;
-        for(int j=0; j<a.length(); j++){
-            if(a[j] != b[i+j]) cnt++;
-        }
-        if(ans > cnt) ans = cnt;
+int main() {
+  string a, b;
+  cin >> a >> b;
+  int ans = b.length();
+  for (int i = 0; i <= b.length() - a.length(); i++) {
+    int cnt = 0;
+    for (int j = 0; j < a.length(); j++) {
+      if (a[j] != b[i + j])
+        cnt++;
     }
+    if (ans > cnt)
+      ans = cnt;
+  }
 
-    cout << ans << endl;
-    return 0;
+  cout << ans << endl;
+  return 0;
 }
