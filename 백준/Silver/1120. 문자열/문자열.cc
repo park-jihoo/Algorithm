9399153ac4e434e09f647bd6b6a4c7ec629fb037
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-09-09 12:21:08 +0900

    [Silver IV] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1120. 문자열/문자열.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string a, b;
+    cin >> a >> b;
+    int ans = b.length();
+    for(int i=0; i<=b.length()-a.length(); i++){
+        int cnt = 0;
+        for(int j=0; j<a.length(); j++){
+            if(a[j] != b[i+j]) cnt++;
+        }
+        if(ans > cnt) ans = cnt;
+    }
+
+    cout << ans << endl;
+    return 0;
+}
