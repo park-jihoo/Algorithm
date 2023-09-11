2c17acfd9e66c3b27cba100a2f6df25bc100657e
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-09-11 13:51:13 +0900

    Time: 67 ms (91.07%), Space: 16.5 MB (31.58%) - LeetHub

---
+++ leetcode/Medium/1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
+        groups = {}
+        result = []
+        
+        for idx, size in enumerate(groupSizes):
+            if size not in groups:
+                groups[size] = []
+            groups[size].append(idx)
+            
+            if len(groups[size]) == size:
+                result.append(groups[size])
+                groups[size] = []
+                
+        return result
