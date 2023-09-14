327a882886c032c8892d58633e97ad87bfc543bf
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-09-14 13:59:02 +0900

    Time: 88 ms (64.27%), Space: 16.8 MB (87.47%) - LeetHub

---
+++ leetcode/Hard/0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        targets = collections.defaultdict(list)
+        for a, b in sorted(tickets)[::-1]:
+            targets[a] += b,
+        route = []
+        def visit(airport):
+            while targets[airport]:
+                visit(targets[airport].pop())
+            route.append(airport)
+        visit('JFK')
+        return route[::-1]
