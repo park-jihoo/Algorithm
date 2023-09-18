4af0a2ca5956806728d8d0b1bb84491d4a003f69
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-09-18 13:07:08 +0900

    Time: 98 ms (91.08%), Space: 16.7 MB (38.2%) - LeetHub

---
+++ leetcode/Easy/1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -0,0 +1,9 @@
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        ans=[]
+        n=len(mat)
+        for i in range(n):
+            ans.append([mat[i].count(1),i])
+
+        ans.sort()    
+        return [i[1] for i in ans[:k]]
