4eb2d9c9e868ecdd61f11e656d4b1fb42d7cf336
park-jihoo <park-jihoo@users.noreply.github.com>
2023-09-19 04:35:37 +0000

    Code formatting

--- leetcode/Medium/0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ leetcode/Medium/0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,6 +1,7 @@
 from collections import Counter
+
+
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         counter = Counter(nums)
-        return (counter.most_common()[0][0])
-        
+        return counter.most_common()[0][0]
