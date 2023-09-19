cfc60ad857e83ff333c837c6ca04c367535e1adc
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-09-19 13:49:47 +0900

    Time: 45 ms (96.62%), Space: 16.3 MB (95.2%) - LeetHub

---
+++ leetcode/Easy/1598-crawler-log-folder/1598-crawler-log-folder.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        #Array String Stack
+        folders = []
+        for log in logs:
+            if len(folders) > 0 and log == "../":
+                folders.pop()
+            elif log == "./" or log == "../":
+                pass
+            else:
+                folders.append(log)
+        return len(folders)
