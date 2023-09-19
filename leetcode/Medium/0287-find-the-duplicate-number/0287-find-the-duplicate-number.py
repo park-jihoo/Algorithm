2adbb3198aa302fa21bad77fa2517feffb089904
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-09-19 13:34:59 +0900

    Time: 558 ms (36.19%), Space: 39.2 MB (5.59%) - LeetHub

---
+++ leetcode/Medium/0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -0,0 +1,6 @@
+from collections import Counter
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        counter = Counter(nums)
+        return (counter.most_common()[0][0])
+        
