90f7b687f8c87566dec0b6fe7baac721f8275b39
park-jihoo <park-jihoo@users.noreply.github.com>
2023-09-19 07:45:45 +0000

    Code formatting

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -1,30 +1,31 @@
 from collections import deque
 import sys
+
 input = sys.stdin.readline
 
 N, M, R = map(int, input().split())
-graph = [[] for _ in range(N+1)]
+graph = [[] for _ in range(N + 1)]
 
 for _ in range(M):
     u, v = map(int, input().split())
     graph[u].append(v)
     graph[v].append(u)
 
-visited = [0]*(N+1) 
-visited[R] = 1 
+visited = [0] * (N + 1)
+visited[R] = 1
 
 queue = deque([R])
 
 cnt = 1
 while queue:
     node = queue.popleft()
-    graph[node].sort()  
+    graph[node].sort()
 
     for i in graph[node]:
         if not visited[i]:
-            queue.append(i) 
+            queue.append(i)
             cnt += 1
-            visited[i] = cnt 
+            visited[i] = cnt
 
 for i in visited[1:]:
     print(i)
