e407002b9ff1898bbcdcce95b6cc5c7ee3af7189
park-jihoo <park-jihoo@users.noreply.github.com>
2023-09-23 01:16:02 +0000

    Code formatting

--- leetcode/Medium/1048-longest-string-chain/1048-longest-string-chain.py
+++ leetcode/Medium/1048-longest-string-chain/1048-longest-string-chain.py
@@ -1,13 +1,12 @@
 class Solution:
     def longestStrChain(self, words: List[str]) -> int:
-        length = sorted(words, key = len)
-        dp = {x : len(x) for x in words}
+        length = sorted(words, key=len)
+        dp = {x: len(x) for x in words}
         for w in length:
             max1 = 0
             for i in range(len(w)):
-                pre = w[:i]+w[i+1:]
-                max1 = max(max1, dp.get(pre, 0)+1)
+                pre = w[:i] + w[i + 1 :]
+                max1 = max(max1, dp.get(pre, 0) + 1)
                 dp[w] = max1
                 print(max1)
         return max(dp.values())
-        
