770a3040fedd3737a93ab3618ae3ab5f245d3bc7
park-jihoo <park-jihoo@users.noreply.github.com>
2023-09-27 02:46:37 +0000

    Code formatting

--- leetcode/Medium/0880-decoded-string-at-index/0880-decoded-string-at-index.py
+++ leetcode/Medium/0880-decoded-string-at-index/0880-decoded-string-at-index.py
@@ -1,12 +1,12 @@
 class Solution:
     def decodeAtIndex(self, s: str, k: int) -> str:
-        length, n = [0] , len(s)
+        length, n = [0], len(s)
         for l in s:
             if l.isdigit():
-                length.append(length[-1]*int(l))
+                length.append(length[-1] * int(l))
             else:
-                length.append(length[-1]+1)
+                length.append(length[-1] + 1)
         for i in range(n, 0, -1):
             k %= length[i]
-            if k == 0 and s[i-1].isalpha():
-                return s[i-1]
+            if k == 0 and s[i - 1].isalpha():
+                return s[i - 1]
