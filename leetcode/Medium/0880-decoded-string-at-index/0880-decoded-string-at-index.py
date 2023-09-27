821befc9d6f741613e983947e527552f42cc83e2
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-09-27 11:45:54 +0900

    Time: 34 ms (80.81%), Space: 16.2 MB (86.87%) - LeetHub

---
+++ leetcode/Medium/0880-decoded-string-at-index/0880-decoded-string-at-index.py
@@ -0,0 +1,12 @@
+class Solution:
+    def decodeAtIndex(self, s: str, k: int) -> str:
+        length, n = [0] , len(s)
+        for l in s:
+            if l.isdigit():
+                length.append(length[-1]*int(l))
+            else:
+                length.append(length[-1]+1)
+        for i in range(n, 0, -1):
+            k %= length[i]
+            if k == 0 and s[i-1].isalpha():
+                return s[i-1]
