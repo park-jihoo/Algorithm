15305039d2ec979369a8aef537e2611aa3711b4d
park-jihoo <park-jihoo@users.noreply.github.com>
2023-10-06 03:54:17 +0000

    Code formatting

--- leetcode/Medium/0343-integer-break/0343-integer-break.c
+++ leetcode/Medium/0343-integer-break/0343-integer-break.c
@@ -1,11 +1,11 @@
-int integerBreak(int n){
+int integerBreak(int n) {
 
-        if (n<=3)
-            return n-1; 
-        int ans = 1;
-        while (n > 4){
-            ans *= 3;
-            n -= 3;
-        }
-        return ans * n;
+  if (n <= 3)
+    return n - 1;
+  int ans = 1;
+  while (n > 4) {
+    ans *= 3;
+    n -= 3;
+  }
+  return ans * n;
 }
