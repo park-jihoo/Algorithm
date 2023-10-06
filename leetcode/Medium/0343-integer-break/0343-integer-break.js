1eb3f75cc80b09adbb73e1c8b155914d217b3210
park-jihoo <park-jihoo@users.noreply.github.com>
2023-10-06 03:55:08 +0000

    Prettified Code!

--- leetcode/Medium/0343-integer-break/0343-integer-break.js
+++ leetcode/Medium/0343-integer-break/0343-integer-break.js
@@ -2,14 +2,12 @@
  * @param {number} n
  * @return {number}
  */
-var integerBreak = function(n) {
-    
-        if (n<=3)
-            return n-1; 
-        var ans = 1;
-        while (n > 4){
-            ans *= 3;
-            n -= 3;
-        }
-        return ans * n;
+var integerBreak = function (n) {
+  if (n <= 3) return n - 1;
+  var ans = 1;
+  while (n > 4) {
+    ans *= 3;
+    n -= 3;
+  }
+  return ans * n;
 };
