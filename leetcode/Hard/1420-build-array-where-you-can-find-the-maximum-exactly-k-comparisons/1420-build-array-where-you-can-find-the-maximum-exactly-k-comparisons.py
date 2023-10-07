aaa4dac20c8e6fe010500d42175baa46e5ad9ffe
park-jihoo <park-jihoo@users.noreply.github.com>
2023-10-07 00:42:58 +0000

    Code formatting

--- leetcode/Hard/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.py
+++ leetcode/Hard/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.py
@@ -5,14 +5,14 @@ class Solution:
             if i == n:
                 if remain == 0:
                     return 1
-                
+
                 return 0
-            
+
             ans = (max_so_far * dp(i + 1, max_so_far, remain)) % MOD
             for num in range(max_so_far + 1, m + 1):
                 ans = (ans + dp(i + 1, num, remain - 1)) % MOD
-                
+
             return ans
-        
-        MOD = 10 ** 9 + 7
+
+        MOD = 10**9 + 7
         return dp(0, 0, k)
