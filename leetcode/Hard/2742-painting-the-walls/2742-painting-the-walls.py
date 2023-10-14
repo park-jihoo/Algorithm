b0364371f4f465ad90e249892680580abe4d28b7
park-jihoo <park-jihoo@users.noreply.github.com>
2023-10-14 02:57:30 +0000

    Code formatting

--- leetcode/Hard/2742-painting-the-walls/2742-painting-the-walls.py
+++ leetcode/Hard/2742-painting-the-walls/2742-painting-the-walls.py
@@ -1,13 +1,13 @@
 class Solution:
     def paintWalls(self, cost: List[int], time: List[int]) -> int:
         n = len(cost)
-        dp = [[0]*(n+1) for _ in range(n+1)]
-        for i in range(1, n+1):
-            dp[n][i] = float('inf')
+        dp = [[0] * (n + 1) for _ in range(n + 1)]
+        for i in range(1, n + 1):
+            dp[n][i] = float("inf")
 
-        for i in range(n-1, -1, -1):
-            for r in range(1, n+1):
-                paint = cost[i] + dp[i+1][max(0, r-1-time[i])]
-                dont = dp[i+1][r]
+        for i in range(n - 1, -1, -1):
+            for r in range(1, n + 1):
+                paint = cost[i] + dp[i + 1][max(0, r - 1 - time[i])]
+                dont = dp[i + 1][r]
                 dp[i][r] = min(paint, dont)
         return dp[0][n]
