d83a20c51d2377c16fdbb4971b329ec53f384f8d
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-10-16 09:47:15 +0900

    Time: 36 ms (74.41%), Space: 16.1 MB (82.18%) - LeetHub

---
+++ leetcode/Easy/0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        if rowIndex == 0:
+            return [1]
+        else:
+            temp = self.getRow(rowIndex -1)
+            ans = ([1] + [temp[x] + temp[x+1] for x in range(rowIndex -1)] + [1])
+            return ans
