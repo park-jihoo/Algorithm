47886b48f7ddccd3dfe454003ed4c7d073f969ce
park-jihoo <park-jihoo@users.noreply.github.com>
2023-10-16 00:48:05 +0000

    Code formatting

--- leetcode/Easy/0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ leetcode/Easy/0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -3,6 +3,6 @@ class Solution:
         if rowIndex == 0:
             return [1]
         else:
-            temp = self.getRow(rowIndex -1)
-            ans = ([1] + [temp[x] + temp[x+1] for x in range(rowIndex -1)] + [1])
+            temp = self.getRow(rowIndex - 1)
+            ans = [1] + [temp[x] + temp[x + 1] for x in range(rowIndex - 1)] + [1]
             return ans
