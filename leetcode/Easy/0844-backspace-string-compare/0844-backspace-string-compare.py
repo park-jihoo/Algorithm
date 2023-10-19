c694ff59d747f9fa16da14eaa7a36278ada2b660
park-jihoo <park-jihoo@users.noreply.github.com>
2023-10-19 12:38:17 +0000

    Code formatting

--- leetcode/Easy/0844-backspace-string-compare/0844-backspace-string-compare.py
+++ leetcode/Easy/0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -3,13 +3,13 @@ class Solution:
         sstack = []
         tstack = []
         for i in s:
-            if i == '#' and len(sstack) > 0:
+            if i == "#" and len(sstack) > 0:
                 sstack.pop()
-            elif i != '#':
+            elif i != "#":
                 sstack.append(i)
         for i in t:
-            if i == '#' and len(tstack) > 0:
+            if i == "#" and len(tstack) > 0:
                 tstack.pop()
-            elif i != '#':
+            elif i != "#":
                 tstack.append(i)
         return sstack == tstack
