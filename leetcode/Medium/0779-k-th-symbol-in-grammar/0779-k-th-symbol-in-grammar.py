0da632878be5760640947f171ba32b82f578e76b
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-10-25 11:24:49 +0900

    Time: 33 ms (85.18%), Space: 16.2 MB (50%) - LeetHub

---
+++ leetcode/Medium/0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
@@ -0,0 +1,9 @@
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        if n == 1:
+            return 0
+        elems = 2 ** (n - 1)
+        half = 2 ** (n - 2)
+        if k > half:
+            return 1 - self.kthGrammar(n, k-half)
+        return self.kthGrammar(n-1, k)
