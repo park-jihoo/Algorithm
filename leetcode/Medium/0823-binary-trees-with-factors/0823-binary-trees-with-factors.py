c7d08d3b43fce7f993b68dd35d7c86ec2625b074
park-jihoo <park-jihoo@users.noreply.github.com>
2023-10-26 05:28:20 +0000

    Code formatting

--- leetcode/Medium/0823-binary-trees-with-factors/0823-binary-trees-with-factors.py
+++ leetcode/Medium/0823-binary-trees-with-factors/0823-binary-trees-with-factors.py
@@ -7,7 +7,7 @@ class Solution:
             dp[n] = 1
         for i, n in enumerate(arr):
             for j in range(i):
-                if not(n%arr[j]) and n//arr[j] in dp:
-                    dp[n] += dp[arr[j]]*dp[n//arr[j]]
-                    dp[n] %= (10**9+7)
-        return sum(dp.values())%(10**9+7)
+                if not (n % arr[j]) and n // arr[j] in dp:
+                    dp[n] += dp[arr[j]] * dp[n // arr[j]]
+                    dp[n] %= 10**9 + 7
+        return sum(dp.values()) % (10**9 + 7)
