2492c7ef7c814f1f809b390b47be670355c8f659
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-10-26 14:27:34 +0900

    Time: 236 ms (90.83%), Space: 16.3 MB (87.08%) - LeetHub

---
+++ leetcode/Medium/0823-binary-trees-with-factors/0823-binary-trees-with-factors.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numFactoredBinaryTrees(self, arr: List[int]) -> int:
+        arr.sort()
+
+        dp = {}
+        for n in arr:
+            dp[n] = 1
+        for i, n in enumerate(arr):
+            for j in range(i):
+                if not(n%arr[j]) and n//arr[j] in dp:
+                    dp[n] += dp[arr[j]]*dp[n//arr[j]]
+                    dp[n] %= (10**9+7)
+        return sum(dp.values())%(10**9+7)
