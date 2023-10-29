e840e54845e0d51d80e409bf0359b605935b7a6f
park-jihoo <park-jihoo@users.noreply.github.com>
2023-10-29 04:57:44 +0000

    Code formatting

--- leetcode/Hard/0458-poor-pigs/0458-poor-pigs.py
+++ leetcode/Hard/0458-poor-pigs/0458-poor-pigs.py
@@ -2,6 +2,6 @@ class Solution:
     def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
         t = minutesToTest // minutesToDie
         x = 0
-        while (t+1)**x < buckets:
+        while (t + 1) ** x < buckets:
             x += 1
         return x
