8c3a932012ff0e48cd57548aded28ccf5ee24ca0
park-jihoo <park-jihoo@users.noreply.github.com>
2023-10-31 13:22:15 +0000

    Code formatting

--- leetcode/Medium/2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.py
+++ leetcode/Medium/2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.py
@@ -3,5 +3,5 @@ class Solution:
         n = len(pref)
         answer = [pref[0]]
         for i in range(1, n):
-            answer.append(pref[i]^pref[i-1])
+            answer.append(pref[i] ^ pref[i - 1])
         return answer
