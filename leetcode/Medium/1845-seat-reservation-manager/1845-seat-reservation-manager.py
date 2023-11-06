865a4b5db73ad90f941938b389893d7118e6772f
park-jihoo <park-jihoo@users.noreply.github.com>
2023-11-06 10:43:03 +0000

    Code formatting

--- leetcode/Medium/1845-seat-reservation-manager/1845-seat-reservation-manager.py
+++ leetcode/Medium/1845-seat-reservation-manager/1845-seat-reservation-manager.py
@@ -1,9 +1,9 @@
 import heapq
 
-class SeatManager:
 
+class SeatManager:
     def __init__(self, n: int):
-        self.unreserved = list(range(1, n+1))
+        self.unreserved = list(range(1, n + 1))
 
     def reserve(self) -> int:
         return heapq.heappop(self.unreserved)
