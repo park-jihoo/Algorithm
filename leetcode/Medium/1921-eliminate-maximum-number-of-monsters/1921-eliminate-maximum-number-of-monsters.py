bb883cb6515ec50160c0be2e9169b5f9ebbf07c3
park-jihoo <park-jihoo@users.noreply.github.com>
2023-11-07 07:47:52 +0000

    Code formatting

--- leetcode/Medium/1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.py
+++ leetcode/Medium/1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.py
@@ -1,6 +1,6 @@
 class Solution:
     def eliminateMaximum(self, dist: List[int], speed: List[int]) -> int:
-        arrival = sorted([d/s for d, s in zip(dist, speed)])
+        arrival = sorted([d / s for d, s in zip(dist, speed)])
         ans = 0
         for idx, arr in enumerate(arrival):
             if arr <= idx:
