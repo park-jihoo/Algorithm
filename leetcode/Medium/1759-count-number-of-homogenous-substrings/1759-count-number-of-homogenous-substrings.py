3ddd89ac31dd26dce4091e6151cd3ab4e3897e88
park-jihoo <park-jihoo@users.noreply.github.com>
2023-11-10 00:55:39 +0000

    Code formatting

--- leetcode/Medium/1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.py
+++ leetcode/Medium/1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.py
@@ -1,16 +1,16 @@
 class Solution:
     def countHomogenous(self, s: str) -> int:
         ans = 0
-        mod = 10 ** 9 + 7
+        mod = 10**9 + 7
         for k, g in groupby(s):
             x = len(list(g))
-            ans += (x * (x+1))//2
+            ans += (x * (x + 1)) // 2
             ans %= mod
         return ans % mod
 
     def countHomogenous2(self, s: str) -> int:
         strs = []
-        mod = 10 ** 9 + 7
+        mod = 10**9 + 7
         for a in s:
             if len(strs) == 0:
                 strs.append(a)
@@ -18,5 +18,5 @@ class Solution:
                 strs[-1] += a
             else:
                 strs.append(a)
-        lenstr = [(len(x)*(len(x) + 1)//2)%mod for x in strs]
-        return sum(lenstr)%mod
+        lenstr = [(len(x) * (len(x) + 1) // 2) % mod for x in strs]
+        return sum(lenstr) % mod
