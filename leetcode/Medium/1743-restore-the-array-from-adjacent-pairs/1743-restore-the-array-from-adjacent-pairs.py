861976ff04b6a9569a2315c48f18a504039d4306
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-11-10 09:52:45 +0900

    Time: 911 ms (65.12%), Space: 65 MB (82.86%) - LeetHub

---
+++ leetcode/Medium/1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.py
@@ -0,0 +1,21 @@
+class Solution:
+    def restoreArray(self, adjacentPairs: List[List[int]]) -> List[int]:
+        adj_dict = defaultdict(list)
+        for x, y in adjacentPairs:
+            adj_dict[x].append(y)
+            adj_dict[y].append(x)
+        
+        ans = []
+        for key, value in adj_dict.items():
+            if len(value) == 1:
+                ans.extend([key, value[0]])
+                break
+        for i in range(len(adjacentPairs)):
+            can = adj_dict[ans[-1]]
+            if len(can) == 1:
+                break
+            elif can[0] == ans[-2]:
+                ans.append(can[1])
+            else:
+                ans.append(can[0])
+        return ans
