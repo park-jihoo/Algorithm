10b390616746c543cd58a484a978245d90757059
park-jihoo <park-jihoo@users.noreply.github.com>
2023-11-13 00:32:38 +0000

    Code formatting

--- leetcode/Medium/2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.py
+++ leetcode/Medium/2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.py
@@ -1,10 +1,10 @@
 class Solution:
     def sortVowels(self, s: str) -> str:
-        vowels = deque(sorted([x for x in list(s) if x.lower() in list('aeiou')]))
+        vowels = deque(sorted([x for x in list(s) if x.lower() in list("aeiou")]))
         idx = 0
         ans = []
         for i in range(len(s)):
-            if s[i].lower() not in list('aeiou'):
+            if s[i].lower() not in list("aeiou"):
                 ans.append(s[i])
             else:
                 ans.append(vowels[0])
