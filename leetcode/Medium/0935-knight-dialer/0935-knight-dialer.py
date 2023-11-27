e644966d2e576d85572d64f47743d0fcd07d55fa
park-jihoo <park-jihoo@users.noreply.github.com>
2023-11-27 02:18:25 +0000

    Code formatting

--- leetcode/Medium/0935-knight-dialer/0935-knight-dialer.py
+++ leetcode/Medium/0935-knight-dialer/0935-knight-dialer.py
@@ -10,26 +10,25 @@ class Solution:
             [1, 7, 0],
             [2, 6],
             [1, 3],
-            [2, 4]
+            [2, 4],
         ]
-        
-        mod = 10 ** 9 + 7
+
+        mod = 10**9 + 7
         dp = [0] * 10
         prev_dp = [1] * 10
-        
+
         for remain in range(1, n):
             dp = [0] * 10
             for s in range(10):
                 ans = 0
                 for i in jumps[s]:
                     ans = (ans + prev_dp[i]) % mod
-                
+
                 dp[s] = ans
-                
+
             prev_dp = dp
 
         ans = 0
         for s in range(10):
             ans = (ans + prev_dp[s]) % mod
         return ans
-        
