a4a0e7105143db20c7f6c71b70d579be89153d20
park-jihoo <park-jihoo@users.noreply.github.com>
2023-11-29 08:46:15 +0000

    Code formatting

--- leetcode/Hard/2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.py
+++ leetcode/Hard/2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.py
@@ -2,8 +2,8 @@ class Solution:
     def numberOfWays(self, corridor: str) -> int:
         a, b, c = 1, 0, 0
         for ch in corridor:
-            if ch == 'S':
-                a, b, c = 0, a+c, b
+            if ch == "S":
+                a, b, c = 0, a + c, b
             else:
-                a, b, c = a+c, b, c
+                a, b, c = a + c, b, c
         return c % (10**9 + 7)
