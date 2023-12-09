0e78fe550ae9e208d4898df78304ab2c42cd00fe
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-12-09 17:23:03 +0900

    Time: 38 ms (57.81%), Space: 16.4 MB (19.15%) - LeetHub

--- leetcode/Easy/0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ leetcode/Easy/0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -9,13 +9,11 @@ class Solution:
         if root is None:
             return []
         result = []
-
         def help(node):
             if node is None:
                 return
             help(node.left)
             result.append(node.val)
             help(node.right)
-
         help(root)
         return result
