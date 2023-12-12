ec35d83b153de674ed5bca0431c67191ef218b53
park-jihoo <park-jihoo@users.noreply.github.com>
2023-12-12 00:12:40 +0000

    Code formatting

--- leetcode/Easy/1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.py
+++ leetcode/Easy/1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.py
@@ -1,11 +1,10 @@
 class Solution:
     def numberOfMatches(self, n: int) -> int:
-        return n-1
-        
+        return n - 1
+
     def numberOfMatches2(self, n: int) -> int:
         ans = 0
         while n > 1:
-            ans += (n // 2)
+            ans += n // 2
             n -= n // 2
         return ans
-        
