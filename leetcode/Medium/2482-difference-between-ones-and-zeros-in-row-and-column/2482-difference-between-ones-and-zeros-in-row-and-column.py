40be2ea164efd1e339287c38120eee3bb4f0019f
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-12-14 15:27:31 +0900

    Time: 1384 ms (62.14%), Space: 53 MB (33.93%) - LeetHub

---
+++ leetcode/Medium/2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
@@ -0,0 +1,6 @@
+class Solution:
+    def onesMinusZeros(self, grid: List[List[int]]) -> List[List[int]]:
+        onesRow = [sum(x) for x in grid]
+        onesCol = [sum([grid[j][i] for j in range(len(grid))]) for i in range(len(grid[0]))]
+
+        return [[2*(onesRow[j] + onesCol[i])-(len(grid[0])+len(grid)) for i in range(len(grid[0]))] for j in range(len(grid))]
