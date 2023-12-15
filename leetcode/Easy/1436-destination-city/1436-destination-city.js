9925ca5978b6835b211b3d0a1f40597bb4afc577
park-jihoo <park-jihoo@users.noreply.github.com>
2023-12-15 00:22:50 +0000

    Prettified Code!

--- leetcode/Easy/1436-destination-city/1436-destination-city.js
+++ leetcode/Easy/1436-destination-city/1436-destination-city.js
@@ -2,14 +2,14 @@
  * @param {string[][]} paths
  * @return {string}
  */
-var destCity = function(paths) {
-    let start = new Set();
-    let end = new Set();
-    for(path of paths){
-        let s = path[0];
-        let e = path[1];
-        start.add(s);
-        end.add(e);
-    }
-    return [...end].filter(x=>!start.has(x))[0]
+var destCity = function (paths) {
+  let start = new Set();
+  let end = new Set();
+  for (path of paths) {
+    let s = path[0];
+    let e = path[1];
+    start.add(s);
+    end.add(e);
+  }
+  return [...end].filter((x) => !start.has(x))[0];
 };
