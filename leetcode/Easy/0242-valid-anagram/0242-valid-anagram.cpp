7660e0b453302a42f128fc5d827e8508dd4d0646
park-jihoo <park-jihoo@users.noreply.github.com>
2023-12-16 02:47:47 +0000

    Code formatting

--- leetcode/Easy/0242-valid-anagram/0242-valid-anagram.cpp
+++ leetcode/Easy/0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
-        if(s.size() != t.size())
-            return false;
-        multiset<char> s_set;
-        multiset<char> t_set;
-        for(int i=0;i<s.size();i++){
-            s_set.insert(s[i]);
-            t_set.insert(t[i]);
-        }
-        return s_set == t_set;
+  bool isAnagram(string s, string t) {
+    if (s.size() != t.size())
+      return false;
+    multiset<char> s_set;
+    multiset<char> t_set;
+    for (int i = 0; i < s.size(); i++) {
+      s_set.insert(s[i]);
+      t_set.insert(t[i]);
     }
+    return s_set == t_set;
+  }
 };
