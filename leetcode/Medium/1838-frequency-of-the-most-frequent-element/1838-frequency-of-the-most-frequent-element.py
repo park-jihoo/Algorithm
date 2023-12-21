0b7bd1c293e2a1c4aea5cd4b4a4453244daea6be
park-jihoo <park-jihoo@users.noreply.github.com>
2023-12-21 05:21:00 +0000

    Code formatting

--- leetcode/Medium/1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
+++ leetcode/Medium/1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
@@ -5,7 +5,7 @@ class Solution:
         for right in range(len(nums)):
             target = nums[right]
             curr += target
-            
+
             if (right - left + 1) * target - curr > k:
                 curr -= nums[left]
                 left += 1
