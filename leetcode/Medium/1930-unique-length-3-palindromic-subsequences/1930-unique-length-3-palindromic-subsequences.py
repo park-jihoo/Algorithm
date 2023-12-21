98a4b39685af6809e2a40a59951dddbd34452adc
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-12-21 14:15:39 +0900

    Time: 106 ms (98.71%), Space: 18.2 MB (16.45%) - LeetHub

---
+++ leetcode/Medium/1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countPalindromicSubsequence(self, s):
+        ans = 0
+        for c in string.ascii_lowercase:
+            i, j = s.find(c), s.rfind(c)
+            if i > -1:
+                ans += len(set(s[i + 1: j]))
+        return ans
