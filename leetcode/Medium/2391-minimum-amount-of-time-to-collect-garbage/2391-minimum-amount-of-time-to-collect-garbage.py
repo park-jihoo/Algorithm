6f0e4af691de7861fecfbe55c379b087483c1329
park-jihoo <park-jihoo@users.noreply.github.com>
2023-12-21 05:11:08 +0000

    Code formatting

--- leetcode/Medium/2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.py
+++ leetcode/Medium/2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.py
@@ -3,10 +3,9 @@ class Solution:
         ans = sum(len(g) for g in garbage)
         G, P, M = False, False, False
         for i in range(len(travel), 0, -1):
-            G |= 'G' in garbage[i]
-            P |= 'P' in garbage[i]
-            M |= 'M' in garbage[i]
-            ans += travel[i-1] * (G + P + M)
-        
+            G |= "G" in garbage[i]
+            P |= "P" in garbage[i]
+            M |= "M" in garbage[i]
+            ans += travel[i - 1] * (G + P + M)
+
         return ans
-        
