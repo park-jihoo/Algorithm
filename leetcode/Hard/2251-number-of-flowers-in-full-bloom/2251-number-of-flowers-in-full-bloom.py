5e22c42419e41315a366b88ccb4b4be3e3a96965
park-jihoo <park-jihoo@users.noreply.github.com>
2023-12-21 05:13:08 +0000

    Code formatting

--- leetcode/Hard/2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.py
+++ leetcode/Hard/2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.py
@@ -1,12 +1,14 @@
 class Solution:
-    def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
+    def fullBloomFlowers(
+        self, flowers: List[List[int]], people: List[int]
+    ) -> List[int]:
         starts = []
         ends = []
-        
+
         for start, end in flowers:
             starts.append(start)
             ends.append(end + 1)
-            
+
         starts.sort()
         ends.sort()
         ans = []
@@ -15,5 +17,5 @@ class Solution:
             i = bisect_right(starts, person)
             j = bisect_right(ends, person)
             ans.append(i - j)
-        
+
         return ans
