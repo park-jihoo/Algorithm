713a5778719ade0ca207d6ce3f5bd72350a25a9c
park-jihoo <park-jihoo@users.noreply.github.com>
2023-12-21 05:15:24 +0000

    Code formatting

--- leetcode/Hard/2050-parallel-courses-iii/2050-parallel-courses-iii.py
+++ leetcode/Hard/2050-parallel-courses-iii/2050-parallel-courses-iii.py
@@ -2,11 +2,11 @@ class Solution:
     def minimumTime(self, n: int, relations: List[List[int]], time: List[int]) -> int:
         graph = defaultdict(list)
         indegree = [0] * n
-        
-        for (x, y) in relations:
+
+        for x, y in relations:
             graph[x - 1].append(y - 1)
             indegree[y - 1] += 1
-        
+
         queue = deque()
         max_time = [0] * n
         for node in range(n):
@@ -17,7 +17,9 @@ class Solution:
         while queue:
             node = queue.popleft()
             for neighbor in graph[node]:
-                max_time[neighbor] = max(max_time[neighbor], max_time[node] + time[neighbor])
+                max_time[neighbor] = max(
+                    max_time[neighbor], max_time[node] + time[neighbor]
+                )
                 indegree[neighbor] -= 1
                 if indegree[neighbor] == 0:
                     queue.append(neighbor)
