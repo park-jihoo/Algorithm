d49bd4bfc306a9acde1440bade4ea6a076812592
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-12-23 20:51:03 +0900

    Time: 55 ms (5.49%), Space: 18.7 MB (5.49%) - LeetHub

---
+++ leetcode/Easy/1496-path-crossing/1496-path-crossing.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+class Solution:
+    def isPathCrossing(self, path: str) -> bool:
+        pathlist = list(path)
+        visited = [(0,0)]
+        x, y = 0, 0
+        for p in pathlist:
+            if p == 'N':
+                x+=1
+            elif p == 'E':
+                y+=1
+            elif p == 'W':
+                y-=1
+            else:
+                x-=1
+            visited.append((x, y))
+            
+        c2 = Counter(visited)
+        return c2.most_common()[0][1] != 1
