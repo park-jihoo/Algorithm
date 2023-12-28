6553a3fca80dec2bf61d6f4a834b44dd6a194259
park-jihoo <park-jihoo@users.noreply.github.com>
2023-12-28 06:58:51 +0000

    Code formatting

--- leetcode/Easy/1108-defanging-an-ip-address/1108-defanging-an-ip-address.py
+++ leetcode/Easy/1108-defanging-an-ip-address/1108-defanging-an-ip-address.py
@@ -1,4 +1,4 @@
 class Solution:
     def defangIPaddr(self, address: str) -> str:
-        address = (address.replace(".", "[.]"))
+        address = address.replace(".", "[.]")
         return address
