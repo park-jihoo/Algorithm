a7eab5d69cda1ae1e6832eb1187e38895236e9e7
park-jihoo <park-jihoo@users.noreply.github.com>
2023-12-28 06:54:01 +0000

    Code formatting

--- leetcode/Easy/0682-baseball-game/0682-baseball-game.py
+++ leetcode/Easy/0682-baseball-game/0682-baseball-game.py
@@ -5,7 +5,7 @@ class Solution:
             if i == "+":
                 score.append(score[-1] + score[-2])
             elif i == "D":
-                score.append(score[-1]*2)
+                score.append(score[-1] * 2)
             elif i == "C":
                 score.pop()
             else:
