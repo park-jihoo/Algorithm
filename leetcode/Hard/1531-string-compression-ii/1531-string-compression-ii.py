3c896a2fdcf0d2903ef6f127be9e01180b4f46fb
park-jihoo <park-jihoo@users.noreply.github.com>
2023-12-28 02:56:49 +0000

    Code formatting

--- leetcode/Hard/1531-string-compression-ii/1531-string-compression-ii.py
+++ leetcode/Hard/1531-string-compression-ii/1531-string-compression-ii.py
@@ -3,14 +3,15 @@ class Solution:
         @cache
         def f(start, pre, last_count, left):
             if left < 0:
-                return float("inf") 
+                return float("inf")
             if start >= len(s):
                 return 0
-            if s[start]==pre:
-                incr = 1 if last_count in (1,9,99) else 0 
-                return incr + f(start+1, pre, last_count+1, left) 
+            if s[start] == pre:
+                incr = 1 if last_count in (1, 9, 99) else 0
+                return incr + f(start + 1, pre, last_count + 1, left)
             else:
-                keep = 1  + f(start+1, s[start], 1, left) 
-                delete = f(start+1, pre, last_count, left-1) 
+                keep = 1 + f(start + 1, s[start], 1, left)
+                delete = f(start + 1, pre, last_count, left - 1)
                 return min(keep, delete)
-        return f(0,"", 0,k)
+
+        return f(0, "", 0, k)
