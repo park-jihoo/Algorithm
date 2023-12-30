bcf965a1d5577b743e6ff259107d61cb47d43107
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-12-30 12:11:36 +0900

    Time: 74 ms (25.08%), Space: 17.5 MB (9.52%) - LeetHub

---
+++ leetcode/Easy/1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.py
@@ -0,0 +1,9 @@
+class Solution:
+    def makeEqual(self, words: List[str]) -> bool:
+        wordCounter = Counter()
+        for word in words:
+            wordCounter += Counter(list(word))
+        for a, count in wordCounter.most_common():
+            if count%len(words) != 0:
+                return False
+        return True
