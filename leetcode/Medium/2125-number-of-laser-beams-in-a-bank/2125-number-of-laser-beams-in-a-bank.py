c217fc76f1c9845032ea3d56f639ecb5556ca995
park-jihoo <park-jihoo@users.noreply.github.com>
2024-01-03 11:56:22 +0000

    Code formatting

--- leetcode/Medium/2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
+++ leetcode/Medium/2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
@@ -2,7 +2,7 @@ class Solution:
     def numberOfBeams(self, bank: List[str]) -> int:
         ans, temp = 0, 0
         for s in bank:
-            n = s.count('1')
+            n = s.count("1")
             if n == 0:
                 continue
             ans += temp * n
