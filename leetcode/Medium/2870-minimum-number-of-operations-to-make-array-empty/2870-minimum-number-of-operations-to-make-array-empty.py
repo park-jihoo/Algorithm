9f7cf2caf4670a54003468aac6655853d6867c81
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-01-04 14:46:35 +0900

    Time: 541 ms (96.67%), Space: 32.3 MB (6.06%) - LeetHub

---
+++ leetcode/Medium/2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        ans = 0
+        for val, cnt in Counter(nums).most_common():
+            if cnt == 1:
+                return -1
+            else:
+                ans += ((cnt-1) // 3 +1)
+        return ans
