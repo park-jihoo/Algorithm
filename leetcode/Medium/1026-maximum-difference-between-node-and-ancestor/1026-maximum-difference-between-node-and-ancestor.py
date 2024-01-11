d65702d5d2e81bdc22e414a7f7f8ce0a4994e802
park-jihoo <park-jihoo@users.noreply.github.com>
2024-01-11 02:38:46 +0000

    Code formatting

--- leetcode/Medium/1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.py
+++ leetcode/Medium/1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.py
@@ -7,23 +7,26 @@
 class Solution:
     def getmin(self, root):
         if root is None:
-            return float('inf')
+            return float("inf")
         return min(root.val, self.getmin(root.left), self.getmin(root.right))
 
     def getmax(self, root):
         if root is None:
             return 0
         return max(root.val, self.getmax(root.left), self.getmax(root.right))
-    
+
     def getdiff(self, root):
         if root is None:
             return 0
         minval, maxval = self.getmin(root), self.getmax(root)
-        diff = max(abs(root.val-minval), abs(root.val-maxval))
+        diff = max(abs(root.val - minval), abs(root.val - maxval))
         return diff
 
     def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
         if root is None:
             return 0
-        return max(self.getdiff(root), self.maxAncestorDiff(root.left), self.maxAncestorDiff(root.right))
-        
+        return max(
+            self.getdiff(root),
+            self.maxAncestorDiff(root.left),
+            self.maxAncestorDiff(root.right),
+        )
