aa9002bc9e1c2a9714fb1dd23466fa2f13aed7b0
park-jihoo <park-jihoo@users.noreply.github.com>
2024-01-20 07:19:15 +0000

    Code formatting

--- leetcode/Medium/0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
+++ leetcode/Medium/0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
@@ -3,11 +3,11 @@ class Solution:
         arr = [0] + arr
         s = deque([0])
         dp = [0] * len(arr)
-        mod = 10**9+7
+        mod = 10**9 + 7
         for i in range(len(arr)):
             while arr[s[-1]] > arr[i]:
                 s.pop()
             j = s[-1]
-            dp[i] = dp[j] + (i-j)*arr[i]
+            dp[i] = dp[j] + (i - j) * arr[i]
             s.append(i)
         return sum(dp) % mod
