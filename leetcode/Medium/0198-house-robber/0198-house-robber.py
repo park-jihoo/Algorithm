0c43b57fe98d6a266f006c59b387a7f785ca45ca
park-jihoo <park-jihoo@users.noreply.github.com>
2024-01-21 05:37:16 +0000

    Code formatting

--- leetcode/Medium/0198-house-robber/0198-house-robber.py
+++ leetcode/Medium/0198-house-robber/0198-house-robber.py
@@ -7,5 +7,5 @@ class Solution:
         dp = [0] * len(nums)
         dp[0], dp[1], dp[2] = nums[0], nums[1], nums[2] + nums[0]
         for i in range(3, len(nums)):
-            dp[i] = nums[i] + max(dp[i-2], dp[i-3])
+            dp[i] = nums[i] + max(dp[i - 2], dp[i - 3])
         return max(dp)
