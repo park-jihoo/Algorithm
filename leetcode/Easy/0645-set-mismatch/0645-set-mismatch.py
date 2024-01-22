24044dad2c80afbc35268b04c918a07296b0ce93
park-jihoo <park-jihoo@users.noreply.github.com>
2024-01-22 03:02:09 +0000

    Code formatting

--- leetcode/Easy/0645-set-mismatch/0645-set-mismatch.py
+++ leetcode/Easy/0645-set-mismatch/0645-set-mismatch.py
@@ -1,6 +1,6 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        original = set(range(1, len(nums)+1))
+        original = set(range(1, len(nums) + 1))
         duplicate = Counter(nums).most_common()[0][0]
         loss = list(original - set(nums))[0]
         return [duplicate, loss]
