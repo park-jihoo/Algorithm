b756e6152681de69ed1a1e82a8b9aab6bbdf8be5
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-01-26 11:50:05 +0900

    Time: 106 ms (52.6%), Space: 24.7 MB (7.51%) - LeetHub

---
+++ leetcode/Medium/0576-out-of-boundary-paths/0576-out-of-boundary-paths.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int, dp={}) -> int:
+        mod = 10 ** 9 + 7
+        if maxMove < 0:
+            return 0
+        if not (0 <= startRow < m and 0 <= startColumn < n):
+            return 1
+        if (m, n, maxMove, startRow, startColumn) not in dp:
+            dp[(m, n, maxMove, startRow, startColumn)] = (self.findPaths(m, n, maxMove - 1, startRow - 1, startColumn) + self.findPaths(m, n, maxMove - 1, startRow + 1, startColumn) + self.findPaths(m, n, maxMove - 1, startRow, startColumn - 1) + self.findPaths(m, n, maxMove - 1, startRow, startColumn + 1))%mod
+        return dp[(m, n, maxMove, startRow, startColumn)]
