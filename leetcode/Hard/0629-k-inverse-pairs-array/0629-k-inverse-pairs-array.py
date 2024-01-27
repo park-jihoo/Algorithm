6dc4c733f8154d6344817bb6f482b23d10f610ec
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-01-27 15:37:07 +0900

    Time: 2326 ms (5.71%), Space: 387.7 MB (5.71%) - LeetHub

--- leetcode/Hard/0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.py
+++ leetcode/Hard/0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.py
@@ -1,17 +1,12 @@
 class Solution:
     def __init__(self):
-        self.mod = 10**9 + 7
-
+        self.mod = 10 ** 9 + 7
     @cache
     def kInversePairs(self, n: int, k: int) -> int:
-        max_pairs = (n * (n - 1)) // 2
-        if n == 0 or k < 0 or k > max_pairs:
+        max_pairs = (n * (n-1))//2
+        if n==0 or k < 0 or k > max_pairs:
             return 0
-        elif k == 0 or k == max_pairs:
+        elif k ==0 or k == max_pairs:
             return 1
         else:
-            return (
-                self.kInversePairs(n - 1, k)
-                + self.kInversePairs(n, k - 1)
-                - self.kInversePairs(n - 1, k - n)
-            ) % self.mod
+            return (self.kInversePairs(n-1, k)+self.kInversePairs(n, k-1) - self.kInversePairs(n-1, k-n))%self.mod
