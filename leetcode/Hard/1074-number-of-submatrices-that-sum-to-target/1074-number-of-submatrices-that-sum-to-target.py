ce35af01f32276f3f8c3cc9682befcfbbcfda190
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-01-28 15:55:54 +0900

    Time: 621 ms (50.61%), Space: 17.7 MB (63.67%) - LeetHub

---
+++ leetcode/Hard/1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numSubmatrixSumTarget(self, matrix: List[List[int]], target: int) -> int:
+        m, n = len(matrix), len(matrix[0])
+        for row in matrix:
+            for i in range(n-1):
+                row[i+1] += row[i] # prefix sum
+        res = 0
+        for i in range(n):
+            for j in range(i, n):
+                c = defaultdict(int)
+                cur, c[0] = 0, 1
+                for k in range(m):
+                    cur += matrix[k][j] - (matrix[k][i-1] if i > 0 else 0)
+                    res += c[cur - target]
+                    c[cur] += 1
+        return res
