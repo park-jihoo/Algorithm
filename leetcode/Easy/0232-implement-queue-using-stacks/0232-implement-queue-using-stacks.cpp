4387cb4ce533d622969ad697851a657062f8b537
park-jihoo <park-jihoo@users.noreply.github.com>
2024-01-29 02:06:59 +0000

    Code formatting

--- leetcode/Easy/0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ leetcode/Easy/0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,45 +1,39 @@
 class MyQueue {
-    stack<int> stack1;
-    stack<int> stack2;
+  stack<int> stack1;
+  stack<int> stack2;
+
 public:
-    MyQueue() {
-        
+  MyQueue() {}
+
+  void push(int x) { stack1.push(x); }
+
+  int pop() {
+    if (stack2.empty()) {
+      while (!stack1.empty()) {
+        stack2.push(stack1.top());
+        stack1.pop();
+      }
     }
-    
-    void push(int x) {
-        stack1.push(x);
+    if (stack2.empty())
+      return -1;
+    int front = stack2.top();
+    stack2.pop();
+    return front;
+  }
+
+  int peek() {
+    if (stack2.empty()) {
+      while (!stack1.empty()) {
+        stack2.push(stack1.top());
+        stack1.pop();
+      }
     }
-    
-    int pop() {
-        if(stack2.empty()){
-            while (!stack1.empty()){
-                stack2.push(stack1.top());
-                stack1.pop();
-            }
-        }
-        if(stack2.empty())
-            return -1;
-        int front = stack2.top();
-        stack2.pop();
-        return front;
-    }
-    
-    int peek() {
-        if(stack2.empty()){
-            while (!stack1.empty()){
-                stack2.push(stack1.top());
-                stack1.pop();
-            }
-        }
-        if(stack2.empty())
-            return -1;
-        return stack2.top();
-        
-    }
-    
-    bool empty() {
-        return stack1.empty() && stack2.empty();
-    }
+    if (stack2.empty())
+      return -1;
+    return stack2.top();
+  }
+
+  bool empty() { return stack1.empty() && stack2.empty(); }
 };
 
 /**
