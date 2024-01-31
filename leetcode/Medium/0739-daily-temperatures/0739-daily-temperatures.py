58dcd8a225f59a421af1367ba930fa4eb16badaa
park-jihoo <park-jihoo@users.noreply.github.com>
2024-01-31 05:46:07 +0000

    Code formatting

--- leetcode/Medium/0739-daily-temperatures/0739-daily-temperatures.py
+++ leetcode/Medium/0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,6 +1,6 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        ans = [0]*len(temperatures)
+        ans = [0] * len(temperatures)
         stack = [0]
         for i in range(1, len(temperatures)):
             while stack and temperatures[i] > temperatures[stack[-1]]:
