0c5757c73692514a9f2dd5d68066749ec1f8dcf0
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-02-04 14:14:23 +0900

    Time: 97 ms (72.38%), Space: 17.3 MB (76.65%) - LeetHub

--- leetcode/Hard/0076-minimum-window-substring/0076-minimum-window-substring.py
+++ leetcode/Hard/0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -2,31 +2,28 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if len(s) < len(t):
             return ""
-        hash_t = [0] * 128
-        hash_s = [0] * 128
+        hash_t = [0]*128
+        hash_s = [0]*128
         for i in range(len(t)):
-            hash_t[ord(t[i])] += 1
-
-        start, from_index, min_length = 0, -1, float("inf")
-
+            hash_t[ord(t[i])]+=1
+        
+        start, from_index, min_length = 0, -1, float('inf')
+        
         count = 0
         for i in range(len(s)):
-            hash_s[ord(s[i])] += 1
-            if hash_s[ord(s[i])] <= hash_t[ord(s[i])]:
-                count += 1
+            hash_s[ord(s[i])]+=1
+            if(hash_s[ord(s[i])] <= hash_t[ord(s[i])]):
+                count+=1
             if count == len(t):
-                while (
-                    hash_s[ord(s[start])] > hash_t[ord(s[start])]
-                    or hash_t[ord(s[start])] == 0
-                ):
-                    if hash_s[ord(s[start])] > hash_t[ord(s[start])]:
-                        hash_s[ord(s[start])] -= 1
-                    start += 1
+                while(hash_s[ord(s[start])] > hash_t[ord(s[start])] or hash_t[ord(s[start])]  == 0):
+                    if(hash_s[ord(s[start])]> hash_t[ord(s[start])]):
+                        hash_s[ord(s[start])] -=1
+                    start +=1
                 length = i - start + 1
                 if min_length > length:
                     min_length = length
                     from_index = start
-
+        
         if from_index == -1:
             return ""
-        return s[from_index : from_index + min_length]
+        return s[from_index: from_index+min_length]
