6e93755774561ecceaa7e7273baf08bd54dd3b5a
park-jihoo <park-jihoo@users.noreply.github.com>
2024-02-07 06:28:36 +0000

    Code formatting

--- leetcode/Medium/0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ leetcode/Medium/0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,3 +1,3 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
-        return "".join(x[0]*x[1] for x in Counter(s).most_common())
+        return "".join(x[0] * x[1] for x in Counter(s).most_common())
