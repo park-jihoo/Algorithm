4b222acada329f61e73fea5b02bfdf1ef3aa74d6
park-jihoo <park-jihoo@users.noreply.github.com>
2024-02-10 14:47:56 +0000

    Code formatting

--- leetcode/Medium/0647-palindromic-substrings/0647-palindromic-substrings.py
+++ leetcode/Medium/0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,10 +1,10 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
         n = len(s)
-        dp = [[0]*n for _ in range(n)]
+        dp = [[0] * n for _ in range(n)]
         ans = 0
-        for i in range(n-1, -1, -1):
+        for i in range(n - 1, -1, -1):
             for j in range(i, n):
-                dp[i][j] = (s[i] == s[j]) and ((j-i+1) < 3 or dp[i+1][j-1])
+                dp[i][j] = (s[i] == s[j]) and ((j - i + 1) < 3 or dp[i + 1][j - 1])
                 ans += dp[i][j]
         return ans
