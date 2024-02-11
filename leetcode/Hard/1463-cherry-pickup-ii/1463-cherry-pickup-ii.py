6ff1248c5af112480b63fd4353967c78c3d38334
park-jihoo <park-jihoo@users.noreply.github.com>
2024-02-11 11:00:18 +0000

    Code formatting

--- leetcode/Hard/1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
+++ leetcode/Hard/1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
@@ -1,9 +1,11 @@
 class Solution:
     def cherryPickup(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
+
         @lru_cache(None)
         def dfs(r, c1, c2):
-            if r == m: return 0
+            if r == m:
+                return 0
             cherries = grid[r][c1] if c1 == c2 else grid[r][c1] + grid[r][c2]
             ans = 0
             for nc1 in range(c1 - 1, c1 + 2):
