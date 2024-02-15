2fe41d0b1b0fc5c8c11c30bd40f8b0e02cdd3fbe
park-jihoo <park-jihoo@users.noreply.github.com>
2024-02-15 01:26:16 +0000

    Code formatting

--- leetcode/Medium/2971-find-polygon-with-the-largest-perimeter/2971-find-polygon-with-the-largest-perimeter.py
+++ leetcode/Medium/2971-find-polygon-with-the-largest-perimeter/2971-find-polygon-with-the-largest-perimeter.py
@@ -4,6 +4,6 @@ class Solution:
         if len(nums) == 3 and nums[0] > nums[1] + nums[2]:
             return -1
         for idx in range(len(nums) - 2):
-            if nums[idx] < sum(nums[idx+1:]):
+            if nums[idx] < sum(nums[idx + 1 :]):
                 return sum(nums[idx:])
         return -1
