a88eee805be07e9b57275ae7efbe28fb530da7a3
park-jihoo <park-jihoo@users.noreply.github.com>
2024-02-17 20:45:50 +0000

    Code formatting

--- leetcode/Medium/1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
+++ leetcode/Medium/1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
@@ -2,7 +2,7 @@ class Solution:
     def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
         heap = []
         for i in range(len(heights) - 1):
-            d = heights[i+1] - heights[i]
+            d = heights[i + 1] - heights[i]
             if d > 0:
                 heapq.heappush(heap, d)
             if len(heap) > ladders:
