04a608acb6ea7b44dfa169a58a5d3adfaba244a3
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-02-23 00:50:50 +0900

    Time: 624 ms (29.49%), Space: 21.8 MB (45.77%) - LeetHub

---
+++ leetcode/Easy/0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        graph_a = defaultdict(set)
+        graph_b = defaultdict(set)
+        for a, b in trust:
+            graph_a[a].add(b)
+            graph_b[b].add(a)
+        for i in range(1, n+1):
+            if len(graph_a[i]) == 0 and len(graph_b[i]) == n-1:
+                return i
+        return -1
