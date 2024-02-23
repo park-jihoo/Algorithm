94de04dfbbd979168129c02508aad55a51e71a36
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-02-24 01:21:38 +0900

    Time: 95 ms (72.86%), Space: 17.9 MB (80.88%) - LeetHub

--- leetcode/Medium/0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ leetcode/Medium/0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,24 +1,22 @@
-from collections import defaultdict
-import heapq
-
-
 class Solution:
-    def findCheapestPrice(
-        self, n: int, flights: List[List[int]], src: int, dst: int, k: int
-    ) -> int:
-        graph = defaultdict(list)
-        for start, end, distance in flights:
-            graph[start].append([end, distance])
-        heap = [(0, src, 0)]
-        K = 0
-        visit = {}
-        while heap:
-            cost, city, K = heapq.heappop(heap)
-            if city == dst:
-                return cost
-            if city not in visit or visit[city] > K:
-                visit[city] = K
-                for ci, co in graph[city]:
-                    if K <= k:
-                        heapq.heappush(heap, (cost + co, ci, K + 1))
-        return -1
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        adj = [[] for _ in range(n)]
+        for flight in flights:
+            adj[flight[0]].append((flight[1], flight[2]))
+        
+        q = [(src, 0)]
+        minCost = [float('inf') for _ in range(n)]
+        stops = 0
+        
+        while q and stops <= k:
+            size = len(q)
+            for i in range(size):
+                currNode, cost = q.pop(0)
+                for neighbour, price in adj[currNode]:
+                    if price + cost >= minCost[neighbour]:
+                        continue
+                    minCost[neighbour] = price + cost
+                    q.append((neighbour, minCost[neighbour]))
+            stops += 1
+        
+        return -1 if minCost[dst] == float('inf') else minCost[dst]
