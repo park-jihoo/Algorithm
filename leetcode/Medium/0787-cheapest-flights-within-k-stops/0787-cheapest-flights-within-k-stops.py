ca96a82b1541a9b6fe5317144eb817829abd0882
park-jihoo <park-jihoo@users.noreply.github.com>
2024-02-23 16:22:16 +0000

    Code formatting

--- leetcode/Medium/0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ leetcode/Medium/0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,13 +1,15 @@
 class Solution:
-    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+    def findCheapestPrice(
+        self, n: int, flights: List[List[int]], src: int, dst: int, k: int
+    ) -> int:
         adj = [[] for _ in range(n)]
         for flight in flights:
             adj[flight[0]].append((flight[1], flight[2]))
-        
+
         q = [(src, 0)]
-        minCost = [float('inf') for _ in range(n)]
+        minCost = [float("inf") for _ in range(n)]
         stops = 0
-        
+
         while q and stops <= k:
             size = len(q)
             for i in range(size):
@@ -18,5 +20,5 @@ class Solution:
                     minCost[neighbour] = price + cost
                     q.append((neighbour, minCost[neighbour]))
             stops += 1
-        
-        return -1 if minCost[dst] == float('inf') else minCost[dst]
+
+        return -1 if minCost[dst] == float("inf") else minCost[dst]
