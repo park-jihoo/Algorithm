d8304f8f652fd77185e6a8a71ddc78108c1b0385
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-02-29 04:09:21 +0100

    Time: 233 ms (53.29%), Space: 51.3 MB (14.4%) - LeetHub

---
+++ leetcode/Medium/1609-even-odd-tree/1609-even-odd-tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isEvenOddTree(self, root: Optional[TreeNode]) -> bool:
+        prev = []
+        def dfs(curr, level):
+            if curr is None:
+                return True
+            if curr.val % 2 == level % 2:
+                return False
+            while len(prev) <= level:
+                prev.append(0)
+            if prev[level]!=0 and ((level % 2 == 0 and curr.val <= prev[level]) or (level % 2 == 1 and curr .val >= prev[level])):
+                return False
+            prev[level] = curr.val
+            return dfs(curr.left, level + 1) and dfs(curr.right, level + 1)
+        return dfs(root, 0)
