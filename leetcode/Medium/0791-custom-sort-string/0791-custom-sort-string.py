da9875b4bf5ccdc6043804978d0693dc64926c8d
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-03-11 16:08:29 +0900

    Time: 31 ms (81.57%), Space: 16.6 MB (61.09%) - LeetHub

---
+++ leetcode/Medium/0791-custom-sort-string/0791-custom-sort-string.py
@@ -0,0 +1,5 @@
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        order_map = {c:idx for idx, c in enumerate(order)}
+        sorted_list = sorted(list(s), key=lambda x:order_map[x] if x in order_map else len(s))
+        return "".join(sorted_list)
