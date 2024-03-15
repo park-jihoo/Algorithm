221119710b9dabe1afc3a59688531042a534fa05
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-03-15 16:50:08 +0900

    Time: 1025 ms (5.04%), Space: 44.9 MB (5.73%) - LeetHub

---
+++ leetcode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -0,0 +1,22 @@
+class Solution:
+    def factor(self, num):
+        factors = Counter({})
+        if num < 0:
+            return self.factor(-1*num) + Counter({-1: 1})
+        if num == 0:
+            return Counter({0: 1})
+
+        i = 2
+
+        while i <= num:
+            if num % i == 0:
+                factors[i] += 1
+                num = num / i
+            else:
+                i += 1
+        return factors
+
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        p = sum([self.factor(num) for num in nums], start=Counter())
+        exc = [p - self.factor(num) for num in nums]
+        return [prod(x ** y for x, y in c.items()) for c in exc]
