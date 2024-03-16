23e77fd2607211cf79bef8d6d14e8de20a79f3b1
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-03-16 19:52:41 +0900

    Time: 600 ms (92.27%), Space: 24.3 MB (5.09%) - LeetHub

---
+++ leetcode/Medium/0525-contiguous-array/0525-contiguous-array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def transform(self, num):
+        if num == 1:
+            return 1
+        else:
+            return -1
+    def findMaxLength(self, nums: List[int]) -> int:
+        h = defaultdict(list)
+        s = 0
+        ans = 0
+        for idx in range(len(nums)):
+            s += self.transform(nums[idx])
+            h[s].append(idx)
+        for key, li in h.items():
+            if key == 0:
+                ans = max(ans, li[-1] + 1)
+            elif len(li) > 1:
+                ans = max(ans, li[-1] - li[0])
+        return ans
