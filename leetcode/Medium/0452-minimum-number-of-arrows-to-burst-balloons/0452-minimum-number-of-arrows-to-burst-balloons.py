f092001eef618c2c667ae9a2219fbad57606228b
park-jihoo <park-jihoo@users.noreply.github.com>
2024-03-18 03:34:22 +0000

    Code formatting

--- leetcode/Medium/0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ leetcode/Medium/0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -2,8 +2,8 @@ class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
         # array greedy sorting
         ans, arrow = 0, 0
-        points.sort(key=lambda x:x[1])
+        points.sort(key=lambda x: x[1])
         for s, e in points:
             if ans == 0 or s > arrow:
-                ans, arrow = ans+1, e
+                ans, arrow = ans + 1, e
         return ans
