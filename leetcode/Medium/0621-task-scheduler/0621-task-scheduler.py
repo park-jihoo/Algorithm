6b6a659a4834e5ce1f342f9ad39810e8622d7e45
park-jihoo <park-jihoo@users.noreply.github.com>
2024-03-19 13:17:44 +0000

    Code formatting

--- leetcode/Medium/0621-task-scheduler/0621-task-scheduler.py
+++ leetcode/Medium/0621-task-scheduler/0621-task-scheduler.py
@@ -3,4 +3,4 @@ class Solution:
         d = list(Counter(tasks).values())
         m = max(d)
         cnt = d.count(m)
-        return max(len(tasks), (m-1)*(n+1)+cnt)
+        return max(len(tasks), (m - 1) * (n + 1) + cnt)
