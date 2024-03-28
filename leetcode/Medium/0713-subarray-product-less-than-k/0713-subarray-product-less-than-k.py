2f3f9058c2fe674620f17fccccfd5b213ac75dcd
park-jihoo <park-jihoo@users.noreply.github.com>
2024-03-28 06:19:30 +0000

    Code formatting

--- leetcode/Medium/0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
+++ leetcode/Medium/0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
@@ -2,11 +2,11 @@ class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
         if k <= 1:
             return 0
-        
+
         ans = 0
         prod = 1
         left = 0
-        
+
         for right, num in enumerate(nums):
             prod *= num
             while prod >= k:
