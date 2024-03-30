4299cc454919cfcbf05c466908c13324503424f3
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-03-30 13:38:32 +0900

    Time: 284 ms (93.37%), Space: 19.9 MB (74.32%) - LeetHub

---
+++ leetcode/Hard/0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+        cnt, ans, left, right = defaultdict(int), 0, 0, 0
+        for num in nums:
+            cnt[num] += 1
+            if cnt[num] == 1:
+                k -= 1
+                if k<0:
+                    cnt[nums[right]] = 0
+                    right += 1
+                    left = right
+            if k <= 0:
+                while cnt[nums[right]] > 1:
+                    cnt[nums[right]] -= 1
+                    right += 1
+                ans += right - left + 1
+        return ans
