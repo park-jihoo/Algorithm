7d22b1a079fdf11b5af2f5fbbb4b690e15e9b4fa
park-jihoo <park-jihoo@users.noreply.github.com>
2024-04-03 08:21:36 +0000

    Code formatting

--- leetcode/Medium/0079-word-search/0079-word-search.py
+++ leetcode/Medium/0079-word-search/0079-word-search.py
@@ -1,14 +1,21 @@
 class Solution:
     def dfs(self, board, i, j, word):
-        
+
         adj = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         if len(word) == 0:
             return True
-        if not (0 <= i < len(board) and 0 <= j < len(board[0]) and word[0] == board[i][j]):
+        if not (
+            0 <= i < len(board) and 0 <= j < len(board[0]) and word[0] == board[i][j]
+        ):
             return False
         tmp = board[i][j]
         board[i][j] = "#"
-        res = self.dfs(board, i+1, j, word[1:]) or self.dfs(board, i-1, j, word[1:]) or self.dfs(board, i, j+1, word[1:]) or self.dfs(board, i, j-1, word[1:])
+        res = (
+            self.dfs(board, i + 1, j, word[1:])
+            or self.dfs(board, i - 1, j, word[1:])
+            or self.dfs(board, i, j + 1, word[1:])
+            or self.dfs(board, i, j - 1, word[1:])
+        )
         board[i][j] = tmp
         return res
 
