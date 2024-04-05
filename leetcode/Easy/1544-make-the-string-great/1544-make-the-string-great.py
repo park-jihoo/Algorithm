b955c557acbff01790b64995dc8578459bafc362
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-04-05 19:25:50 +0900

    Time: 35 ms (75.56%), Space: 16.6 MB (46.83%) - LeetHub

---
+++ leetcode/Easy/1544-make-the-string-great/1544-make-the-string-great.py
@@ -0,0 +1,9 @@
+class Solution:
+    def makeGood(self, s: str) -> str:
+        ans = ""
+        if s == "":
+            return ans
+        for i in range(len(s) - 1):
+            if abs(ord(s[i]) - ord(s[i+1])) == 32:
+                return self.makeGood(s[:i]+s[i+2:])
+        return s
