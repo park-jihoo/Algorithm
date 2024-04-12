0deb6b64670a3b7cc31520ec6fef7cec36466b49
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-04-12 13:41:18 +0900

    Time: 2588 ms (5.41%), Space: 19.3 MB (7.16%) - LeetHub

---
+++ leetcode/Hard/0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -0,0 +1,13 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        ans = 0
+        left, right = 0, Counter(height[2:])
+        for idx in range(1, len(height)-1):
+            mid = height[idx]
+            rightmax = max(right.keys())
+            left = max(left, height[idx-1])
+            right[height[idx+1]]-=1
+            if right[height[idx+1]] == 0:
+                del right[height[idx+1]]
+            ans += max(min(left, rightmax) - mid, 0)
+        return ans
