538ac225ab4b155011171b7c4b1e4d4e639b1f6d
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-04-18 15:29:35 +0900

    Time: 405 ms (53.77%), Space: 16.7 MB (99.31%) - LeetHub

---
+++ leetcode/Easy/0463-island-perimeter/0463-island-perimeter.py
@@ -0,0 +1,13 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        ans = 0
+        d = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+        for x in range(len(grid)):
+            for y in range(len(grid[0])):
+                if grid[x][y] == 1:
+                    ans += 4
+                    for dx, dy in d:
+                        if 0<=x+dx<len(grid) and 0<=y+dy<len(grid[0]):
+                            if grid[x+dx][y+dy] == 1:
+                                ans -= 1
+        return ans
