fbe5101888c4309cc1073bca57ed68d5b2461431
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-04-21 00:31:42 +0900

    Time: 755 ms (93%), Space: 30.4 MB (64%) - LeetHub

---
+++ leetcode/Medium/1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findFarmland(self, land: List[List[int]]) -> List[List[int]]:
+        ans = []
+        m, n = len(land), len(land[0])
+
+        for i in range(m):
+            for j in range(n):
+                if land[i][j]==1 and (i  == 0 or land[i - 1][j] == 0) and (j == 0 or land[i][j - 1] == 0):
+                    bottom_row = i
+                    right_col = j
+
+                    while bottom_row + 1 < m and land[bottom_row+1][j] == 1:
+                        bottom_row += 1
+                    while right_col + 1 < n and land[i][right_col+1] == 1:
+                        right_col += 1
+                    ans.append([i, j, bottom_row, right_col])
+
+        return ans
