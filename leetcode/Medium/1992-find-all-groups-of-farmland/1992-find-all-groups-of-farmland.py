ba512ba49bc4625d9cb98ff4be0bb6d1305e575a
park-jihoo <park-jihoo@users.noreply.github.com>
2024-04-20 15:32:27 +0000

    Code formatting

--- leetcode/Medium/1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.py
+++ leetcode/Medium/1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.py
@@ -5,13 +5,17 @@ class Solution:
 
         for i in range(m):
             for j in range(n):
-                if land[i][j]==1 and (i  == 0 or land[i - 1][j] == 0) and (j == 0 or land[i][j - 1] == 0):
+                if (
+                    land[i][j] == 1
+                    and (i == 0 or land[i - 1][j] == 0)
+                    and (j == 0 or land[i][j - 1] == 0)
+                ):
                     bottom_row = i
                     right_col = j
 
-                    while bottom_row + 1 < m and land[bottom_row+1][j] == 1:
+                    while bottom_row + 1 < m and land[bottom_row + 1][j] == 1:
                         bottom_row += 1
-                    while right_col + 1 < n and land[i][right_col+1] == 1:
+                    while right_col + 1 < n and land[i][right_col + 1] == 1:
                         right_col += 1
                     ans.append([i, j, bottom_row, right_col])
 
