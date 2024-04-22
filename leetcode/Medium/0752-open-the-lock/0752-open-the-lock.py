a6eb8505559775b622ae843432c0f0b95a532ad6
park-jihoo <park-jihoo@users.noreply.github.com>
2024-04-22 01:46:47 +0000

    Code formatting

--- leetcode/Medium/0752-open-the-lock/0752-open-the-lock.py
+++ leetcode/Medium/0752-open-the-lock/0752-open-the-lock.py
@@ -1,7 +1,7 @@
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
         # Array/Hash Table/String/BFS
-        graph = {str(x):[str(x+9)[-1], str(x+1)[-1]] for x in range(10)}
+        graph = {str(x): [str(x + 9)[-1], str(x + 1)[-1]] for x in range(10)}
         visit = set(deadends)
         queue = deque([["0000", 0]])
         while queue:
@@ -15,6 +15,6 @@ class Solution:
                     for n in graph[num]:
                         newnode = numlist[:]
                         newnode[idx] = n
-                        queue.append(["".join(newnode), dist+1])
+                        queue.append(["".join(newnode), dist + 1])
 
         return -1
