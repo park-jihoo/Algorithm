cf72bc7f59329395d56ddd961a18e6b444b03928
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-04-22 10:46:06 +0900

    Time: 433 ms (65.58%), Space: 19.1 MB (6.12%) - LeetHub

---
+++ leetcode/Medium/0752-open-the-lock/0752-open-the-lock.py
@@ -0,0 +1,20 @@
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        # Array/Hash Table/String/BFS
+        graph = {str(x):[str(x+9)[-1], str(x+1)[-1]] for x in range(10)}
+        visit = set(deadends)
+        queue = deque([["0000", 0]])
+        while queue:
+            node, dist = queue.popleft()
+            if node == target:
+                return dist
+            numlist = list(node)
+            if node not in visit:
+                visit.add(node)
+                for idx, num in enumerate(numlist):
+                    for n in graph[num]:
+                        newnode = numlist[:]
+                        newnode[idx] = n
+                        queue.append(["".join(newnode), dist+1])
+
+        return -1
