c84510909c36a961158c7c8f5270d554576142ad
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-04-24 23:41:23 +0900

    Time: 42 ms (8.68%), Space: 16.5 MB (31.32%) - LeetHub

---
+++ leetcode/Easy/1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1 or n == 2:
+            return 1
+        a, b, c = 0, 1, 1
+        for i in range(n - 2):
+            a, b, c = b, c, a+b+c
+        return c
+        
