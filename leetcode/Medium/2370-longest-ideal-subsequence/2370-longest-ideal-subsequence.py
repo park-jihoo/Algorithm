fb619ec6355fc00b8eea4a32a3235cf5182a3b5e
park-jihoo <park-jihoo@users.noreply.github.com>
2024-04-25 12:13:00 +0000

    Code formatting

--- leetcode/Medium/2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.py
+++ leetcode/Medium/2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.py
@@ -3,16 +3,16 @@ class Solution:
         n = len(s)
         ans = [1] * n
         charmap = defaultdict(list)
-        
+
         dp = [0] * 26
-        
+
         for idx, char in enumerate(s):
-            curr = ord(char) - ord('a')
-            
+            curr = ord(char) - ord("a")
+
             for i in range(max(curr - k, 0), min(curr + k + 1, 26)):
                 ans[idx] = max(ans[idx], dp[i] + 1)
-            
+
             dp[curr] = max(dp[curr], ans[idx])
             charmap[curr].append(idx)
-        
+
         return max(ans)
