0653a5db8515efe7fc9a23dc9911c0d239fe75d8
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-04-26 13:58:40 +0900

    Time: 225 ms (77.88%), Space: 20.2 MB (34.96%) - LeetHub

---
+++ leetcode/Hard/1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minFallingPathSum(self, grid: List[List[int]]) -> int:
+        # arrays dp matrix
+        n = len(grid)
+        if n == 1:
+            return grid[0][0]
+        temp = [(val, idx) for idx, val in enumerate(grid[0])]
+        dp1 = [x for x in grid[0]]
+        dp2 = [0]*n
+        for curr in range(1, n):
+            a1, a2 = heapq.nsmallest(2, [(val, idx) for idx, val in enumerate(dp1)])
+            for idx, val in enumerate(grid[curr]):
+                if idx == a1[1]:
+                    dp2[idx] = val + dp1[a2[1]]
+                else:
+                    dp2[idx] = val + dp1[a1[1]]
+            dp1, dp2 = dp2, [0]*n
+        return min(dp1)
