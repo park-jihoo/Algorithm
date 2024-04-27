38f4781923c1a005a9703777c84344547c311305
park-jihoo <park-jihoo@users.noreply.github.com>
2024-04-27 07:07:59 +0000

    Code formatting

--- leetcode/Hard/0514-freedom-trail/0514-freedom-trail.py
+++ leetcode/Hard/0514-freedom-trail/0514-freedom-trail.py
@@ -7,12 +7,12 @@ class Solution:
         for idx, val in enumerate(ring):
             matches[val].append(idx)
             if val == key[0]:
-                dp[idx] = 1 + min(idx, n-idx)
+                dp[idx] = 1 + min(idx, n - idx)
         for i in range(1, len(key)):
             dp2 = {}
             for c in matches[key[i]]:
-                dp2[c] = float('inf')
+                dp2[c] = float("inf")
                 for idx, val in dp.items():
-                    dp2[c] = min(dp2[c], 1+min(abs(c-idx), n-abs(c-idx))+val)
+                    dp2[c] = min(dp2[c], 1 + min(abs(c - idx), n - abs(c - idx)) + val)
             dp = dp2
         return min(dp.values())
