118b752c6e207c898abcb7130ca2ff4635afbd36
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-04-29 14:57:33 +0900

    Time: 611 ms (13.38%), Space: 30.9 MB (81.85%) - LeetHub

--- leetcode/Medium/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.py
+++ leetcode/Medium/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.py
@@ -1,4 +1,4 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
         xor = reduce(lambda a, b: a^b, nums)
-        return reduce(lambda a, b: int(a)+int(b),list(bin(xor^k)[2:]),0)
+        return reduce(lambda a, b: int(a)+int(b),bin(xor^k)[2:],0)
