8da631d4eec04d6632b62828f300eb04b0e32d61
park-jihoo <park-jihoo@users.noreply.github.com>
2024-05-01 14:06:40 +0000

    Code formatting

--- leetcode/Easy/2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
+++ leetcode/Easy/2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
@@ -2,6 +2,6 @@ class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
         try:
             idx = word.index(ch)
-            return word[idx::-1] + word[idx+1:]
+            return word[idx::-1] + word[idx + 1 :]
         except:
             return word
