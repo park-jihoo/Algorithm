8547b7d6a8c966cb82891d5c960b844b7945e7f7
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-05-08 13:11:32 +0900

    Time: 68 ms (47.32%), Space: 18 MB (10.4%) - LeetHub

---
+++ leetcode/Easy/0506-relative-ranks/0506-relative-ranks.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findRelativeRanks(self, score: List[int]) -> List[str]:
+        score_heap = []
+        for idx, val in enumerate(score):
+            heapq.heappush(score_heap, (-val, idx))
+        ans = [0] * len(score)
+        for i in range(len(score)):
+            val, idx = heapq.heappop(score_heap)
+            if i == 0:
+                ans[idx] = "Gold Medal"
+            elif i == 1:
+                ans[idx] = "Silver Medal"
+            elif i == 2:
+                ans[idx] = "Bronze Medal"
+            else:
+                ans[idx] = str(i+1)
+        return ans
