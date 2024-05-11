f667a53853cf048d39e7012c4022c32174a141f1
park-jihoo <park-jihoo@users.noreply.github.com>
2024-05-11 06:36:30 +0000

    Code formatting

--- leetcode/Hard/0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.py
+++ leetcode/Hard/0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.py
@@ -1,7 +1,9 @@
 class Solution:
-    def mincostToHireWorkers(self, quality: List[int], wage: List[int], k: int) -> float:
-        workers = sorted([float(w)/q, q] for w, q in zip(wage, quality))
-        ans = float('inf')
+    def mincostToHireWorkers(
+        self, quality: List[int], wage: List[int], k: int
+    ) -> float:
+        workers = sorted([float(w) / q, q] for w, q in zip(wage, quality))
+        ans = float("inf")
         qsum = 0
         heap = []
         for r, q in workers:
