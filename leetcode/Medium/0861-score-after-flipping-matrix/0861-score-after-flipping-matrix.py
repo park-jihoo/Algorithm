1a3f16cb2b2a4d3f8cca3487b801ecd3db0f84f1
park-jihoo <park-jihoo@users.noreply.github.com>
2024-05-13 04:48:35 +0000

    Code formatting

--- leetcode/Medium/0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.py
+++ leetcode/Medium/0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.py
@@ -1,6 +1,7 @@
 class Solution:
     def d(self, x):
-        return 1-x
+        return 1 - x
+
     def matrixScore(self, grid: List[List[int]]) -> int:
         # 제일 왼쪽이 0 아닌거부터 해서 row로 바꾸고, col의 경우는 다수인 경우로 둠
         for idx, row in enumerate(grid):
@@ -8,7 +9,7 @@ class Solution:
                 grid[idx] = list(map(self.d, row))
         a = 1
         ans = 0
-        for col in range(len(grid[0])-1, -1, -1):
+        for col in range(len(grid[0]) - 1, -1, -1):
             score = sum([grid[x][col] for x in range(len(grid))])
             ans += a * max(score, len(grid) - score)
             a *= 2
