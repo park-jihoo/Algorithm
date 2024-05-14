549ec1a1eb11570bc060537e3604999c9cf692ba
park-jihoo <park-jihoo@users.noreply.github.com>
2024-05-14 06:04:46 +0000

    Code formatting

--- leetcode/Medium/1331-path-with-maximum-gold/1331-path-with-maximum-gold.py
+++ leetcode/Medium/1331-path-with-maximum-gold/1331-path-with-maximum-gold.py
@@ -2,16 +2,18 @@ class Solution:
     def getMaximumGold(self, grid: List[List[int]]) -> int:
         d = [(-1, 0), (1, 0), (0, 1), (0, -1)]
         m, n = len(grid), len(grid[0])
+
         def dfs(r, c):
-            if r < 0 or r == m or c < 0 or c == n or grid[r][c] ==0:
+            if r < 0 or r == m or c < 0 or c == n or grid[r][c] == 0:
                 return 0
             ans = 0
             org = grid[r][c]
-            grid[r][c]=0
+            grid[r][c] = 0
             for dr, dc in d:
-                ans = max(ans, dfs(r+dr, c+dc))
+                ans = max(ans, dfs(r + dr, c + dc))
             grid[r][c] = org
             return ans + grid[r][c]
+
         ans = 0
         for i in range(m):
             for j in range(n):
