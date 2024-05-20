8b860202f141be4064a505d8b532e519a08c0092
park-jihoo <park-jihoo@users.noreply.github.com>
2024-05-20 02:32:59 +0000

    Code formatting

--- leetcode/Easy/1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
+++ leetcode/Easy/1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
@@ -1,6 +1,6 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        ans = reduce(lambda x, y:x|y, nums)
+        ans = reduce(lambda x, y: x | y, nums)
         # 00 + (01) + (11) + (01 ^ 11)
         # 00 + 01 + 11 + 10 => 11 * 2 = 110
         # 000 + 101 + 001 + 110 + 101 ^ 001 + 101 ^ 110 + 001 ^ 110 + 101 ^ 001 ^ 110
