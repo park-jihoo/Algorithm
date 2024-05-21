ff7002624e807f62a7081134e3261ed787c13971
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-05-21 20:50:11 +0900

    Time: 34 ms (77.09%), Space: 17 MB (8.15%) - LeetHub

---
+++ leetcode/Medium/0078-subsets/0078-subsets.py
@@ -0,0 +1,11 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        n = len(nums)
+        for i in range(2 ** n):
+            sub = []
+            for j in range(n):
+                if (i>>j)%2 == 1:
+                    sub.append(nums[j])
+            ans.append(sub)
+        return ans
