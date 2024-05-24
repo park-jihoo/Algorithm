7bca7333d8485c1d9e286a29078a280241db15e3
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-05-24 16:00:23 +0900

    Time: 1100 ms (5.21%), Space: 30.1 MB (6.35%) - LeetHub

---
+++ leetcode/Hard/1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.py
@@ -0,0 +1,26 @@
+class Solution:
+    def subsets(self, words):
+        ans = []
+        n = len(words)
+        for i in range(2 ** n):
+            sub = Counter()
+            for j in range(n):
+                if (i>>j)%2 == 1:
+                    sub += Counter(words[j])
+            ans.append(sub)
+        return ans
+    
+    def getScore(self, word, score):
+        ans = 0
+        for key, val in word.items():
+            ans += score[ord(key) - ord('a')]*val
+        return ans
+
+    def maxScoreWords(self, words: List[str], letters: List[str], score: List[int]) -> int:
+        letter_count = Counter(letters)
+        words_subset = self.subsets(words)
+        ans = 0
+        for word in words_subset:
+            if word <= letter_count:
+                ans = max(ans, self.getScore(word, score))
+        return ans
