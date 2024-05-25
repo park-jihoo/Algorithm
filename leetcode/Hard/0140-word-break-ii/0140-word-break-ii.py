cf1a3560602526da94445a5047f0561ba5d8d844
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-05-25 16:27:31 +0900

    Time: 30 ms (86.5%), Space: 16.5 MB (87.62%) - LeetHub

---
+++ leetcode/Hard/0140-word-break-ii/0140-word-break-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def wordList(self, s, wordDict, memo):
+        if s in memo:
+            return memo[s]
+        if not s:
+            return []
+        ans = []
+        for word in wordDict:
+            if not s.startswith(word):
+                continue
+            if len(word) == len(s):
+                ans.append(word)
+            else:
+                rst = self.wordList(s[len(word): ], wordDict, memo)
+                for item in rst:
+                    item = word + ' ' + item
+                    ans.append(item)
+        memo[s] = ans
+        return ans 
+
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        slist = self.wordList(s, wordDict, {})
+        return slist
