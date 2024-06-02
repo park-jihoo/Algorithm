74fdc22c4f2e7b3b39c103ad88825a0ff14fcc90
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-06-02 09:04:59 +0900

    Time: 178 ms (13.1%), Space: 21.1 MB (32.38%) - LeetHub

--- leetcode/Easy/0344-reverse-string/0344-reverse-string.py
+++ leetcode/Easy/0344-reverse-string/0344-reverse-string.py
@@ -5,6 +5,6 @@ class Solution:
         """
         i, j = 0, len(s) - 1
         while i < j:
-            s[i], s[j] = s[j], s[i]
-            i += 1
-            j -= 1
+            s[i],s[j] = s[j], s[i]
+            i +=1
+            j -=1
