de6e2d8206e83ebbb0bfdf4fb61bf89e0ce2547c
park-jihoo <park-jihoo@users.noreply.github.com>
2024-06-05 05:44:53 +0000

    Code formatting

--- leetcode/Easy/1002-find-common-characters/1002-find-common-characters.py
+++ leetcode/Easy/1002-find-common-characters/1002-find-common-characters.py
@@ -1,4 +1,4 @@
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
-        ans = reduce(lambda a, b:Counter(a) & Counter(b),words,Counter(words[0]))
+        ans = reduce(lambda a, b: Counter(a) & Counter(b), words, Counter(words[0]))
         return ans.elements()
