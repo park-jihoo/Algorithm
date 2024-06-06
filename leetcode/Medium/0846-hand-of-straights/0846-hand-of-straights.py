5c3f0fa5989f6c05750b22f9f4b2a48ba17ccdec
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-06-06 13:01:13 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ leetcode/Medium/0846-hand-of-straights/0846-hand-of-straights.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0:
+            return False
+        c = Counter(hand)
+        while c:
+            num = min(c)
+            cons = Counter(range(num, num+groupSize))
+            print(cons, c)
+            if len(cons - c) > 0:
+                return False
+            c -= cons
+
+        return True
