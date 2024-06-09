31a5f5727051a0221ac6ee13becc66d713b9cdbf
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-06-09 14:13:57 +0900

    Time: 254 ms (5.25%), Space: 21.5 MB (55.53%) - LeetHub

--- leetcode/Medium/0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
+++ leetcode/Medium/0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
@@ -1,14 +1,14 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
         prefix = 0
-        mod_seen = defaultdict(list)
-        mod_seen[0] = [-1]
+        mod_seen = defaultdict(int)
+        mod_seen[0] = 1
         ans = 0
         for i in range(len(nums)):
             prefix += nums[i]
             prefix %= k
             if prefix in mod_seen:
-                if len(mod_seen[prefix]) > 0:
-                    ans += len(mod_seen[prefix])
-            mod_seen[prefix].append(i)
+                if mod_seen[prefix] > 0:
+                    ans += mod_seen[prefix]
+            mod_seen[prefix] += 1
         return ans
