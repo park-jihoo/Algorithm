478cf1a3e1f82619dcdb62357bcaf12921588eb1
park-jihoo <park-jihoo@users.noreply.github.com>
2024-06-10 04:29:31 +0000

    Code formatting

--- leetcode/Easy/1051-height-checker/1051-height-checker.py
+++ leetcode/Easy/1051-height-checker/1051-height-checker.py
@@ -1,3 +1,3 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
-        return sum(int(x!=y) for x,y in zip(heights, sorted(heights)))
+        return sum(int(x != y) for x, y in zip(heights, sorted(heights)))
