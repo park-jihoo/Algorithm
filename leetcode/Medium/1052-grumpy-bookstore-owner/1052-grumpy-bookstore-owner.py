3e12c21b9a073856950c88986f306617bcbda02d
park-jihoo <park-jihoo@users.noreply.github.com>
2024-06-21 07:23:08 +0000

    Code formatting

--- leetcode/Medium/1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
+++ leetcode/Medium/1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
@@ -1,8 +1,14 @@
 class Solution:
-    def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
-        cur = sum(customers[x] for x in range(len(customers)) if grumpy[x] == 0 or x < minutes)
+    def maxSatisfied(
+        self, customers: List[int], grumpy: List[int], minutes: int
+    ) -> int:
+        cur = sum(
+            customers[x] for x in range(len(customers)) if grumpy[x] == 0 or x < minutes
+        )
         ans = cur
         for i in range(len(customers) - minutes):
-            cur += grumpy[i+minutes] * customers[i + minutes] - grumpy[i] * customers[i]
+            cur += (
+                grumpy[i + minutes] * customers[i + minutes] - grumpy[i] * customers[i]
+            )
             ans = max(cur, ans)
         return ans
