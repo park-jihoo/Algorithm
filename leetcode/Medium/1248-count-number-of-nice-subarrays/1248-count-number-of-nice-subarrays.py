f1554f68af77bf1dfd408c0b1cf32f30685a9f8c
park-jihoo <park-jihoo@users.noreply.github.com>
2024-06-22 15:49:42 +0000

    Code formatting

--- leetcode/Medium/1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ leetcode/Medium/1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -1,9 +1,9 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        d = {0:1}
+        d = {0: 1}
         cnt, ans = 0, 0
         for idx, num in enumerate(nums):
-            cnt += (num % 2)
+            cnt += num % 2
             if cnt - k in d:
                 ans += d[cnt - k]
             d[cnt] = d.get(cnt, 0) + 1
