7a27be454142a85435fb5c24ab66fe5ad3364359
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-06-25 13:29:19 +0900

    Time: 35 ms (64.07%), Space: 16.5 MB (31.32%) - LeetHub

---
+++ leetcode/Medium/1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        self.helper(root, [0])
+        return root
+    
+    def helper(self, root, node_sum):
+        if root is None:
+            return
+        self.helper(root.right, node_sum)
+        node_sum[0] += root.val
+        root.val = node_sum[0]
+        self.helper(root.left, node_sum)
