d96577f12b4096b3b8282706f4a619d4493a03e4
park-jihoo <park-jihoo@users.noreply.github.com>
2024-06-26 12:02:15 +0000

    Code formatting

--- SWEA/D5/1265. ［S／W 문제해결 응용］ 9일차 － 달란트2/［S／W 문제해결 응용］ 9일차 － 달란트2.cpp
+++ SWEA/D5/1265. ［S／W 문제해결 응용］ 9일차 － 달란트2/［S／W 문제해결 응용］ 9일차 － 달란트2.cpp
@@ -1,26 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(int argc, char** argv)
-{
-	int test_case;
-	int T;
+int main(int argc, char **argv) {
+  int test_case;
+  int T;
 
-	cin>>T;
+  cin >> T;
 
-	for(test_case = 1; test_case <= T; ++test_case)
-	{
-        int amount, num;
-        long long ans=1;
-        cin>>amount;
-        cin>>num;
-        for(int i=0;i<num;i++){
-            if(i<amount%num)
-           ans=ans*(amount/num+1);
-            else
-                ans=ans*(amount/num);
-        }
-        cout<<"#"<<test_case<<" "<<ans<<endl;
-	}
-	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+  for (test_case = 1; test_case <= T; ++test_case) {
+    int amount, num;
+    long long ans = 1;
+    cin >> amount;
+    cin >> num;
+    for (int i = 0; i < num; i++) {
+      if (i < amount % num)
+        ans = ans * (amount / num + 1);
+      else
+        ans = ans * (amount / num);
+    }
+    cout << "#" << test_case << " " << ans << endl;
+  }
+  return 0; //정상종료시 반드시 0을 리턴해야합니다.
 }
