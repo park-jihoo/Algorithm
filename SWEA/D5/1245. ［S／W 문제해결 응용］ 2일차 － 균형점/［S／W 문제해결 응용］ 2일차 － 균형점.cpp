1ef383438035d53ad259f558377736a331c518e0
park-jihoo <park-jihoo@users.noreply.github.com>
2024-06-29 11:27:19 +0000

    Code formatting

--- SWEA/D5/1245. ［S／W 문제해결 응용］ 2일차 － 균형점/［S／W 문제해결 응용］ 2일차 － 균형점.cpp
+++ SWEA/D5/1245. ［S／W 문제해결 응용］ 2일차 － 균형점/［S／W 문제해결 응용］ 2일차 － 균형점.cpp
@@ -1,79 +1,75 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
-double left(vector<int>x, vector<int>we, double dot){
-    double ans=0;
-    for(int i=0;i<x.size();i++){
-        if(x[i]>dot)
-            break;
-        else
-            ans+=((we[i]/((x[i]-dot)*(x[i]-dot))));    
-    }
-    return ans;
+double left(vector<int> x, vector<int> we, double dot) {
+  double ans = 0;
+  for (int i = 0; i < x.size(); i++) {
+    if (x[i] > dot)
+      break;
+    else
+      ans += ((we[i] / ((x[i] - dot) * (x[i] - dot))));
+  }
+  return ans;
 }
 
-double right(vector<int>x, vector<int>we, double dot){
-    double ans=0;
-    for(int i=x.size()-1;i>=0;i--){
-        if(x[i]<dot)
-            break;
-        else
-            ans+=((we[i]/((x[i]-dot)*(x[i]-dot))));    
-    }
-    return ans;
+double right(vector<int> x, vector<int> we, double dot) {
+  double ans = 0;
+  for (int i = x.size() - 1; i >= 0; i--) {
+    if (x[i] < dot)
+      break;
+    else
+      ans += ((we[i] / ((x[i] - dot) * (x[i] - dot))));
+  }
+  return ans;
 }
 
-int main(int argc, char** argv)
-{
-	int test_case;
-	int T;
-	cin>>T;
-	for(test_case = 1; test_case <= T; ++test_case)
-	{
-        int num;
-        vector<int>x;
-        vector<int>we;
-        cin>>num;
-        for(int i=0;i<num;i++){
-            int a;
-            cin>>a;
-            x.push_back(a);
+int main(int argc, char **argv) {
+  int test_case;
+  int T;
+  cin >> T;
+  for (test_case = 1; test_case <= T; ++test_case) {
+    int num;
+    vector<int> x;
+    vector<int> we;
+    cin >> num;
+    for (int i = 0; i < num; i++) {
+      int a;
+      cin >> a;
+      x.push_back(a);
+    }
+    for (int i = 0; i < num; i++) {
+      int a;
+      cin >> a;
+      we.push_back(a);
+    }
+    vector<double> ans;
+    for (int i = 0; i < num - 1; i++) {
+      double a;
+      double start, mid, end;
+      start = x[i];
+      end = x[i + 1];
+      for (int j = 0; j < 50; j++) {
+        mid = (start + end) / 2;
+        if (left(x, we, mid) == right(x, we, mid)) {
+          a = mid;
+          break;
+        } else if (left(x, we, mid) > right(x, we, mid)) {
+          start = mid;
+        } else {
+          end = mid;
         }
-        for(int i=0;i<num;i++){
-            int a;
-            cin>>a;
-            we.push_back(a);
-        }
-       vector<double>ans;
-        for(int i=0;i<num-1;i++){
-            double a;
-            double start, mid, end;
-            start=x[i];
-            end=x[i+1];
-            for(int j=0;j<50;j++){
-                mid=(start+end)/2;
-                if(left(x, we, mid)==right(x, we, mid)){
-                    a=mid;
-                    break;
-                }
-                else if(left(x,we,mid)>right(x,we,mid)){
-                    start=mid;
-                }
-                else{
-                    end=mid;
-                }
-                if((end-start)/2<1.e-10)
-                    a=(start+end)/2;
-            }
-            ans.push_back(a);
-        }
-        cout<<fixed;
-        cout.precision(10);
-        cout<<"#"<<test_case;
-        for(int i=0;i<ans.size();i++)
-            cout<<" "<<ans[i];
-        cout<<endl;
-	}
-	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+        if ((end - start) / 2 < 1.e-10)
+          a = (start + end) / 2;
+      }
+      ans.push_back(a);
+    }
+    cout << fixed;
+    cout.precision(10);
+    cout << "#" << test_case;
+    for (int i = 0; i < ans.size(); i++)
+      cout << " " << ans[i];
+    cout << endl;
+  }
+  return 0; //정상종료시 반드시 0을 리턴해야합니다.
 }
