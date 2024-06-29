5e93a0aade7f4900697a877bdace0b6193d67d52
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-06-29 20:24:41 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벌꿀채취, Time: 26 ms, Memory: 13,544 KB -BaekjoonHub

---
+++ SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/README.md
@@ -0,0 +1,15 @@
+# [Unrated] [모의 SW 역량테스트] 벌꿀채취 - 2115 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5V4A46AdIDFAWu) 
+
+### 성능 요약
+
+메모리: 13,544 KB, 시간: 26 ms, 코드길이: 1,299 Bytes
+
+### 제출 일자
+
+2024-06-29 20:24
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.cpp
@@ -0,0 +1,57 @@
+
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int N, M, C, res;
+int honey[10][10];
+int max(int a, int b){
+    if(a>=b){return a;}
+    else{return b;}
+}
+void getmaxprice(int x, int y, int cnt, int sum, int price){
+    if(sum>C)return;
+    res=max(res, price);
+    if(cnt==M)return;
+    getmaxprice(x, y+1, cnt+1, sum+honey[x][y], price+honey[x][y]*honey[x][y]);
+    getmaxprice(x, y+1, cnt+1, sum, price);
+}
+int solve(int x, int y){
+    res=0;
+    getmaxprice(x, y, 0, 0, 0);
+    int priceA=res;
+    int priceB=0;
+    int j=y+M;
+    for(int i=x;i<N;i++, j=0){
+        for(;j<N-M+1;j++){
+            res=0;
+            getmaxprice(i, j, 0, 0, 0);
+            priceB=max(res, priceB);
+        }
+    }
+    return priceA+priceB;
+}
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	cin>>T;
+	for(test_case = 1; test_case <= T; ++test_case)
+	{
+        int maxprice=0;
+        cin>>N;
+        cin>>M;
+        cin>>C;
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                cin>>honey[i][j];
+            }
+        }
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N-M+1;j++){
+                maxprice=max(solve(i, j), maxprice);
+            }
+        }
+        printf("#%d %d\n", test_case, maxprice);
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
