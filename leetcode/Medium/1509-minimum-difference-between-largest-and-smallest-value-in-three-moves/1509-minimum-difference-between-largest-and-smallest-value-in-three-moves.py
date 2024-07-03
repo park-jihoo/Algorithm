7fe57d124c4ab18e90076ad9a12d35745b579af3
park-jihoo <park-jihoo@users.noreply.github.com>
2024-07-03 04:49:30 +0000

    Code formatting

--- leetcode/Medium/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
+++ leetcode/Medium/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
@@ -3,7 +3,7 @@ class Solution:
         if len(nums) <= 4:
             return 0
         nums.sort()
-        ans = float('inf')
+        ans = float("inf")
         for i in range(4):
             ans = min(ans, nums[len(nums) - i - 1] - nums[3 - i])
         return ans
