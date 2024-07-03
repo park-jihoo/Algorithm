49ae347f80f1ac00f8dcd1a9f2e64139948b8ebd
park-jihoo <park-jihoo@users.noreply.github.com>
2024-07-03 18:01:23 +0000

    Code formatting

--- SWEA/Unrated/4317. ［연습문제］ 칩 생산/［연습문제］ 칩 생산.py
+++ SWEA/Unrated/4317. ［연습문제］ 칩 생산/［연습문제］ 칩 생산.py
@@ -3,23 +3,28 @@ t = int(input())
 
 def check(x, y):
     if 0 <= x < h - 1 and 0 <= y < w - 1:
-        if arr[x][y] == 0 and arr[x + 1][y] == 0 and arr[x][y + 1] == 0 and arr[x + 1][y + 1] == 0:
+        if (
+            arr[x][y] == 0
+            and arr[x + 1][y] == 0
+            and arr[x][y + 1] == 0
+            and arr[x + 1][y + 1] == 0
+        ):
             return True
     return False
 
 
 def dfs(x, y, cnt):
     global ans
-    if x >= h-1:
+    if x >= h - 1:
         x = 0
         y += 1
-    if y >= w-1:
+    if y >= w - 1:
         ans = max(ans, cnt)
         return
     if x == 0:
         bit = 0
         for i in range(h):
-            bit |= (arr[i][y] << i)
+            bit |= arr[i][y] << i
         if dp[bit][y] >= cnt:
             return
         dp[bit][y] = cnt
@@ -36,4 +41,4 @@ for test_case in range(1, t + 1):
     dp = [[-1] * w for _ in range(1 << h)]
     ans = 0
     dfs(0, 0, 0)
-    print(f'#{test_case} {ans}')
+    print(f"#{test_case} {ans}")
