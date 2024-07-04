51cf03491096198495674527e1af0b87800d6257
park-jihoo <park-jihoo@users.noreply.github.com>
2024-07-04 09:50:27 +0000

    Code formatting

--- 백준/Gold/22860. 폴더 정리 （small）/폴더 정리 （small）.py
+++ 백준/Gold/22860. 폴더 정리 （small）/폴더 정리 （small）.py
@@ -5,9 +5,9 @@ n, m = map(int, input().split())
 
 folders, files = defaultdict(list), defaultdict(list)
 
-for _ in range(n+m):
+for _ in range(n + m):
     p, f, c = input().split()
-    if c == '1':
+    if c == "1":
         folders[p].append(f)
     else:
         files[p].append(f)
@@ -15,7 +15,7 @@ for _ in range(n+m):
 q = int(input())
 
 for _ in range(q):
-    query = input().split('/')
+    query = input().split("/")
     queue = deque([query[-1]])
     file = []
     while queue:
@@ -24,4 +24,4 @@ for _ in range(q):
             queue.extend(folders[cur])
         if cur in files:
             file.extend(files[cur])
-    print(f'{len(set(file))} {len(file)}')
+    print(f"{len(set(file))} {len(file)}")
