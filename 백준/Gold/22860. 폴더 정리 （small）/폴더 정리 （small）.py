e4296e8c8ca94e506323e062e41d6f0c3b68b0aa
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-07-04 18:49:48 +0900

    [Gold III] Title: 폴더 정리 (small), Time: 552 ms, Memory: 34028 KB -BaekjoonHub

---
+++ 백준/Gold/22860. 폴더 정리 （small）/폴더 정리 （small）.py
@@ -0,0 +1,27 @@
+# acmicpc 22860
+from collections import defaultdict, deque
+
+n, m = map(int, input().split())
+
+folders, files = defaultdict(list), defaultdict(list)
+
+for _ in range(n+m):
+    p, f, c = input().split()
+    if c == '1':
+        folders[p].append(f)
+    else:
+        files[p].append(f)
+
+q = int(input())
+
+for _ in range(q):
+    query = input().split('/')
+    queue = deque([query[-1]])
+    file = []
+    while queue:
+        cur = queue.popleft()
+        if cur in folders:
+            queue.extend(folders[cur])
+        if cur in files:
+            file.extend(files[cur])
+    print(f'{len(set(file))} {len(file)}')
