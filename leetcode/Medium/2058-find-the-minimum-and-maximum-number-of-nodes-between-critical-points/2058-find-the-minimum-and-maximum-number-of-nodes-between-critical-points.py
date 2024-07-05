53df24cec4a8dbd0b7b3787644b60aa60daf7663
park-jihoo <park-jihoo@users.noreply.github.com>
2024-07-05 07:02:58 +0000

    Code formatting

--- leetcode/Medium/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
+++ leetcode/Medium/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -7,9 +7,11 @@ class Solution:
     def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
         ans = []
         curr, idx = head, 1
-        minval = float('inf')
+        minval = float("inf")
         while curr.next.next:
-            if (curr.val < curr.next.val and curr.next.val > curr.next.next.val) or (curr.val > curr.next.val and curr.next.val < curr.next.next.val):
+            if (curr.val < curr.next.val and curr.next.val > curr.next.next.val) or (
+                curr.val > curr.next.val and curr.next.val < curr.next.next.val
+            ):
                 if ans:
                     minval = min(minval, idx - ans[-1])
                 ans.append(idx)
