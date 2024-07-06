21e50511233f2fa1d79154a54827476517b773ba
park-jihoo <park-jihoo@users.noreply.github.com>
2024-07-06 11:07:08 +0000

    Code formatting

--- leetcode/Easy/2582-pass-the-pillow/2582-pass-the-pillow.py
+++ leetcode/Easy/2582-pass-the-pillow/2582-pass-the-pillow.py
@@ -2,5 +2,5 @@ class Solution:
     def passThePillow(self, n: int, time: int) -> int:
         if time == 1:
             return 2
-        x = (time % ((n - 1)*2))
-        return min(2*n-1-x, x+1)
+        x = time % ((n - 1) * 2)
+        return min(2 * n - 1 - x, x + 1)
