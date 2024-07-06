b84fc3638b6e24c9b88d0539f0a26803e800d151
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-07-06 20:06:29 +0900

    Time: 32 ms (72.41%), Space: 16.7 MB (7.09%) - LeetHub

---
+++ leetcode/Easy/2582-pass-the-pillow/2582-pass-the-pillow.py
@@ -0,0 +1,6 @@
+class Solution:
+    def passThePillow(self, n: int, time: int) -> int:
+        if time == 1:
+            return 2
+        x = (time % ((n - 1)*2))
+        return min(2*n-1-x, x+1)
