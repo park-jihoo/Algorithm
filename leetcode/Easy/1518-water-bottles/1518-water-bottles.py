0ae69ae83e4ab2415a618e192f95c80e54c01176
park-jihoo <park-jihoo@users.noreply.github.com>
2024-07-07 07:07:13 +0000

    Code formatting

--- leetcode/Easy/1518-water-bottles/1518-water-bottles.py
+++ leetcode/Easy/1518-water-bottles/1518-water-bottles.py
@@ -1,7 +1,7 @@
 class Solution:
     def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
         remain, drank, empty = numBottles, numBottles, 0
-        while remain >= numExchange: 
+        while remain >= numExchange:
             remain, empty = divmod(remain, numExchange)
             drank += remain
             remain += empty
