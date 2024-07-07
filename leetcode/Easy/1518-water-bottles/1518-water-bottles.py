c5be84c146cefeb023c292d7d9b9ee1df0b4b5d1
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-07-07 16:06:33 +0900

    Time: 43 ms (5.97%), Space: 16.4 MB (97.5%) - LeetHub

---
+++ leetcode/Easy/1518-water-bottles/1518-water-bottles.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        remain, drank, empty = numBottles, numBottles, 0
+        while remain >= numExchange: 
+            remain, empty = divmod(remain, numExchange)
+            drank += remain
+            remain += empty
+        return drank
