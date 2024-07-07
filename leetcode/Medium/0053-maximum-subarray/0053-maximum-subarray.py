c15be39c2c30deb17721f59e5de4428a9c43a1af
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-07-07 16:12:22 +0900

    Time: 548 ms (33.08%), Space: 31 MB (25.43%) - LeetHub

---
+++ leetcode/Medium/0053-maximum-subarray/0053-maximum-subarray.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # Kadane's algorithm
+        ans = nums[0]
+        for i in range(1, len(nums)):
+            nums[i] = max(nums[i], nums[i] + nums[i-1])
+            ans = max(ans, nums[i])
+        return ans
