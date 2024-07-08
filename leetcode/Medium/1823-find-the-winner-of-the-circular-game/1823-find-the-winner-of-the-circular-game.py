e3047ca42dcf4f9ce8a727fde7b2ac43916df884
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-07-08 09:15:59 +0900

    Time: 222 ms (9.62%), Space: 16.7 MB (18.56%) - LeetHub

---
+++ leetcode/Medium/1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        ppl, curr = deque(list(range(1, n+1))), 0
+        while len(ppl) > 1:
+            curr += 1
+            can = ppl.popleft()
+            if curr != k:
+                ppl.append(can)
+            else:
+                curr = 0
+        return ppl[0]
