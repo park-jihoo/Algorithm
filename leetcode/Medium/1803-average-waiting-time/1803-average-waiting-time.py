04040f1c63ea147f60fbb4a39ae24fdb72f0bc42
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-07-09 20:19:40 +0900

    Time: 749 ms (57.04%), Space: 57.3 MB (9.97%) - LeetHub

---
+++ leetcode/Medium/1803-average-waiting-time/1803-average-waiting-time.py
@@ -0,0 +1,7 @@
+class Solution:
+    def averageWaitingTime(self, customers: List[List[int]]) -> float:
+        n, t, ans = len(customers), 0, 0
+        for start, wait in customers:
+            t = max(t + wait, start + wait)
+            ans += (t - start)/n
+        return ans
