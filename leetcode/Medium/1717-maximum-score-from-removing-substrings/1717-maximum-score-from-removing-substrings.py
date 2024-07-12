8c36f0c8bed54f0d367733a0c488194129e28244
park-jihoo <park-jihoo@users.noreply.github.com>
2024-07-12 03:52:07 +0000

    Code formatting

--- leetcode/Medium/1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.py
+++ leetcode/Medium/1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.py
@@ -1,14 +1,14 @@
 class Solution:
     def maximumGain(self, s: str, x: int, y: int) -> int:
-        a = 'a'
-        b = 'b'
+        a = "a"
+        b = "b"
         if x < y:
             x, y = y, x
             a, b = b, a
         seen = Counter()
         ans = 0
-        for c in s + 'x':
-            if c in 'ab':
+        for c in s + "x":
+            if c in "ab":
                 if c == b and 0 < seen[a]:
                     ans += x
                     seen[a] -= 1
