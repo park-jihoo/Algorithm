3cbbb6f4bdeed788dd6ca678023610eb4b64f980
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-07-15 15:37:51 +0900

    Time: 1596 ms (39.73%), Space: 26.1 MB (37.37%) - LeetHub

---
+++ leetcode/Medium/2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def createBinaryTree(self, descriptions: List[List[int]]) -> Optional[TreeNode]:
+        visited = set()
+        m = dict()
+        for p, c, l in descriptions:
+            np, nc = m.setdefault(p, TreeNode(p)), m.setdefault(c, TreeNode(c))
+            if l:
+                np.left = nc
+            else:
+                np.right=nc
+            visited.add(c)
+        root = (set(m) - visited).pop()
+        return m[root]
