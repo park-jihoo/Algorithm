5b9972c0ad39b3e61cea1551e19103e0db88252c
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-07-16 15:06:04 +0900

    Time: 574 ms (42.11%), Space: 50.2 MB (94.13%) - LeetHub

---
+++ leetcode/Medium/2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getFromRoot(self, root, val):
+        visited = set()
+        stack = [(root, '')]
+        while stack:
+            node, d = stack.pop()
+            if node.val == val:
+                return d
+            if node.left:
+                stack.append((node.left, d +'L'))
+            if node.right:
+                stack.append((node.right, d + 'R'))
+        return ''
+
+    def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
+        s = deque(self.getFromRoot(root, startValue))
+        d = deque(self.getFromRoot(root, destValue))
+        while s and d and s[0] == d[0]:
+            s.popleft()
+            d.popleft()
+        return 'U'*len(s) + ''.join(d)
