ac628aee3e6a7d492bc94e7eafe442d55b6d0224
park-jihoo <park-jihoo@users.noreply.github.com>
2024-07-20 06:45:33 +0000

    Code formatting

--- leetcode/Medium/1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.py
+++ leetcode/Medium/1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.py
@@ -1,7 +1,9 @@
 class Solution:
     def restoreMatrix(self, rowSum: List[int], colSum: List[int]) -> List[List[int]]:
-        rowheap, colheap = [(val, idx) for idx, val in enumerate(rowSum) if val != 0],[(val, idx) for idx, val in enumerate(colSum) if val != 0]
-        ans = [[0]*len(colSum) for _ in range(len(rowSum))]
+        rowheap, colheap = [(val, idx) for idx, val in enumerate(rowSum) if val != 0], [
+            (val, idx) for idx, val in enumerate(colSum) if val != 0
+        ]
+        ans = [[0] * len(colSum) for _ in range(len(rowSum))]
         while rowheap or colheap:
             rowmin, rowidx = heapq.heappop(rowheap)
             colmin, colidx = heapq.heappop(colheap)
