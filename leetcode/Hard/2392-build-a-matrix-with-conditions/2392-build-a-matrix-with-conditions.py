43160ac82b81a0915e95fe50e7c5c5dc7ef78617
park-jihoo <park-jihoo@users.noreply.github.com>
2024-07-21 08:39:34 +0000

    Code formatting

--- leetcode/Hard/2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.py
+++ leetcode/Hard/2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.py
@@ -14,21 +14,25 @@ class Solution:
                     q.append(i)
         return result
 
-    def buildMatrix(self, k: int, rowConditions: List[List[int]], colConditions: List[List[int]]) -> List[List[int]]:
-        ans = [[0]* k for _ in range(k)]
+    def buildMatrix(
+        self, k: int, rowConditions: List[List[int]], colConditions: List[List[int]]
+    ) -> List[List[int]]:
+        ans = [[0] * k for _ in range(k)]
         rowgraph, colgraph = defaultdict(list), defaultdict(list)
         rowindegree, colindegree = [0 for _ in range(k)], [0 for _ in range(k)]
         for s, e in rowConditions:
-            rowgraph[s-1].append(e-1)
-            rowindegree[e-1] += 1
+            rowgraph[s - 1].append(e - 1)
+            rowindegree[e - 1] += 1
         for s, e in colConditions:
-            colgraph[s-1].append(e-1)
-            colindegree[e-1] += 1
-        row, col = self.topologicalSort(rowgraph, rowindegree), self.topologicalSort(colgraph, colindegree)
+            colgraph[s - 1].append(e - 1)
+            colindegree[e - 1] += 1
+        row, col = self.topologicalSort(rowgraph, rowindegree), self.topologicalSort(
+            colgraph, colindegree
+        )
         if len(row) < k or len(col) < k:
             return []
-        row = {val:idx for idx, val in enumerate(row)}
-        col = {val:idx for idx, val in enumerate(col)}
+        row = {val: idx for idx, val in enumerate(row)}
+        col = {val: idx for idx, val in enumerate(col)}
         for x in range(k):
-            ans[row[x]][col[x]] = x+1
+            ans[row[x]][col[x]] = x + 1
         return ans
