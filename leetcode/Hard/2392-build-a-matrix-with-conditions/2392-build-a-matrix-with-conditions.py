94114c9b98cbe331fd7423a42fe9c030600b4dd2
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-07-21 17:38:57 +0900

    Time: 613 ms (5.34%), Space: 25.4 MB (69.47%) - LeetHub

---
+++ leetcode/Hard/2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.py
@@ -0,0 +1,34 @@
+class Solution:
+    def topologicalSort(self, graph, indegree):
+        result = []
+        q = deque()
+        for idx, val in enumerate(indegree):
+            if val == 0:
+                q.append(idx)
+        while q:
+            node = q.popleft()
+            result.append(node)
+            for i in graph[node]:
+                indegree[i] -= 1
+                if indegree[i] == 0:
+                    q.append(i)
+        return result
+
+    def buildMatrix(self, k: int, rowConditions: List[List[int]], colConditions: List[List[int]]) -> List[List[int]]:
+        ans = [[0]* k for _ in range(k)]
+        rowgraph, colgraph = defaultdict(list), defaultdict(list)
+        rowindegree, colindegree = [0 for _ in range(k)], [0 for _ in range(k)]
+        for s, e in rowConditions:
+            rowgraph[s-1].append(e-1)
+            rowindegree[e-1] += 1
+        for s, e in colConditions:
+            colgraph[s-1].append(e-1)
+            colindegree[e-1] += 1
+        row, col = self.topologicalSort(rowgraph, rowindegree), self.topologicalSort(colgraph, colindegree)
+        if len(row) < k or len(col) < k:
+            return []
+        row = {val:idx for idx, val in enumerate(row)}
+        col = {val:idx for idx, val in enumerate(col)}
+        for x in range(k):
+            ans[row[x]][col[x]] = x+1
+        return ans
