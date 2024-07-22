80d9c3e4358463bc8c2a29c3268f316bfee9c61d
park-jihoo <park-jihoo@users.noreply.github.com>
2024-07-22 03:49:07 +0000

    Code formatting

--- leetcode/Easy/2418-sort-the-people/2418-sort-the-people.py
+++ leetcode/Easy/2418-sort-the-people/2418-sort-the-people.py
@@ -1,4 +1,6 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        ppl = sorted([(names[i], heights[i]) for i in range(len(names))], key=lambda x:-x[1])
+        ppl = sorted(
+            [(names[i], heights[i]) for i in range(len(names))], key=lambda x: -x[1]
+        )
         return [x[0] for x in ppl]
