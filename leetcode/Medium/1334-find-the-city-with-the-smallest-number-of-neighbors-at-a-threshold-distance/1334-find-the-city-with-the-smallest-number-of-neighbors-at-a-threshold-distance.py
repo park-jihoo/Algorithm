dfe5f17d25bd977186f276e72773010521220205
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-07-26 15:28:00 +0900

    Time: 493 ms (21.92%), Space: 17.7 MB (80.04%) - LeetHub

---
+++ leetcode/Medium/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findTheCity(self, n: int, edges: List[List[int]], t: int) -> int:
+        dist = [[float('inf')]*n for i in range(n)]
+        for s, e, w in edges:
+            dist[s][e] = w
+            dist[e][s] = w
+
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+
+        over = min([len([x for idx, x in enumerate(dist[i]) if x <= t and i != idx]) for i in range(n)])
+        for i in range(n-1, -1, -1):
+            if len([x for idx, x in enumerate(dist[i]) if x <= t and i != idx]) == over:
+                return i
+        return 0
