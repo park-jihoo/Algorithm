25940e88accdbcdd8e1ec95c7c9fa834acb74b18
park-jihoo <park-jihoo@users.noreply.github.com>
2024-07-26 06:28:44 +0000

    Code formatting

--- leetcode/Medium/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
+++ leetcode/Medium/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -1,6 +1,6 @@
 class Solution:
     def findTheCity(self, n: int, edges: List[List[int]], t: int) -> int:
-        dist = [[float('inf')]*n for i in range(n)]
+        dist = [[float("inf")] * n for i in range(n)]
         for s, e, w in edges:
             dist[s][e] = w
             dist[e][s] = w
@@ -10,8 +10,13 @@ class Solution:
                 for j in range(n):
                     dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
 
-        over = min([len([x for idx, x in enumerate(dist[i]) if x <= t and i != idx]) for i in range(n)])
-        for i in range(n-1, -1, -1):
+        over = min(
+            [
+                len([x for idx, x in enumerate(dist[i]) if x <= t and i != idx])
+                for i in range(n)
+            ]
+        )
+        for i in range(n - 1, -1, -1):
             if len([x for idx, x in enumerate(dist[i]) if x <= t and i != idx]) == over:
                 return i
         return 0
