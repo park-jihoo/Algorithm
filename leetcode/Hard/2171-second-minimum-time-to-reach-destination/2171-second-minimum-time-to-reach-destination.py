13a6a40ae4cc9da3336560dba6bea41acb0d6ebb
park-jihoo <park-jihoo@users.noreply.github.com>
2024-07-28 00:19:05 +0000

    Code formatting

--- leetcode/Hard/2171-second-minimum-time-to-reach-destination/2171-second-minimum-time-to-reach-destination.py
+++ leetcode/Hard/2171-second-minimum-time-to-reach-destination/2171-second-minimum-time-to-reach-destination.py
@@ -1,8 +1,10 @@
 class Solution:
-    def secondMinimum(self, n: int, edges: List[List[int]], time: int, change: int) -> int:
+    def secondMinimum(
+        self, n: int, edges: List[List[int]], time: int, change: int
+    ) -> int:
         m = defaultdict(list)
         heap = [(0, 1)]
-        d = [[] for _ in range(n+1)]
+        d = [[] for _ in range(n + 1)]
         d[1] = [0]
         for u, v in edges:
             m[u].append(v)
@@ -19,7 +21,7 @@ class Solution:
                 if (mind // change) % 2 == 0:
                     can = mind + time
                 else:
-                    can = math.ceil(mind/(2*change)) * (2*change) + time
+                    can = math.ceil(mind / (2 * change)) * (2 * change) + time
                 if not d[nbr] or (len(d[nbr]) == 1 and d[nbr] != [can]):
                     d[nbr] += [can]
                     heapq.heappush(heap, (can, nbr))
