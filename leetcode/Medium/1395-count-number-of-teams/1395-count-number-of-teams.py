4cb59e818f06b582437536fdef7129bc5b0ce1a2
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-07-29 10:36:27 +0900

    Time: 355 ms (87.5%), Space: 16.7 MB (35.59%) - LeetHub

---
+++ leetcode/Medium/1395-count-number-of-teams/1395-count-number-of-teams.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numTeams(self, rating: List[int]) -> int:
+        ans = 0
+        for idx, val in enumerate(rating):
+            llc, rgc, lgc, rlc = 0, 0, 0, 0
+            for left in rating[:idx]:
+                if left < val:
+                    llc += 1
+                if left > val:
+                    lgc += 1
+            for right in rating[idx+1:]:
+                if right > val:
+                    rgc += 1
+                if right < val:
+                    rlc += 1
+            ans += (llc * rgc + lgc * rlc)
+        return ans
