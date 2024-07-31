22add335daffa490f27cae17a39380d40943780f
park-jihoo <park-jihoo@users.noreply.github.com>
2024-07-31 13:01:38 +0000

    Code formatting

--- leetcode/Medium/1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.py
+++ leetcode/Medium/1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.py
@@ -1,13 +1,13 @@
 class Solution:
     def minHeightShelves(self, books: List[List[int]], shelfWidth: int) -> int:
         n = len(books)
-        dp = [float('inf') for _ in range(n+1)]
+        dp = [float("inf") for _ in range(n + 1)]
         dp[0] = 0
-        for i in range(1, n+1):
+        for i in range(1, n + 1):
             maxwid = shelfWidth
             maxh = 0
-            j = i-1
-            while j >=0 and maxwid - books[j][0] >= 0:
+            j = i - 1
+            while j >= 0 and maxwid - books[j][0] >= 0:
                 maxwid -= books[j][0]
                 maxh = max(maxh, books[j][1])
                 dp[i] = min(dp[i], dp[j] + maxh)
