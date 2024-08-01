977eeb857c50e397c35dee387039dd8df34ee00a
park-jihoo <park-jihoo@users.noreply.github.com>
2024-08-01 06:14:44 +0000

    Code formatting

--- leetcode/Easy/2678-number-of-senior-citizens/2678-number-of-senior-citizens.py
+++ leetcode/Easy/2678-number-of-senior-citizens/2678-number-of-senior-citizens.py
@@ -1,3 +1,3 @@
 class Solution:
     def countSeniors(self, details: List[str]) -> int:
-        return len([x for x in details if int(x[11:13])>60])
+        return len([x for x in details if int(x[11:13]) > 60])
