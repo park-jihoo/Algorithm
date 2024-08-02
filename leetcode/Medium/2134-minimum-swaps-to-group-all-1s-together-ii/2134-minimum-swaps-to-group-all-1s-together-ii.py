4238914a0f9eb066ca474f5eba49f8b7301ca8fc
park-jihoo <park-jihoo@users.noreply.github.com>
2024-08-02 04:49:17 +0000

    Code formatting

--- leetcode/Medium/2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.py
+++ leetcode/Medium/2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.py
@@ -3,9 +3,9 @@ class Solution:
         ones, n = nums.count(1), len(nums)
         x, ones_in = 0, 0
         for i in range(n * 2):
-            if i >= ones and nums[i%n-ones]:
-                x-=1
-            if nums[i%n]==1:
-                x+=1
+            if i >= ones and nums[i % n - ones]:
+                x -= 1
+            if nums[i % n] == 1:
+                x += 1
             ones_in = max(x, ones_in)
-        return ones -ones_in
+        return ones - ones_in
