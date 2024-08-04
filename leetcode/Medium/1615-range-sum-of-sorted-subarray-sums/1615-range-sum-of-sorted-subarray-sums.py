b83c985dc75432cd57995434b75f10c709110d95
park-jihoo <park-jihoo@users.noreply.github.com>
2024-08-04 04:28:07 +0000

    Code formatting

--- leetcode/Medium/1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.py
+++ leetcode/Medium/1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.py
@@ -3,10 +3,10 @@ class Solution:
         heap = [(x, i) for i, x in enumerate(nums)]
         heapq.heapify(heap)
         ans = 0
-        for idx in range(1, right+1):
+        for idx in range(1, right + 1):
             x, i = heapq.heappop(heap)
             if idx >= left:
                 ans += x
-            if i+1 < len(nums):
-                heapq.heappush(heap, (x+nums[i+1], i+1))
-        return ans % (10**9+7)
+            if i + 1 < len(nums):
+                heapq.heappush(heap, (x + nums[i + 1], i + 1))
+        return ans % (10**9 + 7)
