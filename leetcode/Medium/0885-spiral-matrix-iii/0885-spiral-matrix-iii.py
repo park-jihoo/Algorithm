6db64418ea029830dbf301aaa0b18b24bee682aa
park-jihoo <park-jihoo@users.noreply.github.com>
2024-08-08 06:56:57 +0000

    Code formatting

--- leetcode/Medium/0885-spiral-matrix-iii/0885-spiral-matrix-iii.py
+++ leetcode/Medium/0885-spiral-matrix-iii/0885-spiral-matrix-iii.py
@@ -1,7 +1,13 @@
 class Solution:
-    def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
+    def spiralMatrixIII(
+        self, rows: int, cols: int, rStart: int, cStart: int
+    ) -> List[List[int]]:
         def key(a):
             x, y = a
             x, y = x - rStart, y - cStart
-            return (max(abs(x), abs(y)), -((math.atan2(-1, 1) - math.atan2(x, y)) % (math.pi * 2)))
+            return (
+                max(abs(x), abs(y)),
+                -((math.atan2(-1, 1) - math.atan2(x, y)) % (math.pi * 2)),
+            )
+
         return sorted([(i, j) for i in range(rows) for j in range(cols)], key=key)
