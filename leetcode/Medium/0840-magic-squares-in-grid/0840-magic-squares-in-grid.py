1171062d41586e3f65646403a1b3ab8c3ddc354a
park-jihoo <park-jihoo@users.noreply.github.com>
2024-08-09 08:56:23 +0000

    Code formatting

--- leetcode/Medium/0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
+++ leetcode/Medium/0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
@@ -12,22 +12,22 @@ class Solution:
         return x == y == z == a == b == c == c1 == c2
 
     def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
-        l,u, mss = 0,0,0
+        l, u, mss = 0, 0, 0
         hm, rm = len(grid), len(grid[0])
 
-        if hm<3 or rm<3:
+        if hm < 3 or rm < 3:
             return 0
 
         while u + 2 != hm:
             l = 0
-            while l+2 != rm:
+            while l + 2 != rm:
                 res = []
-                for y in range(u,u+3):
-                    for i in range(l,l+3):
+                for y in range(u, u + 3):
+                    for i in range(l, l + 3):
                         res.append(grid[y][i])
-                if set(res) == set(range(1,10)) and self.isMagicSquare(res):
+                if set(res) == set(range(1, 10)) and self.isMagicSquare(res):
                     mss += 1
-                l+=1
-            u += 1 
+                l += 1
+            u += 1
 
         return mss
