9b21dcba2f246b5b2b3fa30fd62efd8f4197d71d
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-08-17 15:09:44 +0900

    Time: 1600 ms (37.07%), Space: 51 MB (34.63%) - LeetHub

---
+++ leetcode/Medium/2067-maximum-number-of-points-with-cost/2067-maximum-number-of-points-with-cost.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        m, n = len(points), len(points[0])
+        for i in range(m-1):
+            for j in range(n-2, -1, -1):
+                points[i][j] = max(points[i][j] , points[i][j+1] - 1)
+            for j in range(n):
+                points[i][j] = max(points[i][j], points[i][j-1] - 1 if j else 0)
+                points[i+1][j] += points[i][j]
+        return max(points[-1])
