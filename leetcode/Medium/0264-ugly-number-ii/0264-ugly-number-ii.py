59dfbe95e8c6fd85bd170664eefb183cc97903a8
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-08-18 16:51:23 +0900

    Time: 417 ms (9.13%), Space: 16.4 MB (93.61%) - LeetHub

---
+++ leetcode/Medium/0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        ugly = set()
+        ugly.add(1)
+        cur = 1
+        for i in range(n):
+            cur = min(ugly)
+            ugly.remove(cur)
+            ugly.add(cur*2)
+            ugly.add(cur*3)
+            ugly.add(cur*5)
+        return cur
