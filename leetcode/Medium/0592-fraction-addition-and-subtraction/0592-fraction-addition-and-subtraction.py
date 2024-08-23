149048084297a8e74192964db6dd6ab7ca478398
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-08-23 19:37:51 +0900

    Time: 40 ms (28.29%), Space: 16.5 MB (59.69%) - LeetHub

---
+++ leetcode/Medium/0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.py
@@ -0,0 +1,11 @@
+class Solution:
+    def fractionAddition(self, expression: str) -> str:
+        elist = expression.replace('-', '+-').split('+')
+        mot,son = 0, 1
+        for f in elist:
+            if f:
+                m, s = map(int, f.split('/'))
+                mot = mot * s + son * m
+                son *= s
+        gcd = math.gcd(mot, son)
+        return str(mot//gcd) + "/"+str(son//gcd)
