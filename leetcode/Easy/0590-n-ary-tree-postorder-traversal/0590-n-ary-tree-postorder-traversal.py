213c02795b30f5e5d3a7d54ada6f896f83ce7b6f
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-08-26 14:59:34 +0900

    Time: 41 ms (82.54%), Space: 18.3 MB (43.75%) - LeetHub

---
+++ leetcode/Easy/0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.py
@@ -0,0 +1,21 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        answer = []
+        if root == None: 
+            return answer
+
+        stack = deque([root])
+        while stack:
+            curr = stack.pop()
+            answer.append(curr.val)
+            stack.extend(curr.children)
+
+        return answer[::-1]
