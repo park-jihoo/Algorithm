0702a732b4f030e1480c1e49b2df1c1ef7cd3b78
park-jihoo <park-jihoo@users.noreply.github.com>
2024-09-03 01:47:19 +0000

    Code formatting

--- leetcode/Easy/1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.py
+++ leetcode/Easy/1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.py
@@ -1,6 +1,6 @@
 class Solution:
     def getLucky(self, s: str, k: int) -> int:
-        s_num = "".join([str(ord(d)-ord('a')+1) for d in s])
+        s_num = "".join([str(ord(d) - ord("a") + 1) for d in s])
         ans = s_num
         for i in range(k):
             ans = str(sum([int(x) for x in list(ans)]))
