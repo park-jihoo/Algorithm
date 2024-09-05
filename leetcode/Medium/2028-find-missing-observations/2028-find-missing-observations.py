6dca351bd2607d9bba5a79430bb772173fbba01c
park-jihoo <park-jihoo@users.noreply.github.com>
2024-09-05 01:33:23 +0000

    Code formatting

--- leetcode/Medium/2028-find-missing-observations/2028-find-missing-observations.py
+++ leetcode/Medium/2028-find-missing-observations/2028-find-missing-observations.py
@@ -1,7 +1,7 @@
 class Solution:
     def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
         # add n rolls and make mean as 'mean'
-        aim = mean * (len(rolls)+n) - sum(rolls)
+        aim = mean * (len(rolls) + n) - sum(rolls)
         div, mod = divmod(aim, n)
         if div <= 0 or div > 6 or (div == 6 and mod > 0):
             return []
