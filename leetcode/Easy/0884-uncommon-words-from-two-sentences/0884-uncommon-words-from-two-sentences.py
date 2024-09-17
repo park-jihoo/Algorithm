5630dbc7c8d10002903d3b65dac949f9ddf31a25
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-09-17 09:15:31 +0900

    Time: 36 ms (50.34%), Space: 16.5 MB (44.21%) - LeetHub

---
+++ leetcode/Easy/0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.py
@@ -0,0 +1,4 @@
+class Solution:
+    def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
+        cnt = Counter((s1+" "+s2).split())
+        return [key for key, val in cnt.items() if val==1]
