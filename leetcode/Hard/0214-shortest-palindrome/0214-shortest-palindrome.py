70c59b0700fcc5129f4c280d7976f78310a5f14c
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-09-20 13:37:10 +0900

    Time: 32 ms (100%), Space: 17 MB (64.78%) - LeetHub

---
+++ leetcode/Hard/0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -0,0 +1,10 @@
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
+        # adding in front of it
+        if not s or len(s) == 1:
+            return s
+        j = 0
+        for i in reversed(range(len(s))):
+            if s[i] == s[j]:
+                j += 1
+        return s[::-1][:len(s)-j] + self.shortestPalindrome(s[:j-len(s)]) + s[j-len(s):]
