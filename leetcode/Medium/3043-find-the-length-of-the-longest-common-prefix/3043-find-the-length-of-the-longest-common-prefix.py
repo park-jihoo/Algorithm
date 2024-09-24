45bef209a18a10240d6820df0941b076a67cd726
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-09-24 10:11:39 +0900

    Time: 873 ms (68.9%), Space: 34.6 MB (16.51%) - LeetHub

---
+++ leetcode/Medium/3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.py
@@ -0,0 +1,32 @@
+class Trie:
+    def __init__(self):
+        self.head = {}
+
+    def add(self, word):
+        cur = self.head
+        for ch in word:
+            if ch not in cur:
+                cur[ch] = {}
+            cur = cur[ch]
+            cur['*'] = True
+
+    def common_prefix(self, word):
+        cur = self.head
+        for idx, ch in enumerate(word):
+            if ch not in cur:
+                return word[:idx]
+            cur = cur[ch]
+        if '*' in cur:
+            return word
+        return ''
+
+
+class Solution:
+    def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
+        trie = Trie()
+        for word in arr1:
+            trie.add(str(word))
+        ans = 0
+        for word in arr2:
+            ans = max(ans, len(trie.common_prefix(str(word))))
+        return ans
