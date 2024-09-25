0172158f596d9b85478be2e645227dbe72d75403
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-09-25 10:29:58 +0900

    Time: 1228 ms (95.86%), Space: 211.3 MB (96.9%) - LeetHub

---
+++ leetcode/Hard/2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.py
@@ -0,0 +1,26 @@
+class Trie:
+    def __init__(self):
+        self.head = {}
+
+    def add(self, word):
+        cur = self.head
+        for ch in word:
+            if ch not in cur:
+                cur[ch] = {}
+            cur = cur[ch]
+            cur["*"] = cur.get("*", 0) + 1
+
+    def score(self, word):
+        cur = self.head
+        ans = 0
+        for ch in word:
+            ans += cur[ch]['*']
+            cur = cur[ch]
+        return ans
+
+class Solution:
+    def sumPrefixScores(self, words: List[str]) -> List[int]:
+        trie = Trie()
+        for word in words:
+            trie.add(word)
+        return [trie.score(word) for word in words]
