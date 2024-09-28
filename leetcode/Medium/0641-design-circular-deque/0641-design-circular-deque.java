61d965f4f782f653359881f2226b498f6f4b526a
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-09-28 14:20:32 +0900

    Time: 5 ms (37.86%), Space: 45.2 MB (12.52%) - LeetHub

---
+++ leetcode/Medium/0641-design-circular-deque/0641-design-circular-deque.java
@@ -0,0 +1,86 @@
+class MyCircularDeque {
+    private List<Integer> q;
+    private int x;
+
+    public MyCircularDeque(int k) {
+        q = new LinkedList<Integer>();
+        x = k;
+    }
+    
+    public boolean insertFront(int value) {
+        if(q.size() == x){
+            return false;
+        }else{
+            q.addFirst(value);
+            return true;
+        }
+    }
+    
+    public boolean insertLast(int value) {
+        if(q.size() == x){
+            return false;
+        }else{
+            q.addLast(value);
+            return true;
+        }
+    }
+    
+    public boolean deleteFront() {
+        if(q.size() == 0){
+            return false;
+        }else{
+            q.removeFirst();
+            return true;
+        }
+        
+    }
+    
+    public boolean deleteLast() {
+        if(q.size() == 0){
+            return false;
+        }else{
+            q.removeLast();
+            return true;
+        }
+        
+    }
+    
+    public int getFront() {
+        if(q.isEmpty()){
+            return -1;
+        }
+        return q.getFirst();
+    }
+    
+    public int getRear() {
+        if(q.isEmpty()){
+            return -1;
+        }
+        return q.getLast();
+    }
+    
+    public boolean isEmpty() {
+        return q.isEmpty();
+    }
+    
+    public boolean isFull() {
+        if(q.size() == x){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
+/**
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * MyCircularDeque obj = new MyCircularDeque(k);
+ * boolean param_1 = obj.insertFront(value);
+ * boolean param_2 = obj.insertLast(value);
+ * boolean param_3 = obj.deleteFront();
+ * boolean param_4 = obj.deleteLast();
+ * int param_5 = obj.getFront();
+ * int param_6 = obj.getRear();
+ * boolean param_7 = obj.isEmpty();
+ * boolean param_8 = obj.isFull();
+ */
