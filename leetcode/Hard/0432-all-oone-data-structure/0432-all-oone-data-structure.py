66812ad24dff3feafb7ac74de69ff443de74cd15
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-09-29 19:36:48 +0900

    Time: 5489 ms (5.08%), Space: 31.8 MB (94.21%) - LeetHub

---
+++ leetcode/Hard/0432-all-oone-data-structure/0432-all-oone-data-structure.py
@@ -0,0 +1,41 @@
+class AllOne:
+
+    def __init__(self):
+        self.dict = {}
+        self.max = ""
+        
+    def inc(self, key: str) -> None:
+        if key in self.dict:
+            self.dict[key]+=1
+            if self.dict[key]>self.dict[self.max]:
+                self.max=key
+        else:
+            self.dict[key]=1
+            if self.max=="":
+                self.max=key
+
+    def dec(self, key: str) -> None:
+        self.dict[key]-=1
+        if self.dict[key]==0:
+            del self.dict[key]
+            if key==self.max:
+                self.max=""
+        else:
+            if key==self.max:
+                self.max=max(zip(self.dict.values(), self.dict.keys()))[1]
+
+    def getMaxKey(self) -> str:
+        return self.max
+
+    def getMinKey(self) -> str:
+        if not self.dict:
+            return ""
+        return min(zip(self.dict.values(), self.dict.keys()))[1]
+
+
+# Your AllOne object will be instantiated and called as such:
+# obj = AllOne()
+# obj.inc(key)
+# obj.dec(key)
+# param_3 = obj.getMaxKey()
+# param_4 = obj.getMinKey()
