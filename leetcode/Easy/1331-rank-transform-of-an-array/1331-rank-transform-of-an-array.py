dedd35fb1cd9c2dded7f6fee4f6beea740bf8c80
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-10-02 09:54:05 +0900

    Time: 236 ms (77.17%), Space: 35.9 MB (21.23%) - LeetHub

---
+++ leetcode/Easy/1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -0,0 +1,6 @@
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        hashmap = {}
+        for idx, val in enumerate(sorted(list(set(arr)))):
+            hashmap.setdefault(val, (idx+1))
+        return [hashmap[x] for x in arr]
