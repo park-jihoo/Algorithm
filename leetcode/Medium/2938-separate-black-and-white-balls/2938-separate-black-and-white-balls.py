1c5ab4d04f773a09abbf8cb1a0ea98a1369d85af
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-10-15 09:52:58 +0900

    Time: 98 ms (62.39%), Space: 17.6 MB (31.34%) - LeetHub

---
+++ leetcode/Medium/2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+        ans, cnt = 0, 0
+        for i in range(len(s)-1, -1, -1):
+            if s[i] == '1':
+                ans += cnt
+            else:
+                cnt += 1
+        return ans
