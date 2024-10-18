a9d98132730f9a750a904617ce05d78632316114
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-10-18 11:17:17 +0900

    Time: 3704 ms (5.18%), Space: 17.7 MB (24.36%) - LeetHub

---
+++ leetcode/Medium/2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countMaxOrSubsets(self, nums: List[int]) -> int:
+        maxor = functools.reduce(lambda a, b: a | b, nums)
+        dp = [0] * (1 << 17)
+        dp[0] = 1
+        for num in nums:
+            for i in range(maxor, -1, -1):
+                dp[i|num] += dp[i]
+        return dp[maxor]
