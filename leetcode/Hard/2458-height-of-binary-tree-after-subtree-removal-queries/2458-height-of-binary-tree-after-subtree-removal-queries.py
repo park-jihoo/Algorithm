2cc5e0993d29afc68834002425aa7b667ba10a1a
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-10-26 21:07:33 +0900

    Time: 534 ms (74.36%), Space: 85.1 MB (6.16%) - LeetHub

---
+++ leetcode/Hard/2458-height-of-binary-tree-after-subtree-removal-queries/2458-height-of-binary-tree-after-subtree-removal-queries.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def treeQueries(self, root: Optional[TreeNode], queries: List[int]) -> List[int]:
+        @lru_cache(None)
+        def height(node):
+            if not node:
+                return -1
+            return 1 + max(height(node.left), height(node.right))
+        
+        d = defaultdict(int)
+
+        stack = [(root, 0, 0)]
+
+        while stack:
+            node, depth, maxval = stack.pop()
+            if node:
+                d[node.val] = maxval
+                stack.append((node.right, depth + 1, max(maxval, depth+1+height(node.left))))
+                stack.append((node.left, depth+1, max(maxval, depth+1+height(node.right))))
+
+        return [d[i] for i in queries]
