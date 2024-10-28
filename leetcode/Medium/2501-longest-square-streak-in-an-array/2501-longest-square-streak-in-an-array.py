4e94a0405e7ebbdc5b23079bb613ea2f75d05723
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-10-28 20:00:26 +0900

    Time: 127 ms (54.05%), Space: 34 MB (47%) - LeetHub

---
+++ leetcode/Medium/2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestSquareStreak(self, nums: List[int]) -> int:
+        streak = 0
+        set_nums = set(nums)
+        for start in nums:
+            curr_streak, curr = 0, start
+            while curr in set_nums:
+                curr_streak += 1
+                if curr ** 2 > 10**5:
+                    break
+                curr *= curr
+            streak = max(streak, curr_streak)
+        return streak if streak >= 2 else -1
