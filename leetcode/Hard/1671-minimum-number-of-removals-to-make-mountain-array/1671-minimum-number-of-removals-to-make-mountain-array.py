6c1b3714516eeba8de51e8962f5d69b47fe6e5d3
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-10-30 21:00:07 +0900

    Time: 2389 ms (11.63%), Space: 16.9 MB (13.17%) - LeetHub

---
+++ leetcode/Hard/1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = [10**10] * (len(nums) + 1)
+        for val in nums: 
+            dp[bisect.bisect_left(dp, val)] = val  
+        return dp.index(10**10)
+
+    def minimumMountainRemovals(self, nums: List[int]) -> int:
+        ans = 0
+        n = len(nums)
+        for i in range(1, n - 1):
+            left  = [num for num in nums[:i] if num < nums[i]] + [nums[i]]
+            right = [nums[i]] + [num for num in nums[i+1:] if num < nums[i]]
+            right = right[::-1]
+            a, b = self.lengthOfLIS(left), self.lengthOfLIS(right)
+            if a >= 2 and b >= 2: 
+                ans = max(ans, a + b - 1)
+
+        return n - ans
