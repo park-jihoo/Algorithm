66d502c1794be8ad1c8b71adda051095dd8d701c
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-11-02 10:32:44 +0900

    Time: 3 ms (12.78%), Space: 16.7 MB (11.84%) - LeetHub

---
+++ leetcode/Easy/2490-circular-sentence/2490-circular-sentence.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isCircularSentence(self, sentence: str) -> bool:
+        senlst = sentence.split()
+        if senlst[0][0] != senlst[-1][-1]:
+            return False
+        for i in range(len(senlst) - 1):
+            if senlst[i][-1] != senlst[i+1][0]:
+                return False
+        return True
