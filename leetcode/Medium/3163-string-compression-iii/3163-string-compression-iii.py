65455d60dc4f25579853386c90d120da45160c56
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-11-04 12:37:41 +0900

    Time: 178 ms (80.85%), Space: 18.9 MB (90.27%) - LeetHub

---
+++ leetcode/Medium/3163-string-compression-iii/3163-string-compression-iii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def compressedString(self, word: str) -> str:
+        comp = ""
+        cnt, tmp = 1, word[0]
+        for idx, ch in enumerate(word):
+            if idx == 0:
+                continue
+            if ch != tmp or cnt == 9:
+                comp += (str(cnt) + str(tmp))
+                cnt, tmp = 0, ch
+            cnt += 1
+        comp += (str(cnt) + str(tmp))
+        return comp
