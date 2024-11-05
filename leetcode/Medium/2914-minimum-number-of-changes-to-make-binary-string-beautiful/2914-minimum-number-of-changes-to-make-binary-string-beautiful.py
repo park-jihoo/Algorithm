9a17506e6c726ca7d539b628c564e7741d5057e5
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-11-05 12:27:12 +0900

    Time: 26 ms (32.43%), Space: 16.8 MB (89.02%) - LeetHub

---
+++ leetcode/Medium/2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minChanges(self, s: str) -> int:
+        ans = 0
+        for i in range(len(s) // 2):
+            if s[2*i] != s[2*i+1]:
+                ans += 1
+        return ans
