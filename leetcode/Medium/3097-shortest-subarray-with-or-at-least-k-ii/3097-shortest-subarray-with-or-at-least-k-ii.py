d5231ae7311641c3b715ae6147a9dec4b563fc29
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-11-10 21:08:28 +0900

    Time: 1732 ms (60.46%), Space: 35.8 MB (72.93%) - LeetHub

---
+++ leetcode/Medium/3097-shortest-subarray-with-or-at-least-k-ii/3097-shortest-subarray-with-or-at-least-k-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def update(self, bits,x, change):
+        for i in range(32):
+            if (x>>i)&1:
+                bits[i] += change
+    
+    def convert(self, bits):
+        result = 0
+        for i in range(32):
+            if bits[i]:
+                result |= 1 << i
+        return result
+
+    def minimumSubarrayLength(self, nums: List[int], k: int) -> int:
+        bits = [0]*32
+        ans = len(nums) + 1
+        start = 0
+
+        for end in range(len(nums)):
+            self.update(bits, nums[end], 1)
+            while start <= end and self.convert(bits) >= k:
+                ans = min(ans, end - start + 1)
+                self.update(bits, nums[start], -1)
+                start += 1
+        if ans == len(nums) + 1:
+            return -1
+        return ans
