174cbf5dc317447d7bb948011f291283795620f4
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-11-18 12:33:23 +0900

    Time: 3 ms (56.07%), Space: 16.6 MB (62.03%) - LeetHub

---
+++ leetcode/Easy/1652-defuse-the-bomb/1652-defuse-the-bomb.py
@@ -0,0 +1,10 @@
+class Solution:
+    def decrypt(self, code: List[int], k: int) -> List[int]:
+        n = len(code)
+        if k == 0:
+            return [0 for x in code]
+        d = code + code
+        if k > 0:
+            return [sum(d[idx+1:idx+k+1]) for idx in range(n)]
+        else:
+            return [sum(d[idx+n+k:idx+n]) for idx in range(n)]
