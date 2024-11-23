f4fdefe1924755cdce4bfdf18c2303ddac83a977
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-11-23 20:58:11 +0900

    Time: 1879 ms (74.37%), Space: 29.1 MB (43.28%) - LeetHub

---
+++ leetcode/Medium/1861-rotating-the-box/1861-rotating-the-box.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
+        rotatedBox = []
+        for row in box:
+            stone, empty, ans = 0, 0, []
+            for cell in row:
+                if cell == "#":
+                    stone += 1
+                elif cell == "*":
+                    ans += empty * ["."] + stone * ["#"] + ["*"]
+                    stone, empty = 0, 0
+                else:
+                    empty += 1
+            ans += empty * ["."] + stone * ["#"]
+            rotatedBox.append(ans)
+
+        m, n = len(box), len(box[0])
+        return [[rotatedBox[m-j-1][i] for j in range(m)] for i in range(n)]
