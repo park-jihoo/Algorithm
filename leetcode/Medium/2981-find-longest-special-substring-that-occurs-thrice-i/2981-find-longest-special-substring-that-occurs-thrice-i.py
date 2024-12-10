82ab6482205ba893f9be68e048d930ccf748064f
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-12-10 12:30:33 +0900

    Time: 8 ms (80.81%), Space: 17.3 MB (9.27%) - LeetHub

---
+++ leetcode/Medium/2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maximumLength(self, s: str) -> int:
+        cnt = Counter(s)
+        cand = {}
+        for key, val in cnt.items():
+            if val >= 3:
+                cand[key] = defaultdict(int)
+        if not cand:
+            return -1
+        for ltr, size in groupby(s):
+            if ltr not in cand:
+                continue
+            test = len(list(size))
+            for substr in range(test, 0, -1):
+                cand[ltr][substr] += (test - substr + 1)
+        ans = 1
+        for substr in cand:
+            for size in cand[substr]:
+                if cand[substr][size] >= 3:
+                    ans = max(ans, size)
+        return ans
