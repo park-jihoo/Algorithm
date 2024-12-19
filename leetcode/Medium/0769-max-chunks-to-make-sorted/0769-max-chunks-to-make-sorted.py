281b7f83e246114ffb2273ddec059cffcbab5774
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-12-19 12:32:15 +0900

    Time: 0 ms (100%), Space: 17.6 MB (14.45%) - LeetHub

---
+++ leetcode/Medium/0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.py
@@ -0,0 +1,5 @@
+class Solution:
+    def maxChunksToSorted(self, arr: List[int]) -> int:
+        pref_max = list(accumulate(arr, max))
+        suff_min = list(accumulate(arr[::-1], min))[::-1]
+        return sum(int(i==0 or suff_min[i] > pref_max[i-1]) for i in range(len(arr)))
