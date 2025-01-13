1866754d8788b1c62ed5255583272b1f88f4edf4
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-01-13 19:28:43 +0900

    Time: 147 ms (41.27%), Space: 18.9 MB (14.92%) - LeetHub

---
+++ leetcode/Medium/3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        cnt, ans = Counter(s), 0
+        for key, val in cnt.items():
+            if val >= 3:
+                ans += 2*((val - 1)//2)
+        return len(s) - ans
