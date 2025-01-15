5aa97c3818ce70ffd16983818839173aeabb6e41
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-01-15 20:08:37 +0900

    Time: 0 ms (100%), Space: 17.6 MB (59.2%) - LeetHub

---
+++ leetcode/Medium/2429-minimize-xor/2429-minimize-xor.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minimizeXor(self, num1: int, num2: int) -> int:
+        bit_diff = num1.bit_count() - num2.bit_count()
+        while bit_diff != 0:
+            if bit_diff > 0:
+                bit_diff -= 1
+                num1 &= num1-1
+            else:
+                bit_diff += 1
+                num1 |= num1+1
+        return num1
