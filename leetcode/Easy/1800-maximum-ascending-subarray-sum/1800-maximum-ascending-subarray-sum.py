d21caa60631ed336d81378765d3b3ef7b285ca57
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-02-04 20:04:15 +0900

    Time: 0 ms (100%), Space: 18 MB (9.06%) - LeetHub

---
+++ leetcode/Easy/1800-maximum-ascending-subarray-sum/1800-maximum-ascending-subarray-sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxAscendingSum(self, nums: List[int]) -> int:
+        answer = 0
+        temp = 0
+        for i in range(len(nums)):
+            temp += nums[i]
+            if(i==len(nums)-1):
+                if(temp>answer):
+                    return temp
+                else:
+                    return answer
+            if(nums[i+1]<=nums[i]):
+                if(temp>answer):
+                    answer = temp
+                temp = 0
+            print(answer)
+        return answer
