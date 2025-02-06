383a0058c31d58248584a1b22e0b52a1111228ee
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-02-06 19:50:28 +0900

    Time: 424 ms (36.97%), Space: 46.4 MB (41.71%) - LeetHub

---
+++ leetcode/Medium/1726-tuple-with-same-product/1726-tuple-with-same-product.py
@@ -0,0 +1,7 @@
+class Solution:
+    def tupleSameProduct(self, nums: List[int]) -> int:
+        cntr = defaultdict(int)
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                cntr[nums[i]*nums[j]] += 1
+        return sum(cntr[key] * (cntr[key] - 1) * 4 for key in cntr)
