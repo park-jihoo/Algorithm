5243e8364fcc6f54283b8e0c53ed851cef9f20b7
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-02-22 19:25:24 +0900

    Time: 7 ms (99.71%), Space: 18.4 MB (38.53%) - LeetHub

---
+++ leetcode/Hard/1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def recoverFromPreorder(self, traversal: str) -> Optional[TreeNode]:
+        pattern = "[0-9]+|[-]+"
+        tokens = re.findall(pattern, traversal)
+
+        root = TreeNode(int(tokens[0]))        
+        node_lvl = {0: root}
+
+        for i in range(1, len(tokens), 2):
+
+            depth = len(tokens[i])
+            node = TreeNode(int(tokens[i+1]))
+            parent = node_lvl[depth - 1]
+            
+            if parent.left is None:
+                parent.left = node
+            else:
+                parent.right = node
+            
+            node_lvl[depth] = node
+
+        return root
