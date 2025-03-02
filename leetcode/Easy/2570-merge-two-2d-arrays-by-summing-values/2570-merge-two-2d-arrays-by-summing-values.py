4b6ec954e0b6c32dd8a73edb57cb6229565fd536
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-03-02 16:42:35 +0900

    Time: 0 ms (100%), Space: 17.8 MB (99.24%) - LeetHub

---
+++ leetcode/Easy/2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.py
@@ -0,0 +1,8 @@
+class Solution:
+    def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
+        cnt = Counter()
+        for idx, val in nums1:
+            cnt[idx] += val
+        for idx, val in nums2:
+            cnt[idx] += val
+        return sorted([[key, val] for key, val in cnt.items()], key=lambda x:x[0])
