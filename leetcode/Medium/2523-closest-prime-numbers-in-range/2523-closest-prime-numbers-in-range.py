a5d818d5c3021b3f4eadb2dbcb77144729b3ddb0
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-03-07 22:40:23 +0900

    Time: 6664 ms (6.56%), Space: 130.6 MB (5.31%) - LeetHub

---
+++ leetcode/Medium/2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.py
@@ -0,0 +1,14 @@
+class Solution:
+    def closestPrimes(self, left: int, right: int) -> List[int]:
+        primes = set(range(left, right+1))
+        primes -= set([1])
+        for i in range(2, math.ceil(math.sqrt(right))+1):
+            primes -= set(range(i*2, right+1, i))
+        if len(primes) < 2:
+            return [-1, -1]
+        primes = sorted(list(primes))
+        ans = [primes[0], primes[1]]
+        for i in range(1, len(primes) - 1):
+            if primes[i+1]-primes[i] < ans[1] - ans[0]:
+                ans = [primes[i] , primes[i+1]]
+        return ans
