72f3c19a15f90c6c41579336a1d6365d36936e6d
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-03-11 19:38:17 +0900

    Time: 67 ms (92.89%), Space: 17.9 MB (39.71%) - LeetHub

---
+++ leetcode/Medium/1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        ans = 0
+        pos = [-1, -1, -1]
+        for i in range(len(s)):
+            pos[ord(s[i])-ord('a')] = i
+            ans += 1 + min(pos)
+        return ans
