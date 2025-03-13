cdc8830bc5b793803d1d3c20a08d302abe230826
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-03-13 21:49:33 +0900

    Time: 136 ms (93.03%), Space: 63.1 MB (68.09%) - LeetHub

---
+++ leetcode/Medium/3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
+        n, m=len(nums), len(queries)
+        freq=[0]*(n+1)
+        tmp, ans=0, 0
+        for i in range(n):
+            while tmp<nums[i]-freq[i]:
+                if ans>=m: 
+                    return -1
+                l, r, v=queries[ans]
+                if r<i:
+                    ans+=1 
+                    continue
+                freq[max(l, i)]+=v
+                freq[r+1]-=v
+                ans+=1
+            tmp+=freq[i]
+        return ans
