aefe238e9eb7b9bf7707cddec00d540ed6bb9bf2
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-03-18 19:32:18 +0900

    Time: 79 ms (91.77%), Space: 33.6 MB (26.22%) - LeetHub

---
+++ leetcode/Medium/2401-longest-nice-subarray/2401-longest-nice-subarray.py
@@ -0,0 +1,10 @@
+class Solution:
+    def longestNiceSubarray(self, nums: List[int]) -> int:
+        i, cumand, ans = 0, 0, 0
+        for idx, num in enumerate(nums):
+            while cumand&num != 0:
+                cumand -= nums[i]
+                i += 1
+            cumand += num
+            ans = max(ans, idx-i+1)
+        return ans
