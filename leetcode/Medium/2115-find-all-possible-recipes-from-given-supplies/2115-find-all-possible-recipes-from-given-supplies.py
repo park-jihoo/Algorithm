be1ef84cb9123ea4f37f81ef2066bcc20553732b
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-03-22 00:05:52 +0900

    Time: 65 ms (67.34%), Space: 19.7 MB (79.98%) - LeetHub

---
+++ leetcode/Medium/2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -0,0 +1,24 @@
+class Solution:
+    #topological sort
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        graph = defaultdict(list)
+        for i in range(len(recipes)):
+            for ing in ingredients[i]:
+                graph[ing].append(recipes[i])
+        indegree = {recipe: len(ingredient) for recipe, ingredient in zip(recipes, ingredients)}
+
+        result_dict = defaultdict(set)
+        q = deque(supplies)
+        while q:
+            node = q.popleft()
+            for i in graph[node]:
+                indegree[i] -= 1
+                if indegree[i] == 0:
+                    q.append(i)
+                    result_dict[i].add(node)
+                    result_dict[i].update(result_dict[node])
+        ans = set()
+        for key, val in result_dict.items():
+            if len(val) != 0:
+                ans.add(key)
+        return list(ans)
