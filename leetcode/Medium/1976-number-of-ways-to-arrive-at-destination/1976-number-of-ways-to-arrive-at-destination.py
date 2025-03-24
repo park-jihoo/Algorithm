e9546756f906516a9e72cba84993eb3a53d554c6
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-03-24 20:02:16 +0900

    Time: 23 ms (38.41%), Space: 25.1 MB (68.38%) - LeetHub

---
+++ leetcode/Medium/1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countPaths(self, n: int, roads: List[List[int]]) -> int:
+        graph = defaultdict(list)
+        for u,v,t in roads:
+            graph[u].append((v, t))
+            graph[v].append((u, t))
+        dist, ans = [float('inf')]*n, [0]*n
+        pq = [(0, 0)]
+        dist[0], ans[0] = 0, 1
+        mod = 10**9+7
+        while pq:
+            d, node = heapq.heappop(pq)
+            if d > dist[node]:
+                continue
+            for nbr, time in graph[node]:
+                if dist[node] + time < dist[nbr]:
+                    dist[nbr] = dist[node] + time
+                    ans[nbr] = ans[node]
+                    heapq.heappush(pq, (dist[nbr], nbr))
+                elif dist[node] + time == dist[nbr]:
+                    ans[nbr] = (ans[nbr] + ans[node])%mod
+            
+        return ans[n-1]
