71a2547653aec8cda2bfd386535ff383bbf13e73
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-03-25 20:52:12 +0900

    Time: 292 ms (85.14%), Space: 83.8 MB (10.14%) - LeetHub

---
+++ leetcode/Medium/3394-check-if-grid-can-be-cut-into-sections/3394-check-if-grid-can-be-cut-into-sections.py
@@ -0,0 +1,20 @@
+class Solution:
+    def split(self, intervals):
+        intervals.sort()
+        cuts = 0
+        cur = intervals[0][1]
+        for s, e in intervals[1:]:
+            if s >= cur:
+                cuts += 1
+                if cuts >= 2:
+                    return True
+            cur = max(cur, e)
+        return False
+
+    def checkValidCuts(self, n: int, rectangles: List[List[int]]) -> bool:
+        xs, ys = [], []
+        for sx, sy, ex, ey in rectangles:
+            xs.append((sx, ex))
+            ys.append((sy, ey))
+        return self.split(xs) or self.split(ys)
+        
