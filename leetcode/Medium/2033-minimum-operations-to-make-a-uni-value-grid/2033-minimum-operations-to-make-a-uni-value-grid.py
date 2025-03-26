eb69ebc9cf380060545c0b81e2f076c75a92cc56
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-03-26 19:55:52 +0900

    Time: 243 ms (22.81%), Space: 42.3 MB (10.96%) - LeetHub

---
+++ leetcode/Medium/2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minOperations(self, grid: List[List[int]], x: int) -> int:
+        values = sorted(list(chain.from_iterable(grid)))
+        values = list(map(lambda a:a-values[0], values))
+        if any(val%x != 0 for val in values):
+            return -1
+        values = list(map(lambda a:a//x, values))
+        center = values[len(values) // 2]
+        return sum(abs(a-center) for a in values)
