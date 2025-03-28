ff468c543c935f74add16ee266b09cd7848943ab
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-03-28 21:18:24 +0900

    Time: 606 ms (66.01%), Space: 42.7 MB (24.18%) - LeetHub

---
+++ leetcode/Hard/2503-maximum-number-of-points-from-grid-queries/2503-maximum-number-of-points-from-grid-queries.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxPoints(self, grid: List[List[int]], queries: List[int]) -> List[int]:
+        # biggest island including top-left, size which is bigger than x
+        m, n = len(grid), len(grid[0])
+        queries_sorted = sorted((query, idx) for idx, query in enumerate(queries))
+        ans = [0]*len(queries)
+        heap, visited, total = [(grid[0][0], 0, 0)], set([(0, 0)]), 0
+        ds = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        for query, index in queries_sorted:
+            while heap and heap[0][0] < query:
+                v, r, c = heapq.heappop(heap)
+                total += 1
+                for dr, dc in ds:
+                    nr, nc = r + dr, c + dc
+                    if 0<=nr<m and 0<=nc<n and (nr, nc) not in visited:
+                        visited.add((nr, nc))
+                        heapq.heappush(heap, (grid[nr][nc], nr, nc))
+            ans[index] = total
+        return ans
