e2dfe0b887f12da0e0699f3ccdd71fc5671983d6
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-03-30 22:05:54 +0900

    Time: 6 ms (47.27%), Space: 17.8 MB (57.51%) - LeetHub

---
+++ leetcode/Medium/0763-partition-labels/0763-partition-labels.py
@@ -0,0 +1,12 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        pos = [-1] * 26
+        for idx, ch in enumerate(s):
+            pos[ord(ch) - ord('a')] = idx
+        ans, st, ed = [], 0, -1
+        for idx, ch in enumerate(s):
+            ed = max(ed, pos[ord(ch) - ord('a')])
+            if idx == ed:
+                ans.append(ed-st+1)
+                st = idx+1
+        return ans
