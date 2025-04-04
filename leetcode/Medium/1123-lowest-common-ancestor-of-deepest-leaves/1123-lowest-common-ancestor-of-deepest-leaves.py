88af91fb0d04cb8943103fba2b4044382cef6c16
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-04-04 21:56:46 +0900

    Time: 3 ms (57.53%), Space: 18.1 MB (71.52%) - LeetHub

---
+++ leetcode/Medium/1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def dfs(self, root):
+        if not root:
+            return 0, None
+        l1, l2 = self.dfs(root.left)
+        r1, r2 = self.dfs(root.right)
+        if l1 > r1:
+            return l1+1, l2
+        elif l1 < r1:
+            return r1+1, r2
+        return l1+1, root
+
+    def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        return self.dfs(root)[1]
