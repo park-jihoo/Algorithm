f27a766971c7ea85006cd764537596ba7716398f
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-04-11 16:00:27 +0900

    Time: 212 ms (95.11%), Space: 17.9 MB (54.99%) - LeetHub

---
+++ leetcode/Easy/2843-count-symmetric-integers/2843-count-symmetric-integers.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countSymmetricIntegers(self, low: int, high: int) -> int:
+        ans = 0
+        for i in range(low, high+1):
+            if math.ceil(math.log10(i)) == 2 and i % 11 == 0:
+                ans += 1
+            elif math.ceil(math.log10(i)) == 4:
+                left, right = divmod(i, 100)
+                if left%10+left//10 == right%10+right//10:
+                    ans += 1
+        return ans
