a41c433c0d4e33663cf3490f486555469662073b
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-04-16 20:00:20 +0900

    Time: 117 ms (81.23%), Space: 34.4 MB (82.85%) - LeetHub

---
+++ leetcode/Medium/2537-count-the-number-of-good-subarrays/2537-count-the-number-of-good-subarrays.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countGood(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        fr = defaultdict(int)
+        ans, cnt, left = 0, 0, 0
+        for r, x in enumerate(nums):
+            cnt += fr[x]
+            fr[x] += 1
+            while cnt >= k:
+                ans += (n-r)
+                fr[nums[left]] -= 1
+                cnt -= fr[nums[left]]
+                left += 1
+        return ans 
