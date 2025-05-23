fbee3ba741256719c8605d1705b8adb382298e05
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-05-23 13:46:58 +0900

    Time: 50 ms (48.28%), Space: 26.2 MB (58.62%) - LeetHub

--- leetcode/Hard/3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.py
+++ leetcode/Hard/3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.py
@@ -2,7 +2,7 @@ class Solution:
     def maximumValueSum(self, nums: List[int], k: int, edges: List[List[int]]) -> int:
         # XOR operator ^
         # XOR twice then original value
-        xors = [i ^ k for i in nums]
+        xors = [i^k for i in nums]
         ans = sum(max(xors[i], nums[i]) for i in range(len(nums)))
         cnt = sum(xors[i] > nums[i] for i in range(len(nums)))
         if cnt % 2:
