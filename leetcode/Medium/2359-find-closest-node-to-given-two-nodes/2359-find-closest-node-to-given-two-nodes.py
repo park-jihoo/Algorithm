0a290e3c77d8848e7ee7c3b5c764d4d733977217
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-05-30 22:55:28 +0900

    Time: 109 ms (89.14%), Space: 31.6 MB (91.86%) - LeetHub

---
+++ leetcode/Medium/2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.py
@@ -0,0 +1,25 @@
+class Solution:
+    def closestMeetingNode(self, edges: List[int], node1: int, node2: int) -> int:
+        def get_distances(start: int) -> List[int]:
+            n = len(edges)
+            dist = [-1] * n
+            d = 0
+            curr = start
+            while curr != -1 and dist[curr] == -1:
+                dist[curr] = d
+                d += 1
+                curr = edges[curr]
+            return dist
+
+        dist1 = get_distances(node1)
+        dist2 = get_distances(node2)
+        
+        min_dist = float('inf')
+        answer = -1
+        for i in range(len(edges)):
+            if dist1[i] != -1 and dist2[i] != -1:
+                max_dist = max(dist1[i], dist2[i])
+                if max_dist < min_dist:
+                    min_dist = max_dist
+                    answer = i
+        return answer
