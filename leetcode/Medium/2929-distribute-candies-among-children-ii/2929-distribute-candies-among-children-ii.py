e8459f85555e49f543b4f1ca69a22b7c03e7f48d
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-06-02 19:31:25 +0900

    Time: 5515 ms (6.2%), Space: 17.7 MB (69.66%) - LeetHub

---
+++ leetcode/Medium/2929-distribute-candies-among-children-ii/2929-distribute-candies-among-children-ii.py
@@ -0,0 +1,6 @@
+class Solution:
+    def distributeCandies(self, n: int, limit: int) -> int:
+        s = 0
+        for i in range(0, min(n, limit)+1):
+            s += max(min(limit, n - i) - max(0, n - i - limit) + 1, 0)
+        return s
