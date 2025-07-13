ebcab1f6c8b8b37030ab976ad902446370836b4d
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-07-13 17:16:17 +0900

    Time: 75 ms (71.43%), Space: 33.6 MB (82.54%) - LeetHub

---
+++ leetcode/Medium/2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.py
@@ -0,0 +1,14 @@
+class Solution:
+    def matchPlayersAndTrainers(self, players: List[int], trainers: List[int]) -> int:
+        trainers.sort(reverse=True)
+        players.sort(reverse=True)
+        ans, pi = 0, 0
+        for ti in range(len(trainers)):
+            while pi < len(players):
+                if players[pi] <= trainers[ti]:
+                    pi +=1
+                    ans += 1
+                    break
+                else:
+                    pi +=1
+        return ans
