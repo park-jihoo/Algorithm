792c90bb7ec806c2c3d5af5d50dae41bdd70de21
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-07-27 14:18:05 +0900

    Time: 0 ms (100%), Space: 18 MB (20.24%) - LeetHub

---
+++ leetcode/Easy/2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countHillValley(self, nums: List[int]) -> int:
+        ans = 0
+        numu = []
+        for i in nums:
+            if numu and numu[-1] == i:
+                continue
+            numu.append(i)
+        for i in range(1, len(numu) - 1):
+            if (numu[i-1] - numu[i]) * (numu[i+1] - numu[i]) > 0:
+                ans += 1
+        return ans
