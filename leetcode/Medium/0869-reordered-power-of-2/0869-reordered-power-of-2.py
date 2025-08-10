963467d4ecb4c87f1c64f5ff756bfa5257172c08
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-08-10 21:17:38 +0900

    Time: 7 ms (30.53%), Space: 17.7 MB (79.77%) - LeetHub

---
+++ leetcode/Medium/0869-reordered-power-of-2/0869-reordered-power-of-2.py
@@ -0,0 +1,6 @@
+class Solution:
+    def reorderedPowerOf2(self, n: int) -> bool:
+        c = []
+        for i in range(31):
+            c.append(Counter(str(1<<i)))
+        return Counter(str(n)) in c
