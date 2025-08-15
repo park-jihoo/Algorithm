7d2d02f850c069b926aca3c985b674b80cc40069
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-08-15 16:45:37 +0900

    Time: 4 ms (5.22%), Space: 17.6 MB (88.88%) - LeetHub

--- leetcode/Easy/0342-power-of-four/0342-power-of-four.py
+++ leetcode/Easy/0342-power-of-four/0342-power-of-four.py
@@ -1,12 +1,12 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        if n < 0:
+        if n < 0 :
             return False
         binary = str(bin(n))[2:]
-        if binary[0] != "1":
+        if binary[0] != '1':
             return False
         if len(binary) % 2 != 1:
             return False
-        if binary.count("0") == len(binary) - 1:
+        if binary.count('0') == len(binary) - 1:
             return True
         return False
