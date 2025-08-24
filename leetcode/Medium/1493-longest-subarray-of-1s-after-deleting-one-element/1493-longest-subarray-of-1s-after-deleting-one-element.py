ec88f13bbee49da7dcc4364ca0efa6fa8ff4624e
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-08-24 18:00:06 +0900

    Time: 50 ms (76.89%), Space: 27.8 MB (6.2%) - LeetHub

--- leetcode/Medium/1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
+++ leetcode/Medium/1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -1,11 +1,11 @@
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        num_split = [len(y) for y in "".join([str(x) for x in nums]).split("0")]
+        num_split = [len(y) for y in ''.join([str(x) for x in nums]).split('0')]
         if len(num_split) == 0:
             return 0
         elif len(num_split) == 1:
             return num_split[0] - 1
         answer = num_split[0]
         for i in range(1, len(num_split)):
-            answer = max(answer, num_split[i] + num_split[i - 1])
+            answer = max(answer, num_split[i] + num_split[i-1])
         return answer
