e6c3bb8ed52b5713b13e029ac5058a58a2cbae18
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-09-09 19:15:01 +0900

    Time: 4 ms (63.5%), Space: 18 MB (50.36%) - LeetHub

---
+++ leetcode/Medium/2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.py
@@ -0,0 +1,17 @@
+class Solution:
+    def peopleAwareOfSecret(self, n: int, delay: int, forget: int) -> int:
+        if n == 1:
+            return 1
+        dp = [0] * (n+1)
+        mod = 10 ** 9 + 7
+        dp[1] = 1
+        window = 0
+        for i in range(2, n+1):
+            enter, out = i-delay, i-forget
+            if enter >= 1:
+                window = (window+dp[enter])%mod
+            if out>=1:
+                window = (window-dp[out]+mod)%mod
+            dp[i] = window
+        start = max(1, n-forget+1)
+        return sum(dp[start:n+1])%mod
