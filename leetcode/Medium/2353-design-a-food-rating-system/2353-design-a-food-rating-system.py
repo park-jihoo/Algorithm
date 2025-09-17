5a7cc2d227280e12a2cfb4547615e4e06ba132be
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-09-17 19:10:14 +0900

    Time: 276 ms (23.22%), Space: 50.2 MB (95.51%) - LeetHub

--- leetcode/Medium/2353-design-a-food-rating-system/2353-design-a-food-rating-system.py
+++ leetcode/Medium/2353-design-a-food-rating-system/2353-design-a-food-rating-system.py
@@ -1,13 +1,14 @@
 from sortedcontainers import SortedList
 
-
 class FoodRatings:
+
     def __init__(self, foods: List[str], cuisines: List[str], ratings: List[int]):
         self.mp = {}
         self.data = defaultdict(SortedList)
         for food, cuisine, rating in zip(foods, cuisines, ratings):
             self.mp[food] = (cuisine, rating)
             self.data[cuisine].add((-rating, food))
+        
 
     def changeRating(self, food: str, newRating: int) -> None:
         cuisine, rating = self.mp[food]
@@ -17,6 +18,7 @@ class FoodRatings:
 
     def highestRated(self, cuisine: str) -> str:
         return self.data[cuisine][0][1]
+        
 
 
 # Your FoodRatings object will be instantiated and called as such:
