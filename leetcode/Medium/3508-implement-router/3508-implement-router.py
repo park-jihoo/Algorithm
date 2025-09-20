5cc87e8a313f8412fc2abd8b34b4d39e8c0fa2ee
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-09-20 16:44:24 +0900

    Time: 509 ms (24.18%), Space: 92.2 MB (11.2%) - LeetHub

---
+++ leetcode/Medium/3508-implement-router/3508-implement-router.py
@@ -0,0 +1,38 @@
+class Router:
+    def __init__(self, memoryLimit: int):
+        self.routes = deque()
+        self.max_routes = memoryLimit
+        self.routes_set = set()
+        self.dest_timestamp = defaultdict(deque)
+        
+    def addPacket(self, source: int, destination: int, timestamp: int) -> bool:
+        if (source, destination, timestamp) in self.routes_set:
+            return False
+        if len(self.routes) == self.max_routes:
+            self.forwardPacket()
+
+        self.routes.append([source, destination, timestamp])
+        self.routes_set.add((source, destination, timestamp))
+        self.dest_timestamp[destination].append(timestamp)
+        return True
+        
+    def forwardPacket(self) -> List[int]:
+        if self.routes:
+            source, destination, timestamp = self.routes.popleft()
+            self.routes_set.remove((source, destination, timestamp))
+            self.dest_timestamp[destination].popleft()
+            return [source, destination, timestamp]
+        return []
+
+    def getCount(self, destination: int, startTime: int, endTime: int) -> int:
+        time_range = self.dest_timestamp[destination]
+        L = bisect_left(time_range, startTime)
+        R = bisect_right(time_range, endTime)
+        return R-L
+
+
+# Your Router object will be instantiated and called as such:
+# obj = Router(memoryLimit)
+# param_1 = obj.addPacket(source,destination,timestamp)
+# param_2 = obj.forwardPacket()
+# param_3 = obj.getCount(destination,startTime,endTime)
