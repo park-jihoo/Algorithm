3e9a48be117a7ac37f903b8b46c77e700a8b9907
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-10-05 13:11:56 +0900

    Time: 39 ms (38.91%), Space: 19.3 MB (79.71%) - LeetHub

---
+++ leetcode/Medium/0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -0,0 +1,26 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        graph = defaultdict(list)
+        m, n = len(heights[0]), len(heights)
+        pacific, atlantic = set(), set()
+
+        def bfs(i, j, visited):
+            q = deque([])
+            q.append((i, j))
+            visited.add((i, j))
+            while q:
+                x, y = q.popleft()
+                for dx, dy in [(0, -1), (0, 1), (1, 0), (-1, 0)]:
+                    if 0<=x+dx<n and 0<=y+dy<m and (x+dx, y+dy) not in visited and heights[x][y] <= heights[x+dx][y+dy]:
+                        visited.add((x+dx, y+dy))
+                        q.append((x+dx, y+dy))
+        
+        for i in range(n):
+            bfs(i, 0, pacific)
+        for i in range(m):
+            bfs(0, i, pacific)
+        for i in range(n):
+            bfs(i, m-1, atlantic)
+        for i in range(m):
+            bfs(n-1, i, atlantic)
+        return [list(x) for x in atlantic & pacific]
