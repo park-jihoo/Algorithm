f74cf6c280b7782ff8fed418a35492b57d3d836a
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-10-10 22:21:36 +0900

    Time: 1223 ms (23.13%), Space: 31.8 MB (17.5%) - LeetHub

---
+++ leetcode/Medium/3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maximumEnergy(self, energy: List[int], k: int) -> int:
+        ks = [[] for _ in range(k)]
+        for idx, val in enumerate(energy):
+            ks[idx%k].append(val)
+        ans = float('-inf')
+        for kl in ks:
+            ans = max(ans, *accumulate(reversed(kl)))
+        return ans
