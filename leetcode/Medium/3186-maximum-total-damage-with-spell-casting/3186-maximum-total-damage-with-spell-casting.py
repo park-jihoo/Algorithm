55394683b45fb8bdcf51350c582209d0705988bc
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-10-11 12:48:45 +0900

    Time: 471 ms (69.25%), Space: 45.5 MB (46.5%) - LeetHub

---
+++ leetcode/Medium/3186-maximum-total-damage-with-spell-casting/3186-maximum-total-damage-with-spell-casting.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maximumTotalDamage(self, power: List[int]) -> int:
+        cnt = Counter(power)
+        v = [(-(10**9), 0)]
+        for k in sorted(cnt.keys()):
+            v.append((k, cnt[k]))
+        n = len(v)
+        dp, x, j = [0]*n, 0, 1
+        for i in range(1, n):
+            while j < i and v[j][0] < v[i][0] - 2:
+                x = max(x, dp[j])
+                j += 1
+            dp[i] = x + v[i][0] * v[i][1]
+        return max(dp)
