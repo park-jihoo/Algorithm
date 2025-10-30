7a12990c4ffa7e99994024214d5877cb0f4c21e3
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-10-30 18:59:28 +0900

    Time: 37 ms (61.63%), Space: 26.6 MB (96.16%) - LeetHub

---
+++ leetcode/Hard/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minNumberOperations(self, target: List[int]) -> int:
+        ans, n = 0, len(target)
+        for i in range(n - 1):
+            if target[i] > target[i+1]:
+                ans += (target[i] - target[i+1])
+        return ans + target[n-1]
