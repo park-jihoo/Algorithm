0fc6f4091b9eeccc84d896d2ff47bf7c236209ad
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-11-09 16:38:44 +0900

    Time: 185 ms (5.01%), Space: 35.5 MB (5.13%) - LeetHub

---
+++ leetcode/Easy/2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.py
@@ -0,0 +1,5 @@
+class Solution:
+    def countOperations(self, num1: int, num2: int) -> int:
+        if num1 == 0 or num2 == 0:
+            return 0
+        return self.countOperations(min(num1, num2), abs(num1-num2)) + 1
