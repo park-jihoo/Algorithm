8cbc17b09594bb1db2de4e509119cc599a8908eb
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-11-16 17:55:01 +0900

    Time: 7 ms (94.38%), Space: 18.7 MB (9.74%) - LeetHub

---
+++ leetcode/Medium/1513-number-of-substrings-with-only-1s/1513-number-of-substrings-with-only-1s.py
@@ -0,0 +1,7 @@
+class Solution:
+    def numSub(self, s: str) -> int:
+        ans, mod = 0, 10**9+7
+        for x in s.split('0'):
+            ans += (len(x) * (len(x) + 1))//2
+            ans %= mod
+        return ans
