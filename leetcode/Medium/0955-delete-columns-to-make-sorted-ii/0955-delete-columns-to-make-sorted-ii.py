6e612c9ad71489da3b6c7ec5cd441450b7d75fc8
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2025-12-21 14:24:47 +0900

    Time: 12 ms (20.51%), Space: 17.4 MB (100%) - LeetHub

---
+++ leetcode/Medium/0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minDeletionSize(self, strs):
+        def is_sorted(A):
+            return all(A[i] <= A[i+1] for i in range(len(A) - 1))
+
+        ans = 0
+        cur = [""] * len(strs)  
+
+        for col in zip(*strs):
+            cur2 = cur[:]
+            for i, letter in enumerate(col):
+                cur2[i] = cur2[i] + letter
+
+            if is_sorted(cur2):
+                cur = cur2
+            else:
+                ans += 1
+
+        return ans
