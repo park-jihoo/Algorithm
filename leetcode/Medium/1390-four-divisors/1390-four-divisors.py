32a593f531f0a84708173b68d43683a24cbbf1b6
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2026-01-04 20:47:50 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ leetcode/Medium/1390-four-divisors/1390-four-divisors.py
@@ -0,0 +1,23 @@
+class Solution:
+    def find_primes(self, n):
+        che = [False, False] + [True] * (n - 1)
+        primes = []
+        for i in range(2, n + 1):
+            if che[i]:
+                primes.append(i)
+                for j in range(i * 2, n + 1, i):
+                    che[j] = False
+        return primes
+    
+    def sumFourDivisors(self, nums: List[int]) -> int:
+        primes = self.find_primes(max(nums))
+        ans = 0
+        for num in nums:
+            for p in primes:
+                if num // p != p and num / p in primes:
+                    ans += (num // p + p + 1 + num)
+                    break
+                if num == p**3:
+                    ans += (1 + p + p**2 + p**3)
+                    break
+        return ans
