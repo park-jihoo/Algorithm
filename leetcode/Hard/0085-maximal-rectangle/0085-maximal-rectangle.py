8e09e9d1e56ac52ef4e72f77f945c89a651ec347
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2026-01-11 21:49:13 +0900

    Time: 22 ms (99.68%), Space: 24.6 MB (8.87%) - LeetHub

--- leetcode/Hard/0085-maximal-rectangle/0085-maximal-rectangle.py
+++ leetcode/Hard/0085-maximal-rectangle/0085-maximal-rectangle.py
@@ -3,13 +3,13 @@ class Solution:
         if not matrix or not matrix[0]:
             return 0
         n = len(matrix[0])
-        height = [0] * (n + 1)
+        height = [0]*(n+1)
         ans = 0
         for row in matrix:
             for i in range(n):
-                height[i] = height[i] + 1 if row[i] == "1" else 0
+                height[i] = height[i]+1 if row[i] == "1" else 0
             stack = deque([-1])
-            for i in range(n + 1):
+            for i in range(n+1):
                 while height[i] < height[stack[-1]]:
                     h = height[stack.pop()]
                     w = i - 1 - stack[-1]
