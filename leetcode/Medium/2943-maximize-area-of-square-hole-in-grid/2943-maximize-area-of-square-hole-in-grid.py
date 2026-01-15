dc6de9a1304875903d1a187b2d0d15d83d04ed1f
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2026-01-15 20:16:33 +0900

    Time: 3 ms (52.31%), Space: 19.4 MB (13.85%) - LeetHub

---
+++ leetcode/Medium/2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maximizeSquareHoleArea(self, n: int, m: int, hBars: List[int], vBars: List[int]) -> int:
+        def longCons(arr):
+            m = {}
+            res = 0
+            for i in arr:
+                if i in m:
+                    continue
+                l = m.get(i-1, 0)
+                r = m.get(i+1, 0)
+                f = l + r + 1
+                res = max(res, f)
+                m[i-l] = f
+                m[i+r] = f
+            return res
+
+        s = min(longCons(hBars), longCons(vBars)) + 1
+        return s*s
