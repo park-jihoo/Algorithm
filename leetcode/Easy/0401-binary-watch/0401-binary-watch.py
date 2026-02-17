ebc7f52f97bb39bb28483a8d8176023e082433a3
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2026-02-17 17:13:46 +0900

    Time: 0 ms (100%), Space: 19.6 MB (13.49%) - LeetHub

---
+++ leetcode/Easy/0401-binary-watch/0401-binary-watch.py
@@ -0,0 +1,14 @@
+class Solution:
+    def readBinaryWatch(self, turnedOn: int) -> List[str]:
+        bmap = defaultdict(list)
+        for i in range(60):
+            bl = i.bit_count()
+            bmap[bl].append(i)
+        ans = []
+        for hr in range(turnedOn+1):
+            mn = turnedOn - hr
+            for h in bmap[hr]:
+                if 0 <= h < 12:
+                    for m in bmap[mn]:
+                        ans.append(f'{h}:{m:02}')
+        return ans
