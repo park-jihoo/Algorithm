79ba1130192eb6d837eaf6f5fdca6bb4893b4b9e
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2026-03-23 19:56:43 +0900

    Time: 3 ms (85.09%), Space: 19.4 MB (66.67%) - LeetHub

---
+++ leetcode/Medium/1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxProductPath(self, grid):
+        m, n = len(grid), len(grid[0])
+        MOD = 10**9 + 7
+
+        mx = [0] * n
+        mn = [0] * n
+
+        mx[0] = mn[0] = grid[0][0]
+        for j in range(1, n):
+            mx[j] = mn[j] = mx[j - 1] * grid[0][j]
+
+        for i in range(1, m):
+            mx[0] = mn[0] = mx[0] * grid[i][0]
+
+            for j in range(1, n):
+                x = grid[i][j]
+
+                a = mx[j] * x      
+                b = mn[j] * x      
+                c = mx[j - 1] * x  
+                d = mn[j - 1] * x 
+
+                mx[j] = max(a, b, c, d)
+                mn[j] = min(a, b, c, d)
+
+        ans = mx[-1]
+        return -1 if ans < 0 else ans % MOD
