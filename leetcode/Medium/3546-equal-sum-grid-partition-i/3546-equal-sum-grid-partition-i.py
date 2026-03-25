f31c31e54a657b413b501047cb217de2c892ef31
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2026-03-25 19:51:13 +0900

    Time: 111 ms (82.19%), Space: 43.8 MB (62.33%) - LeetHub

---
+++ leetcode/Medium/3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.py
@@ -0,0 +1,5 @@
+class Solution:
+    def canPartitionGrid(self, grid: List[List[int]]) -> bool:
+        hor = list(accumulate(sum(x) for x in grid))
+        ver = list(accumulate(sum(x[i] for x in grid) for i in range(len(grid[0]))))
+        return (hor[-1] %2 == 0 and hor[-1] // 2 in hor) or (ver[-1] %2 == 0 and ver[-1] // 2 in ver)
