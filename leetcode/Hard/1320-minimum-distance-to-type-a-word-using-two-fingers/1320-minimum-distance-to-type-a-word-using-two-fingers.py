41fd2987cdee02ad2e0b3888623419841d501e30
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2026-04-12 20:29:13 +0900

    Time: 187 ms (54.24%), Space: 54.4 MB (20.9%) - LeetHub

---
+++ leetcode/Hard/1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minimumDistance(self, word: str) -> int:
+        from functools import lru_cache
+
+        def dist(a, b):
+            if a == 26 or b == 26:
+                return 0
+            return abs(a//6 - b//6) + abs(a%6 - b%6)
+
+        @lru_cache(None)
+        def solve(i, f1, f2):
+            if i == len(word):
+                return 0
+
+            cur = ord(word[i]) - ord('A')
+
+            move1 = dist(f1, cur) + solve(i+1, cur, f2)
+            move2 = dist(f2, cur) + solve(i+1, f1, cur)
+
+            return min(move1, move2)
+
+        return solve(0, 26, 26)
