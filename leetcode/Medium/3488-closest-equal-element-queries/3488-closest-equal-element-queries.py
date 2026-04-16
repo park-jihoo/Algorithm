59a5c68360414641d8af4c1d078fec72d26b112d
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2026-04-16 16:44:21 +0900

    Time: 310 ms (70.31%), Space: 55.2 MB (25%) - LeetHub

---
+++ leetcode/Medium/3488-closest-equal-element-queries/3488-closest-equal-element-queries.py
@@ -0,0 +1,16 @@
+class Solution:
+    def solveQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        d = defaultdict(list)
+        m = len(nums)
+        for idx, val in enumerate(nums):
+            d[val].append(idx)
+        ans = []
+        for q in queries:
+            lst = d[nums[q]]
+            n = len(lst)
+            if n == 1:
+                ans.append(-1)
+            else:
+                idx = bisect_left(lst, q)
+                ans.append(min((lst[idx] - lst[(idx-1)%n])%m, (lst[(idx+1)%n]-lst[idx])%m))
+        return ans
