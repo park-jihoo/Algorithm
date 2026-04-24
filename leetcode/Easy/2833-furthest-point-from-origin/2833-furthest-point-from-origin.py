510593081687e82a04fb64f690463606ce19281a
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2026-04-24 17:14:28 +0900

    Time: 0 ms (100%), Space: 19.5 MB (18.15%) - LeetHub

---
+++ leetcode/Easy/2833-furthest-point-from-origin/2833-furthest-point-from-origin.py
@@ -0,0 +1,4 @@
+class Solution:
+    def furthestDistanceFromOrigin(self, moves: str) -> int:
+        cnt = Counter(moves)
+        return max(abs(cnt['L'] - cnt['R'] - cnt['_']), abs(cnt['L'] - cnt['R'] + cnt['_']))
