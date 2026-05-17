a533caa14df801e95063ad972dfc1bf0b454160a
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2026-05-17 15:11:28 +0900

    Time: 8 ms (77.85%), Space: 26.5 MB (71.44%) - LeetHub

---
+++ leetcode/Medium/1306-jump-game-iii/1306-jump-game-iii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canReach(self, arr: List[int], start: int) -> bool:
+        visited, q = set([start]), deque([start])
+        while q:
+            node = q.popleft()
+            if arr[node]==0:
+                return True
+            plus, minus = node + arr[node], node - arr[node]
+            if 0 <= plus < len(arr) and plus not in visited:
+                q.append(plus)
+                visited.add(plus)
+            if 0 <= minus < len(arr) and minus not in visited:
+                q.append(minus)
+                visited.add(minus)
+        return False
