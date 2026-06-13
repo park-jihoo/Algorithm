02425d6c9f60cfdbf97f51e44e77ca0bd2b08686
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2026-06-13 16:16:56 +0900

    Time: 11 ms (46.98%), Space: 19.1 MB (99.07%) - LeetHub

---
+++ leetcode/Easy/3838-weighted-word-mapping/3838-weighted-word-mapping.py
@@ -0,0 +1,3 @@
+class Solution:
+    def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
+        return "".join([chr(ord('z')-sum(weights[ord(c)-ord('a')] for c in word)%26) for word in words])
