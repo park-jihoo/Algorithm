2f60ee2a13b41de72a752ff58d4e4a340f2a116b
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2026-06-22 19:22:52 +0900

    Time: 11 ms (6.06%), Space: 19.4 MB (9.97%) - LeetHub

---
+++ leetcode/Easy/1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        cnt, bal, ans = Counter(text), Counter('balloon'), 0
+        while cnt >= bal:
+            cnt -= Counter('balloon')
+            ans += 1
+        return ans
