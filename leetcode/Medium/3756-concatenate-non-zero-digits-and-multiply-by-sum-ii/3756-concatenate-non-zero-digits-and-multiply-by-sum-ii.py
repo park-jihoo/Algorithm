f7b0425a925ce5976e780f0be4c6aefd1ad141ab
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2026-07-08 19:42:15 +0900

    Time: 443 ms (80.25%), Space: 58.3 MB (16.05%) - LeetHub

---
+++ leetcode/Medium/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def sumAndMultiply(self, s: str, queries: List[List[int]]) -> List[int]:
+        n, ans, mod = len(s), [], 10**9+7
+        power = [1] * (n+1)
+        for i in range(1, n+1):
+            power[i] = (power[i-1]*10)%mod
+        a, b, L = [[0] * (n+1) for _ in range(3)]
+        for i in range(n):
+            d = int(s[i])
+            a[i+1] = a[i] + d
+            b[i+1] = (b[i]*10+d)%mod if d else b[i]
+            L[i+1] = L[i] + (d>0)
+        
+        for l, r in queries:
+            r += 1
+
+            sub = (b[l] * power[L[r] - L[l]]) % mod
+            x = (b[r] - sub) % mod
+            ans.append((x*(a[r]-a[l]))%mod)
+
+        return ans
