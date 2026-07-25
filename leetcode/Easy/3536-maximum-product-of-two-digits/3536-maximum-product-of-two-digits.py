9f73d1c133c10ec43676093d6e55e6ad60c2f52c
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2026-07-25 22:44:16 +0900

    Time: 0 ms (100%), Space: 19.5 MB (22.61%) - LeetHub

---
+++ leetcode/Easy/3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxProduct(self, n: int) -> int:
+        pq = []
+        while n:
+            n, d = divmod(n, 10)
+            heapq.heappush(pq, -d)
+        a = heapq.heappop(pq)
+        b = heapq.heappop(pq)
+        return a*b
