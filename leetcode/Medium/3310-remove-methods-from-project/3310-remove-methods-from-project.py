f71495c090be2837a235115d003cb1d80450920d
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2026-08-05 20:03:28 +0900

    Time: 419 ms (38.63%), Space: 107.2 MB (56.06%) - LeetHub

---
+++ leetcode/Medium/3310-remove-methods-from-project/3310-remove-methods-from-project.py
@@ -0,0 +1,24 @@
+class Solution:
+    def remainingMethods(self, n: int, k: int, invocations: List[List[int]]) -> List[int]:
+        graph = defaultdict(list)
+        for s, e in invocations:
+            graph[s].append(e)
+        ans = []
+        visited = set()
+        queue = deque([k])
+        while queue:
+            cur = queue.pop()
+            visited.add(cur)
+            for node in graph[cur]:
+                if node not in visited:
+                    queue.append(node)
+
+        ans = []
+        for method in range(n):
+            if method in visited:
+                continue
+            for node in graph[method]:
+                if node in visited:
+                    return list(range(n))
+            ans.append(method)
+        return ans
