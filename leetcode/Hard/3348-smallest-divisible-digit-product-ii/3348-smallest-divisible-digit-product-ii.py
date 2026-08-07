48b83311877df7215f2c150e9b8daa731a49149f
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2026-08-07 20:43:51 +0900

    Time: 287 ms (76.92%), Space: 33.7 MB (82.05%) - LeetHub

---
+++ leetcode/Hard/3348-smallest-divisible-digit-product-ii/3348-smallest-divisible-digit-product-ii.py
@@ -0,0 +1,55 @@
+class Solution:
+    def smallestNumber(self, num: str, t: int) -> str:
+        temp = t
+        for i in range(2, 10):
+            while temp % i == 0:
+                temp //= i
+
+        if temp > 1:
+            return "-1"
+
+        n = len(num)
+        rem = [0] * (n + 1)
+        rem[0] = t
+        pos = n - 1
+
+        num_list = list(num)
+        for i in range(n):
+            if num_list[i] == "0":
+                pos = i
+                break
+            rem[i + 1] = rem[i] // math.gcd(rem[i], int(num_list[i]))
+
+        if rem[n] == 1:
+            return num
+
+        for i in range(pos, -1, -1):
+            while True:
+                num_list[i] = chr(ord(num_list[i]) + 1)
+                if num_list[i] > "9":
+                    break
+
+                t_now = rem[i] // math.gcd(rem[i], int(num_list[i]))
+                k = 9
+
+                for j in range(n - 1, i, -1):
+                    while t_now % k != 0:
+                        k -= 1
+                    t_now //= k
+                    num_list[j] = str(k)
+
+                if t_now == 1:
+                    return "".join(num_list)
+
+        ans = []
+        original_t = t
+        for i in range(9, 1, -1):
+            while original_t % i == 0:
+                ans.append(str(i))
+                original_t //= i
+
+        ans_str = "".join(ans)
+        padding = max(n + 1 - len(ans_str), 0)
+        ans_str += "1" * padding
+
+        return ans_str[::-1]
