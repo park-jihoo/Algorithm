175c3b4ce476426cb6ac3f067b91c1fdd01d2447
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2026-08-17 20:56:30 +0900

    Time: 43 ms (89.63%), Space: 30.5 MB (60%) - LeetHub

---
+++ leetcode/Medium/2029-stone-game-ix/2029-stone-game-ix.py
@@ -0,0 +1,13 @@
+class Solution:
+    def stoneGameIX(self, stones: List[int]) -> bool:
+        cnt0 = cnt1 = cnt2 = 0
+        for val in stones:
+            if (typ := val % 3) == 0:
+                cnt0 += 1
+            elif typ == 1:
+                cnt1 += 1
+            else:
+                cnt2 += 1
+        if cnt0 % 2 == 0:
+            return cnt1 >= 1 and cnt2 >= 1
+        return cnt1 - cnt2 > 2 or cnt2 - cnt1 > 2
