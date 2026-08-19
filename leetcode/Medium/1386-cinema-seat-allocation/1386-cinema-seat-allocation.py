5f52be670612b4b4869fced011b3a3fd290cf9db
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2026-08-19 15:13:27 +0900

    Time: 23 ms (93.32%), Space: 22.4 MB (94.6%) - LeetHub

---
+++ leetcode/Medium/1386-cinema-seat-allocation/1386-cinema-seat-allocation.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxNumberOfFamilies(self, n: int, reservedSeats: List[List[int]]) -> int:
+        ans = 0
+        c = defaultdict(int)
+        for r, s in reservedSeats:
+            if 1 < s < 10:
+                c[r] |= 1 << (s-2)
+        return (n-len(c))*2 + sum(0 in (r&15, r&60, r&240) for r in c.values())
