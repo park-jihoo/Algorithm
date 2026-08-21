3ce5d1066950802375c97337f3968635b9d2aaf9
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2026-08-21 16:45:10 +0900

    Time: 168 ms (29.27%), Space: 19.9 MB (37.8%) - LeetHub

---
+++ leetcode/Hard/3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findKthSmallest(self, coins: List[int], k: int) -> int:
+        n = len(coins)
+        dic = defaultdict(list)
+        for i in range(1, n + 1):
+            for comb in itertools.combinations(coins, i):
+                dic[len(comb)].append(math.lcm(*comb))
+        
+        def count(dic, target):
+            ans = 0
+            for i in range(1, n + 1):
+                for lcm in dic[i]:
+                    ans += target // lcm * pow(-1, i + 1)
+            return ans
+        
+        start, end = min(coins), min(coins) * k
+        while start + 1 < end:
+            mid = (start + end) // 2
+            if count(dic, mid) >= k:
+                end = mid
+            else:
+                start = mid
+        if count(dic, start) >= k:
+            return start
+        else:
+            return end
