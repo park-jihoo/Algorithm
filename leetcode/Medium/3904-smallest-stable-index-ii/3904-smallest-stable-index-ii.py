76d906ebb8ce289d48fbb3a455003743c2a4b3e4
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2026-09-06 00:00:42 +0900

    Time: 123 ms (93.18%), Space: 32.3 MB (67.73%) - LeetHub

---
+++ leetcode/Medium/3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.py
@@ -0,0 +1,7 @@
+class Solution:
+    def firstStableIndex(self, nums: list[int], k: int) -> int:
+        pmax, pmin = list(accumulate(nums, max)), list(accumulate(nums[::-1], min))[::-1]
+        for i in range(len(nums)):
+            if pmax[i] - pmin[i] <= k:
+                return i
+        return -1
