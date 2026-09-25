d6f7ccc3ff9fe8986facac3f3ac9a7fe2420a92d
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2026-09-25 19:46:23 +0900

    Time: 23 ms (5.46%), Space: 20.2 MB (6.56%) - LeetHub

---
+++ leetcode/Hard/1096-brace-expansion-ii/1096-brace-expansion-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def braceExpansionII(self, expression: str) -> list[str]:
+        ans = set()
+
+        def dfs(s):
+            r = s.find('}')
+            if r == -1:
+                ans.add(s)
+                return
+            l = s.rfind('{', 0, r)
+
+            left = s[:l]
+            right = s[r + 1:]
+
+            inside = s[l + 1:r]
+
+            for part in inside.split(','):
+                dfs(left + part + right)
+
+        dfs(expression)
+        return sorted(ans)
